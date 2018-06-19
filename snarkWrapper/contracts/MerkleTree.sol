b35e6ff242c6d8882e6bbe31f9473d6cf267ca8f
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-06-19 19:53:49 +0100

    Cleanup the Merkle tree contract

--- snarkWrapper/contracts/MerkleTree.sol
+++ snarkWrapper/contracts/MerkleTree.sol
@@ -5,28 +5,28 @@ contract MerkleTree {
     mapping (bytes32 => bool) public roots;
 
     struct Mtree {
-        uint cur;
-        bytes32[16] leaves;
+        uint currentNode; // Index of the current node where to insert the next incoming commitment
+        bytes32[16] leaves; // Array containing the 16 leaves of the merkle tree of commitments
     }
 
     Mtree public MT;
-    bytes public vk;
-
+    
+    // Constructor
     function MerkleTree() {
-        for (uint i = 0; i < 16; i++)
-            MT.leaves[i] = 0x0;
-
+        for (uint i = 0; i < 16; i++) {
+            MT.leaves[i] = 0x0; // Initialization of all the leaves to the zero value 0x0
+        }
     }
 
-
-    //Merkletree.append(com)
-    function insert(bytes32 com) internal returns (bool res) {
-        if (MT.cur == 16) {
-            return false;
+    // Appends a commitment to the tree
+    function insert(bytes32 commitment) internal returns (bool res) {
+        if (MT.currentNode == 16) {
+            return false; // All the leaves contain a commitment => The tree is "full"
         }
-        MT.leaves[MT.cur] = com;
-        MT.cur++;
-        return true;
+
+        MT.leaves[MT.currentNode] = commitment;
+        MT.currentNode++;
+        return true; // Insertion sucessful
     }
 
     function getSha256(bytes32 input, bytes32 sk) constant returns ( bytes32) { 
@@ -34,22 +34,24 @@ contract MerkleTree {
     } 
 
     function getLeaves() constant returns (bytes32[16]) {
-        return MT.leaves;
+        return MT.leaves; // Returns the array of leaves of the merkle tree
     }
 
     function getTree() constant returns (bytes32[32] tree) {
         //bytes32[32] memory tree;
+
         bytes32 test = 0;
         uint i;
-        for (i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++) {
             tree[16 + i] = MT.leaves[i];
+        }
         for (i = 16 - 1; i > 0; i--) {
             tree[i] = sha256(tree[i*2], tree[i*2+1]); 
         }
         return tree;
     }
 
-    //Merkletree.root()
+    // Returns the root of the merkle tree
     function getRoot() constant returns(bytes32 root) {
         root = getTree()[1];
     }
