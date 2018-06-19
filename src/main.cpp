efffe3b3797932b92a08e5d7e874ac39e48b27b2
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-06-19 21:07:15 +0100

    Removed useless comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,9 @@
 #include "libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp" //hold key
 
 #include <libsnark/common/data_structures/merkle_tree.hpp>
+
 #include <libsnark/gadgetlib1/gadget.hpp>
 #include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_authentication_path_variable.hpp>
-#include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_read_gadget.hpp>
-
 #include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_read_gadget.hpp>
 #include <libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_update_gadget.hpp>
 
@@ -69,25 +68,22 @@ class Miximus {
         std::shared_ptr<multipacking_gadget<FieldT>> unpacker;
         std::shared_ptr<multipacking_gadget<FieldT>> unpacker1;
 
-        //digest_variable<FieldT> root_digest(pb, digest_len, "root_digest");
         std::shared_ptr<digest_variable<FieldT>> root_digest;
-        //digest_variable<FieldT> cm(pb, digest_len, "cm_digest");
         std::shared_ptr<digest_variable<FieldT>> cm;
-        //digest_variable<FieldT> sk(pb, digest_len, "sk_digest");
         std::shared_ptr<digest_variable<FieldT>> sk;
-        //digest_variable<FieldT> leaf_digest(pb, digest_len, "leaf_digest");
         std::shared_ptr<digest_variable<FieldT>> leaf_digest;
 
         std::shared_ptr<sha256_ethereum> cm_hash;
 
-
-        std::shared_ptr<merkle_authentication_path_variable<FieldT, HashT>> path_var;
-
-        std::shared_ptr<merkle_tree_check_read_gadget<FieldT, HashT>> ml;
+        std::shared_ptr<merkle_authentication_path_variable<FieldT, HashT>> path_variable;
+        // See: libsnark/gadgetlib1/gadgets/merkle_tree/merkle_tree_check_read_gadget.hpp
+        // The gadget checks the following: given a root R, address A, value V, and
+        // authentication path P, check that P is a valid authentication path for the
+        // value V as the A-th leaf in a Merkle tree with root R.
+        std::shared_ptr<merkle_tree_check_read_gadget<FieldT, HashT>> check_membership;
 
         pb_variable_array<FieldT> address_bits_va;
         std::shared_ptr <block_variable<FieldT>> input_variable;
-
         pb_variable<FieldT> ZERO;
 
         // I think there are 2 variable arrays because we want to prove 2 things
@@ -100,7 +96,7 @@ class Miximus {
         pb_variable_array<FieldT> packed_inputs1;
         pb_variable_array<FieldT> unpacked_inputs1;
 
-
+        // Constructor of the class "Miximus"
         Miximus() {
             packed_inputs.allocate(pb, 1 + 1, "packed");
             packed_inputs1.allocate(pb, 1 + 1, "packed");
@@ -114,73 +110,53 @@ class Miximus {
             sk.reset(new digest_variable<FieldT>(pb, 256, "sk"));
             leaf_digest.reset(new digest_variable<FieldT>(pb, 256, "leaf_digest"));
 
-            //unpacked_inputs.insert(unpacked_inputs.end(), true );
             unpacked_inputs.insert(unpacked_inputs.end(), root_digest->bits.begin(), root_digest->bits.end());
-            //unpacked_inputs.insert(unpacked_inputs.end(), cm->bits.begin(), cm->bits.end());
-
-            unpacker.reset(new multipacking_gadget<FieldT>(
-                        pb,
-                        unpacked_inputs,
-                        packed_inputs,
-                        FieldT::capacity(),
-                        "unpacker"
-                        ));
+            unpacker.reset(new multipacking_gadget<FieldT>(pb, unpacked_inputs, packed_inputs, FieldT::capacity(), "unpacker"));
 
             unpacked_inputs1.insert(unpacked_inputs1.end(), cm->bits.begin(), cm->bits.end());
+            unpacker1.reset(new multipacking_gadget<FieldT>(pb, unpacked_inputs1, packed_inputs1, FieldT::capacity(), "unpacker"));
 
-            unpacker1.reset(new multipacking_gadget<FieldT>(
-                        pb,
-                        unpacked_inputs1,
-                        packed_inputs1,
-                        FieldT::capacity(),
-                        "unpacker"
-                        ));
-
-            pb.set_input_sizes(18 + 1 );
+            pb.set_input_sizes(18 + 1); // The size of the inputs is: 16 (leaves) + root + sk + cm (TODO: To verify)
 
             input_variable.reset(new block_variable<FieldT>(pb, *cm, *sk, "input_variable")); 
 
-            cm_hash.reset(new sha256_ethereum(
-                        pb, SHA256_block_size, *input_variable, *leaf_digest, "cm_hash"
-                        ));
-            //sha256_ethereum g(pb, SHA256_block_size, *input_variable, *leaf_digest, "g");
+            cm_hash.reset(new sha256_ethereum(pb, SHA256_block_size, *input_variable, *leaf_digest, "cm_hash"));
+            path_variable.reset(new  merkle_authentication_path_variable<FieldT, HashT> (pb, tree_depth, "path_variable" ));
+            // merkle_authentication_path_variable<FieldT, HashT> path_variable(pb, tree_depth, "path_variable");
+            check_membership.reset(new merkle_tree_check_read_gadget<FieldT, HashT>(pb, tree_depth, address_bits_va, *leaf_digest, *root_digest, *path_variable, ONE, "check_membership"));
 
-            path_var.reset(new  merkle_authentication_path_variable<FieldT, HashT> (pb, tree_depth, "path_var" ));
-            //merkle_authentication_path_variable<FieldT, HashT> path_var(pb, tree_depth, "path_var");
-
-            ml.reset(new merkle_tree_check_read_gadget<FieldT, HashT>(pb, tree_depth, address_bits_va, *leaf_digest, *root_digest, *path_var, ONE, "ml"));
-            //merkle_tree_check_read_gadget<FieldT, HashT> ml(pb, tree_depth, address_bits_va, *leaf_digest, *root_digest, path_var, ONE, "ml");
-
-            // generate constraints
-            //root_digest.generate_r1cs_constraints();
+            // Generate constraints
+            // root_digest.generate_r1cs_constraints();
             unpacker->generate_r1cs_constraints(true);
             unpacker1->generate_r1cs_constraints(false);
 
             generate_r1cs_equals_const_constraint<FieldT>(pb, ZERO, FieldT::zero(), "ZERO");
             cm_hash->generate_r1cs_constraints(true);
-            path_var->generate_r1cs_constraints();
-            ml->generate_r1cs_constraints();
+            path_variable->generate_r1cs_constraints();
+            check_membership->generate_r1cs_constraints();
             leaf_digest->generate_r1cs_constraints();
         }
 
+        // The purpose of this cpp program is to generate the proof that is going to be verified
+        // on the solidity contract deployed on the blockchain
         void prove() { 
             // generate witness
             // unpacker->generate_r1cs_constraints(false);
             std::vector<merkle_authentication_node> path(tree_depth);
 
-            libff::bit_vector leaf = node16;
+            libff::bit_vector leaf = node16; // The proof is given for the node16 (containing the inserted commitment)
             libff::bit_vector address_bits;
             size_t address = 0; // uint of address_bits
-            address_bits = {0,0,0,0}; // defines the ordering of hashing 
-            path = {node3,node5,node9,node17};
+            address_bits = {0,0,0,0}; // defines the ordering of hashing
+            path = {node3,node5,node9,node17}; // path is declared as a vector of length tree_depth (= 4, here), of merkle tree nodes
 
             cm->generate_r1cs_witness(nullifier);
             root_digest->generate_r1cs_witness(node_root);
             sk->generate_r1cs_witness(secret);
             cm_hash->generate_r1cs_witness();  
             //leaf_digest->generate_r1cs_witness(leaf);
-            path_var->generate_r1cs_witness(address, path);
-            ml->generate_r1cs_witness();
+            path_variable->generate_r1cs_witness(address, path);
+            check_membership->generate_r1cs_witness();
             unpacker->generate_r1cs_witness_from_bits();
             unpacker1->generate_r1cs_witness_from_bits();
 
@@ -201,40 +177,21 @@ class Miximus {
             // const size_t expected_constraints = merkle_tree_check_read_gadget<FieldT, HashT>::expected_constraints(tree_depth);
             dump_key(pb, "out.txt");
             // assert(num_constraints == expected_constraints);
-
-
-            // execute test 
-
-
-            // pb_variable_array<FieldT> packed_inputs;
-            // pb_variable_array<FieldT> unpacked_inputs;
-            // std::shared_ptr<multipacking_gadget<FieldT>> unpacker; 
         }
 };
 
-    template<typename ppT>
-void test_all_merkle_tree_gadgets()
-{
+template<typename ppT>
+void test_all_merkle_tree_gadgets() {
     typedef libff::Fr<ppT> FieldT;
     // test();
     // main_merkle_tree_check_read_gadget<FieldT, sha256_ethereum>();
-    Miximus<FieldT, sha256_ethereum> c;
-    c.prove();
+    Miximus<FieldT, sha256_ethereum> prover;
+    prover.prove();
 }
 
 int main () {
-
+    // See: https://github.com/scipr-lab/libff/blob/master/libff/algebra/curves/alt_bn128/alt_bn128_init.cpp
     libff::alt_bn128_pp::init_public_params();
-    // test_merkle_tree_check_read_gadget<libff::alt_bn128_pp, sha256_two_to_one_hash_gadget<libff::alt_bn128_pp> >();
-
-    // libff::bn128_pp::init_public_params();
-    // test_all_merkle_tree_gadgets<libff::alt_bn128_pp>();
-    // main_merkle_tree_check_read_gadget<libff::bn128_pp, sha256_two_to_one_hash_gadget<libff::bn128_pp> >();
-
-    // libff::edwards_pp::init_public_params();
-    // main_merkle_tree_check_read_gadget<libff::edwards_pp,  sha256_two_to_one_hash_gadget<libff::edwards_pp>>();
-    // libff::edwards_pp::init_public_params();
     test_all_merkle_tree_gadgets<libff::alt_bn128_pp>();
-
     return 0;
 }
