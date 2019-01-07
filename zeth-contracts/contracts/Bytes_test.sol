45263e3e41e2422bb2f3372685841e507cf49d68
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-07 19:43:50 +0000

    Finished test for the Bytes.sol library

--- zeth-contracts/contracts/Bytes_test.sol
+++ zeth-contracts/contracts/Bytes_test.sol
@@ -46,8 +46,8 @@ contract Bytes_tests {
 		return ok;
 	}
 
-	function testBytesToBytes32() public returns (bool) {
-		 memory test_bytes = new bytes(32);
+    function testBytesToBytes32() public returns (bool) {
+        memory test_bytes = new bytes(32);
         for (uint i = 0; i < 15; i++) {
             test_bytes[i] = bytes1(0xAB);
         }
@@ -56,12 +56,27 @@ contract Bytes_tests {
         }
         bytes32 test_bytes32 = bytesToBytes32(test_bytes, 0);
 
-		bool ok = (test_bytes32 == bytes32(0xabababababababababababababababcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd));
-		require(
-			ok,
-			"[testBytesToBytes32] Failed"
-		);
+        bool ok = (test_bytes32 == bytes32(0xabababababababababababababababcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd));
+        require(
+            ok,
+            "[testBytesToBytes32] Failed"
+        );
 
-		return ok;
-	}
+        return ok;
+    }
+
+    function testGetDigestFromFieldElements() public returns (bool) {
+        uint256[] memory test_input = new uint[](2);
+        test_input[0] = 0x16cc12975b9a52d97c6a5c0cc91b76b7432306724ed800ef1c29e86393b1e757;
+        test_input[1] = 0x4;
+        bytes32 test_res = testRootAssemble(test_input);
+
+        bool ok = (test_res == bytes32(0xeae78dc9c6179438f7001b724e60c4c2ed6ed893303a563e9b4a59dae9483369));
+        require(
+             ok,
+            "[testGetDigestFromFieldElements] Failed"
+        );
+
+        return ok;
+    }
 }
