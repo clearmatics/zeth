56baaf8e425128ebe15c8a35c5bf5ee42f68a414
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-08 11:33:32 +0000

    Deleted the use of the buggy 'padZero' function to use the Bytes library

--- zeth-contracts/contracts/Miximus.sol
+++ zeth-contracts/contracts/Miximus.sol
@@ -53,10 +53,6 @@ contract Miximus is MerkleTreeSha256 {
         uint commitmentAddress = insert(commitment);
         emit LogAddress(commitmentAddress);
 
-        // We need to padZero the tree root because when we generate the proof
-        // The last byte get stripped
-        //
-        // Fix the issue with padZero: See if we can safely delete it and use the 2 inputs representing the root instead
         bytes32 currentRoot = getRoot();
         emit LogMerkleRoot(currentRoot);
 
@@ -77,23 +73,26 @@ contract Miximus is MerkleTreeSha256 {
         uint[] memory input
     ) public {
         // The recipient is part of the nullifier --> To change!
-        address recipient = nullifierToAddress(flip_endianness(bytes32(input[2])));
+        address recipient = nullifierToAddress(Bytes.flip_endianness(bytes32(input[2])));
         // See: https://solidity.readthedocs.io/en/v0.5.0/types.html#address
         // for more information on address payable type and conversion from and to address type
         address payable recipientAddr = address(uint160(recipient));
 
         // We re-assemble the full root digest from both field elements (created when we packed the
         // 256-bit digest into field elements, that are both encoded on 253 bits --> size of the field we use)
-        uint256[] memory root_inputs = new uint[](2);
-        root_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
-        root_inputs[1] = input[1];
+        uint256[] memory digest_inputs = new uint[](2);
+        digest_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
+        digest_inputs[1] = input[1];
         require(
-            roots[Bytes.sha256_digest_from_field_elements(root_inputs)],
+            roots[Bytes.sha256_digest_from_field_elements(digest_inputs)],
             "Invalid root: This root doesn't exist"
         );
 
+        digest_inputs[0] = input[2]; // See the way the inputs are ordered in the extended proof
+        digest_inputs[1] = input[3];
+        bytes32 current_nullifier = Bytes.sha256_digest_from_field_elements(digest_inputs);
         require(
-            !nullifiers[padZero(flip_endianness(bytes32(input[2])))],
+            !nullifiers[current_nullifier],
             "Invalid nullifier: This nullifier has already been used"
         );
 
@@ -106,7 +105,7 @@ contract Miximus is MerkleTreeSha256 {
         recipientAddr.transfer(denomination * (1 ether));
 
         // Declare the nullifier as being used (not usable anymore: prevents double spend)
-        nullifiers[padZero(flip_endianness(bytes32(input[2])))] = true;
+        nullifiers[current_nullifier] = true;
     }
 
     // The forward function enables a user who has been the recipient of a "private payment" in the past
@@ -127,7 +126,7 @@ contract Miximus is MerkleTreeSha256 {
         uint[2] memory k,
         uint[] memory input
     ) public {
-        address recipient  = nullifierToAddress(flip_endianness(bytes32(input[2])));
+        address recipient  = nullifierToAddress(Bytes.flip_endianness(bytes32(input[2])));
 
         require(
             msg.sender == recipient,
@@ -136,18 +135,22 @@ contract Miximus is MerkleTreeSha256 {
 
         // We re-assemble the full root digest from both field elements (created when we packed the
         // 256-bit digest into field elements, that are both encoded on 253 bits --> size of the field we use)
-        uint256[] memory root_inputs = new uint[](2);
-        root_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
-        root_inputs[1] = input[1];
+        uint256[] memory digest_inputs = new uint[](2);
+        digest_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
+        digest_inputs[1] = input[1];
         require(
-            roots[Bytes.sha256_digest_from_field_elements(root_inputs)],
+            roots[Bytes.sha256_digest_from_field_elements(digest_inputs)],
             "Invalid root: This root doesn't exist"
         );
 
+        digest_inputs[0] = input[2]; // See the way the inputs are ordered in the extended proof
+        digest_inputs[1] = input[3];
+        bytes32 current_nullifier = Bytes.sha256_digest_from_field_elements(digest_inputs);
         require(
-            !nullifiers[padZero(flip_endianness(bytes32(input[2])))],
+            !nullifiers[current_nullifier],
             "Invalid nullifier: This nullifier has already been used"
         );
+
         require(
             zksnark_verifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input),
             "Invalid proof: Unable to verify the proof correctly"
@@ -164,6 +167,10 @@ contract Miximus is MerkleTreeSha256 {
 
         bytes32 currentRoot = getRoot();
         emit LogMerkleRoot(currentRoot);
+
+        // Declare the nullifier as being used (not usable anymore: prevents double spend)
+        nullifiers[current_nullifier] = true;
+        // Add the new root to the list of existing roots
         roots[currentRoot] = true;
     }
 
@@ -180,38 +187,5 @@ contract Miximus is MerkleTreeSha256 {
             mstore(add(y, 20), source)
         }
         return(address(y[0]));
-    }
-
-    // Hack to side step the fact that libsnark only allows 253 bit chunks in its output
-    // to overcome this we only validate the first 252 bits of the merkle root
-    // and the nullifier. We set the last byte to zero.
-    function padZero(bytes32 x) internal pure returns(bytes32) {
-        return(x & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0);
-    }
-
-    // Functions used to flip endianness
-    // Example:
-    // Input: 1011...01100
-    // Output: 00110...1101
-    function flip_endianness(bytes32 a) internal pure returns(bytes32) {
-        uint r;
-        uint i;
-        uint b;
-        for (i=0; i<32; i++) {
-            b = (uint(a) >> ((31-i)*8)) & 0xff;
-            b = reverseByte(b);
-            r += b << (i*8);
-        }
-        return bytes32(r);
-    }
-
-    // Example:
-    // Input: 8 (decimal) -> 0000 1000 (binary)
-    // Output: 0001 0000 (binary) -> 16 (decimal)
-    function reverseByte(uint a) internal pure returns (uint) {
-        uint c = 0xf070b030d0509010e060a020c0408000;
-
-        return (( c >> ((a & 0xF)*8)) & 0xF0)   +
-               (( c >> (((a >> 4)&0xF)*8) + 4) & 0xF);
     }
 }
