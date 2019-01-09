1867369c9b196d5a8d88382544e6fdc1910213ad
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-09 17:46:00 +0000

    Removed useless npm scripts and adjusted ganache-cli parameters

--- zeth-contracts/truffle.js
+++ zeth-contracts/truffle.js
@@ -1,14 +1,9 @@
-module.exports = { // Truffle config from Ion to keep a consistent config across projects
+module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       gas: 0xFFFFFFFFFFF,
-      network_id: "*"
-    },
-    clique: {
-      host: "localhost",
-      port: 8501,
       network_id: "*"
     },
     coverage: {
