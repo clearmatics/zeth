5b665fa73bc37872d4c84855503fd55b454bbcfe
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-22 18:03:45 +0000

    Added first test to deposit arbitrary value on the mixer

--- zeth-contracts/contracts/Miximus.sol
+++ zeth-contracts/contracts/Miximus.sol
@@ -13,21 +13,9 @@ contract Miximus is MerkleTreeSha256 {
     // The public list of nullifiers (prevents double spend)
     mapping(bytes32 => bool) nullifiers;
 
-    // Smart contract responsible of on-chain verification of proofs
-    Verifier public zksnark_verifier;
-
-    // Denomination of the mixer
-    uint denomination;
-
     // Constructor
-    constructor(address _zksnark_verify, uint denom, uint depth) MerkleTreeSha256(depth) public {
-        zksnark_verifier = Verifier(_zksnark_verify);
-        denomination = denom;
-    }
-
-    // /!\ WARNING: Function used for a **development purpose** --> change the verifier contract
-    function setVerifier (address _zksnark_verify) public {
-        zksnark_verifier = Verifier(_zksnark_verify);
+    constructor(uint depth) MerkleTreeSha256(depth) public {
+        // Nothing
     }
 
     // Event to emit the address of a commitment in the merke tree
@@ -41,16 +29,27 @@ contract Miximus is MerkleTreeSha256 {
     // (useful to ease backup of user's wallets)
     event LogSecretCiphers(string ciphertext);
 
+    // Debug only
+    event LogRecomputedCommmitment(bytes32 recompComm);
+    event LogTrapdoorS(bytes32 trap);
+    event LogInternalCommitment(bytes32 interComm);
+    event LogValue(uint256 value);
+    event LogABIEncoded(bytes encoded);
+
     // Deposit takes a commitment as a parameter. The commitment in inserted in the Merkle Tree of commitment
     // in exchange of an amount of ether (the mixer's denomination) being paid
     function deposit(
         string memory ciphertext,
-        bytes32 commitment
+        bytes32 commitment,
+        bytes32 internal_commitment_k, // Added to support minting of arbitrary values
+        bytes32 trapdoor_s // Added to support minting of arbitrary values
     ) public payable {
-        // We assume that the denomination is an int multiple of ethers (to adjust if necessary)
+        // Verify that the value the depositer commited to is the same as the value paid during the function call
+        // Recompute the commitment from the value, the internal_commitment_k, and the trapdoor_s
+        bytes32 recomputed_commitment = sha256(abi.encodePacked(trapdoor_s, internal_commitment_k, msg.value));
         require(
-            msg.value == (denomination * (1 ether)),
-            "Wrong msg.value: Should equal the denomination of the mixer"
+            recomputed_commitment == commitment,
+            "Wrong msg.value: Should equal the value commited to"
         );
 
         uint commitmentAddress = insert(commitment);
@@ -63,136 +62,5 @@ contract Miximus is MerkleTreeSha256 {
         emit LogSecretCiphers(ciphertext);
 
         roots[currentRoot] = true;
-    }
-
-    // The withdraw function enables a user to redeem the mixer's denomination amount of ether by providing
-    // a valid proof that he knows the pre-image of a commitment in the merkle tree that has never been "spent"
-    function withdraw (
-        uint[2] memory a,
-        uint[2] memory a_p,
-        uint[2][2] memory b,
-        uint[2] memory b_p,
-        uint[2] memory c,
-        uint[2] memory c_p,
-        uint[2] memory h,
-        uint[2] memory k,
-        uint[] memory input
-    ) public {
-        // The recipient is part of the nullifier --> To change!
-        address recipient = nullifierToAddress(Bytes.flip_endianness(bytes32(input[2])));
-        // See: https://solidity.readthedocs.io/en/v0.5.0/types.html#address
-        // for more information on address payable type and conversion from and to address type
-        address payable recipientAddr = address(uint160(recipient));
-
-        // We re-assemble the full root digest from both field elements (created when we packed the
-        // 256-bit digest into field elements, that are both encoded on 253 bits --> size of the field we use)
-        uint256[] memory digest_inputs = new uint[](2);
-        digest_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
-        digest_inputs[1] = input[1];
-        require(
-            roots[Bytes.sha256_digest_from_field_elements(digest_inputs)],
-            "Invalid root: This root doesn't exist"
-        );
-
-        digest_inputs[0] = input[2]; // See the way the inputs are ordered in the extended proof
-        digest_inputs[1] = input[3];
-        bytes32 current_nullifier = Bytes.sha256_digest_from_field_elements(digest_inputs);
-        require(
-            !nullifiers[current_nullifier],
-            "Invalid nullifier: This nullifier has already been used"
-        );
-
-        require(
-            zksnark_verifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input),
-            "Invalid proof: Unable to verify the proof correctly"
-        );
-
-        // Send the right denomination to the recipient
-        recipientAddr.transfer(denomination * (1 ether));
-
-        // Declare the nullifier as being used (not usable anymore: prevents double spend)
-        nullifiers[current_nullifier] = true;
-    }
-
-    // The transfer function enables a user who has been the recipient of a "private payment" in the past
-    // (someone possessing the secret associated with a non-spent nullifier, and a commitment in the tree)
-    // to use it to pay someone else (ie: "spend" his nullifier and creating a new commitment in the tree to pay someone else)
-    //
-    // This function basically does a payment via the use of commitments and zero knowledge proof verification on-chain
-    function transfer (
-        string memory ciphertext,
-        bytes32 commitment,
-        uint[2] memory a,
-        uint[2] memory a_p,
-        uint[2][2] memory b,
-        uint[2] memory b_p,
-        uint[2] memory c,
-        uint[2] memory c_p,
-        uint[2] memory h,
-        uint[2] memory k,
-        uint[] memory input
-    ) public {
-        address recipient  = nullifierToAddress(Bytes.flip_endianness(bytes32(input[2])));
-
-        require(
-            msg.sender == recipient,
-            "Invalid sender: The sender should be the address specified in the nullifier"
-        );
-
-        // We re-assemble the full root digest from both field elements (created when we packed the
-        // 256-bit digest into field elements, that are both encoded on 253 bits --> size of the field we use)
-        uint256[] memory digest_inputs = new uint[](2);
-        digest_inputs[0] = input[0]; // See the way the inputs are ordered in the extended proof
-        digest_inputs[1] = input[1];
-        require(
-            roots[Bytes.sha256_digest_from_field_elements(digest_inputs)],
-            "Invalid root: This root doesn't exist"
-        );
-
-        digest_inputs[0] = input[2]; // See the way the inputs are ordered in the extended proof
-        digest_inputs[1] = input[3];
-        bytes32 current_nullifier = Bytes.sha256_digest_from_field_elements(digest_inputs);
-        require(
-            !nullifiers[current_nullifier],
-            "Invalid nullifier: This nullifier has already been used"
-        );
-
-        require(
-            zksnark_verifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input),
-            "Invalid proof: Unable to verify the proof correctly"
-        );
-
-        // Declare the nullifier as being used (not usable anymore: prevents double spend)
-        nullifiers[current_nullifier] = true;
-
-        // We insert the new commitment in the tree once:
-        // 1. We checked that the transfer request was triggered by the recipient of a past payment who has an "unspent nullifier"
-        // 2. The proof given is valid
-        //uint commitmentAddress = insert(commitment);
-        //emit LogAddress(commitmentAddress);
-        emit LogAddress(insert(commitment));
-
-        // Add the new root to the list of existing roots
-        bytes32 currentRoot = getRoot();
-        roots[currentRoot] = true;
-        emit LogMerkleRoot(currentRoot);
-
-        // Emit the coin's secret data encrypted with the recipient's key
-        emit LogSecretCiphers(ciphertext);
-    }
-
-    function nullifierToAddress(bytes32 source) internal pure returns(address) {
-        bytes20[2] memory y = [bytes20(0), 0];
-        assembly {
-            // mstore(p, v) signifies --> mem[p..(p+32)) := v
-            // where, mem[a...b) signifies the bytes of memory starting
-            // at position a up to (excluding) position b
-            // Thus, here: mstore(y, source) means that we set the first 32bytes
-            // of y (which is 40 bytes in total), to the value stored in source
-            // The remaining 8bytes of the last element of y (y[1]) remain 0
-            mstore(y, source)
-            mstore(add(y, 20), source)
-        }
-        return(address(y[0]));
     }
 }
--- zeth-contracts/migrations/2_deploy_contracts.js
+++ zeth-contracts/migrations/2_deploy_contracts.js
@@ -1,14 +1,14 @@
 // Get verification key
 var path = require('path');
-var setup_path = process.env.ZETH_TRUSTED_SETUP_DIR;
-var vk_json = path.join(setup_path, 'vk.json');
-var vk = require(vk_json);
-
-var tmp = []
-for (var i = 0; i < vk.IC.length; i++) {
-    tmp = [].concat(tmp, vk.IC[i])
-}
-vk.IC = tmp;
+//var setup_path = process.env.ZETH_TRUSTED_SETUP_DIR;
+//var vk_json = path.join(setup_path, 'vk.json');
+//var vk = require(vk_json);
+//
+//var tmp = []
+//for (var i = 0; i < vk.IC.length; i++) {
+//    tmp = [].concat(tmp, vk.IC[i])
+//}
+//vk.IC = tmp;
 
 const MerkleTreeSha256 = artifacts.require("./MerkleTreeSha256.sol");
 const Miximus = artifacts.require("./Miximus.sol");
@@ -18,24 +18,26 @@ const Bytes_tests = artifacts.require("./Bytes_tests.sol");
 
 module.exports = (deployer) => {
   deployer.deploy(MerkleTreeSha256, 3);
-  deployer.deploy(
-    Verifier,
-    vk.a[0],
-    vk.a[1],
-    vk.b,
-    vk.c[0],
-    vk.c[1],
-    vk.g[0],
-    vk.g[1],
-    vk.gb1,
-    vk.gb2[0],
-    vk.gb2[1],
-    vk.z[0],
-    vk.z[1],
-    vk.IC
-  ).then(function () {
-    return deployer.deploy(Miximus, Verifier.address, 2, 4);
-  })
+//  deployer.deploy(
+//    Verifier,
+//    vk.a[0],
+//    vk.a[1],
+//    vk.b,
+//    vk.c[0],
+//    vk.c[1],
+//    vk.g[0],
+//    vk.g[1],
+//    vk.gb1,
+//    vk.gb2[0],
+//    vk.gb2[1],
+//    vk.z[0],
+//    vk.z[1],
+//    vk.IC
+//  ).then(function () {
+//    return deployer.deploy(Miximus, Verifier.address, 2, 4);
+//  })
+
+  deployer.deploy(Miximus, 4);
 
   deployer.deploy(Bytes);
   deployer.link(Bytes, Bytes_tests);
