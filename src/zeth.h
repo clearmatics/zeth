f6f7f0a419683570583d0f0bc8148b476f5c51a4
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-28 19:33:25 +0000

    Removed useless constant

--- src/zeth.h
+++ src/zeth.h
@@ -4,7 +4,6 @@
 #define ZETH_NUM_JS_INPUTS 2
 #define ZETH_NUM_JS_OUTPUTS 2
 #define ZETH_MERKLE_TREE_DEPTH 4
-#define ZETH_MERKLE_TREE_DEPTH_TESTING 3
 
 #define ZETH_V_SIZE 8 // 64 bits for the value
 #define ZETH_RHO_SIZE 32 // 256 bits for rho
