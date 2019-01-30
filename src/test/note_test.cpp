439d943ae8eb5338f976a2982d655d56381ea9a1
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-01-30 16:09:19 +0000

    Wrote test for the output_note_gadget

--- src/test/note_test.cpp
+++ src/test/note_test.cpp
@@ -188,6 +188,95 @@ TEST(TestNoteCircuits, TestInputNoteGadget) {
     ASSERT_TRUE(is_valid_witness);
 };
 
+TEST(TestNoteCircuits, TestOutputNoteGadget) {
+    libsnark::protoboard<FieldT> pb;
+    libsnark::pb_variable<FieldT> ZERO;
+    ZERO.allocate(pb);
+    pb.val(ZERO) = FieldT::zero();
+
+    std::ostream &stream = std::cout;
+
+    libff::enter_block("[BEGIN] Initialize the output coins' data (a_pk, cm, rho)", true);
+
+    // trap_r: 384 bits
+    // hex: 0x0F000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF
+    char* trap_r_str = "0F000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF";
+    bits384 trap_r_bits384 = get_bits384_from_vector(hexadecimal_str_to_binary_vector(trap_r_str));
+
+    // 64 bits for the value
+    // hex: 0x2F0000000000000F
+    char* value_str = "2F0000000000000F";
+    bits64 value_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_str));
+
+    // hex: 0xFFFF000000000000000000000000000000000000000000000000000000009009
+    char* rho_str = "FFFF000000000000000000000000000000000000000000000000000000009009";
+    bits256 rho_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(rho_str));
+
+    // hex: 0x8FFFC00000000000000000000000000000000000000000000000000000002402
+    char* rho_254_padded_with_01 = "8FFFC00000000000000000000000000000000000000000000000000000002402";
+
+    char* a_pk_str = "6461f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b";
+    bits256 a_pk_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_pk_str));
+
+    // Get the coin's commitment (COMM)
+    //
+    // inner_k = sha256(a_pk || rho)
+    char* inner_k_str = "c8064b9113f19c058aa0295a88fd79c096eb2b9553f95af0c9c7b322448e8446";
+    // outer_k = sha256(r || [inner_commitment]_128)
+    char* inner_k_str_first_128_bits = "c8064b9113f19c058aa0295a88fd79c0";
+    char* outer_k_str = "806e5c213a2f3d436273e924eb6311ac2db6c33624b28165b79c779e00fa2752";
+    // cm = sha256(outer_k || 0^192 || value_v)
+    char* value_front_padded_zeroes = "0000000000000000000000000000000000000000000000002F0000000000000F";
+    char* cm_str = "823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331";
+    bits256 cm_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str));
+
+    /*
+     *
+     * At that point we have a coin with data:
+     * - value = 0x2F0000000000000F
+     * - trap_r = 0x0F000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF
+     * - a_pk = 0x6461f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b
+     * - commitment_k = 0x806e5c213a2f3d436273e924eb6311ac2db6c33624b28165b79c779e00fa2752
+     * - commitment_cm = 0x823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331
+     * 
+    **/
+    libff::leave_block("[END] Initialize the output coins' data (a_pk, cm, rho)", true);
+
+    libff::enter_block("[BEGIN] Data conversion to generate a witness of the note gadget", true);
+
+    std::shared_ptr<libsnark::digest_variable<FieldT> > commitment;
+    commitment.reset(new libsnark::digest_variable<FieldT>(pb, HashT::get_digest_len(), "root_digest"));
+    commitment->generate_r1cs_constraints();
+    commitment->generate_r1cs_witness(libff::bit_vector(get_vector_from_bits256(cm_bits256)));
+    std::shared_ptr<output_note_gadget<FieldT>> output_note_g  = std::shared_ptr<output_note_gadget<FieldT>>(
+        new output_note_gadget<FieldT>(
+            pb,
+            ZERO,
+            commitment
+        )
+    );
+
+    // Create a note from the coin's data
+    ZethNote note(
+        a_pk_bits256, 
+        value_bits64, 
+        rho_bits256, 
+        trap_r_bits384, 
+        cm_bits256
+    );
+
+    output_note_g->generate_r1cs_constraints();
+    output_note_g->generate_r1cs_witness(
+        note
+    );
+    libff::leave_block("[END] Data conversion to generate a witness of the note gadget", true);
+
+    bool is_valid_witness = pb.is_satisfied();
+    std::cout << "************* SAT result: " << is_valid_witness <<  " ******************" << std::endl;
+
+    ASSERT_TRUE(is_valid_witness);
+};
+
 } // namespace
 
 int main(int argc, char **argv) {
