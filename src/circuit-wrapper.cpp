051f2a51b2cc80a05a46dd6f453162f0e126bf31
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-02-07 10:46:49 +0000

    Wrote first test for the prover

--- src/circuit-wrapper.cpp
+++ src/circuit-wrapper.cpp
@@ -30,14 +30,12 @@ public:
     typedef sha256_ethereum<FieldT> HashT; // We instantiate the HashT with sha256_ethereum
 
     protoboard<FieldT> pb;
-    boost::filesystem::path vkPath;
-    boost::filesystem::path pkPath;
+    boost::filesystem::path setupPath;
     std::shared_ptr<joinsplit_gadget<FieldT, HashT, NumInputs, NumOutputs>> joinsplit_g;
 
     CircuitWrapper(
-        const boost::filesystem::path vkPath, 
-        const boost::filesystem::path pkPath
-    ): vkPath(vkPath), pkPath(pkPath) {};
+        const boost::filesystem::path setupPath = ""
+    ): setupPath(setupPath) {};
 
     // Generate the trusted setup
     libsnark::r1cs_ppzksnark_keypair<ppT> generate_trusted_setup() {
@@ -50,59 +48,61 @@ public:
         libsnark::r1cs_ppzksnark_keypair<ppT> keypair = gen_trusted_setup<ppT>(pb);
 
         // Write the keys in a file
-        write_setup<ppT>(keypair, this->keysPath); // Take the default path
+        write_setup<ppT>(keypair, this->setupPath); // Take the default path
 
         return keypair;
     }
 
     // Generate a proof and returns an extended proof
     extended_proof<ppT> prove(
-        libff::bit_vector root_bits,
+        const bits256& root_bits,
         const std::array<JSInput, NumInputs>& inputs,
         const std::array<ZethNote, NumOutputs>& outputs,
-        uint64_t vpub,
+        bits64 vpub,
         libsnark::r1cs_ppzksnark_proving_key<ppT> proving_key
     ) {
-            // left hand side and right hand side of the joinsplit
-            uint64_t lhs_value = 0;
-            uint64_t rhs_value = vpub;
+        // left hand side and right hand side of the joinsplit
+        std::array<bool, 64> zero_array;
+        zero_array.fill(0);
+        bits64 lhs_value = zero_array;
+        bits64 rhs_value = vpub;
 
-            // Compute the sum on the left hand side of the joinsplit
-            for (size_t i = 0; i < NumInputs; i++) {
-                lhs_value += inputs[i].note.value();
-            }
-
-            // Compute the sum on the right hand side of the joinsplit
-            for (size_t i = 0; i < NumOutputs; i++) {
-                rhs_value += outputs[i].value();
-            }
-
-            // Make sure that the balance betweem rhs and lfh is respected
-            if (lhs_value != rhs_value) {
-                throw std::invalid_argument("invalid joinsplit balance");
-            }
-
-            libsnark::protoboard<FieldT> pb;
-
-            joinsplit_gadget<FieldT, HashT, NumInputs, NumOutputs> g(pb);
-            g.generate_r1cs_constraints();
-            g.generate_r1cs_witness(
-                root_bits,
-                inputs,
-                outputs,
-                vpub
-            );
-
-            bool is_valid_witness = pb.is_satisfied();
-            std::cout << "******* [DEBUG] Satisfiability result: " << is_valid_witness << " *******" << std::endl;
-
-            // Build a proof using the witness built above and the proving key generated during the trusted setup
-            extended_proof<ppT> ext_proof = gen_proof<ppT>(pb, proving_key);
-            // Write the extended proof in a file
-            ext_proof.write_extended_proof(); // Take the default path
-
-            return ext_proof;
+        // Compute the sum on the left hand side of the joinsplit
+        for (size_t i = 0; i < NumInputs; i++) {
+            lhs_value = binaryAddition<64>(lhs_value, inputs[i].note.value());
         }
+
+        // Compute the sum on the right hand side of the joinsplit
+        for (size_t i = 0; i < NumOutputs; i++) {
+            rhs_value = binaryAddition<64>(rhs_value, outputs[i].value());
+        }
+
+        // [SANITY CHECK] Make sure that the balance betweem rhs and lfh is respected
+        if (lhs_value != rhs_value) {
+            throw std::invalid_argument("invalid joinsplit balance");
+        }
+
+        libsnark::protoboard<FieldT> pb;
+
+        joinsplit_gadget<FieldT, HashT, NumInputs, NumOutputs> g(pb);
+        g.generate_r1cs_constraints();
+        g.generate_r1cs_witness(
+            root_bits,
+            inputs,
+            outputs,
+            vpub
+        );
+
+        bool is_valid_witness = pb.is_satisfied();
+        std::cout << "******* [DEBUG] Satisfiability result: " << is_valid_witness << " *******" << std::endl;
+
+        // Build a proof using the witness built above and the proving key generated during the trusted setup
+        extended_proof<ppT> ext_proof = gen_proof<ppT>(pb, proving_key);
+        // Write the extended proof in a file
+        ext_proof.write_extended_proof(); // Take the default path
+
+        return ext_proof;
+    }
 };
 
 } // libzeth
