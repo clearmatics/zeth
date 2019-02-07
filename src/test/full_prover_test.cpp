0c13856673e2dda6b68967f2c008d881005dd13b
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-02-07 16:31:15 +0000

    Updated tests for the prover

--- src/test/full_prover_test.cpp
+++ src/test/full_prover_test.cpp
@@ -32,18 +32,17 @@ void dump_bit_vector(std::ostream &out, const libff::bit_vector &v)
     out << v[v.size() - 1] << "}\n";
 }
 
-TEST(MainTest, ValidProof)
-{
+bool Test_ValidJS1In1Out(
+    CircuitWrapper<1, 1> &prover,
+    libsnark::r1cs_ppzksnark_keypair<ppT> keypair
+) {
     // --- General setup for the tests --- //
-    libff::print_header("Starting prover tests");
+    libff::print_header("Starting test (JS: 1 in, 1 out of same value, and no public value)");
 
     libff::enter_block("[START] General setup for the tests", true);
 
     // Create a prover for the tests
-    CircuitWrapper<1, 1> prover;
-
-    // Run the trusted setup once for all tests, and keep the keypair in memory for the duration of the tests
-    libsnark::r1cs_ppzksnark_keypair<ppT> keypair = prover.generate_trusted_setup();
+    ////CircuitWrapper<1, 1> prover;
 
     // Create a merkle tree to run our tests
     // Note: make_unique should be C++14 compliant, but here we use c++11, so we instantiate our unique_ptr manually
@@ -83,7 +82,9 @@ TEST(MainTest, ValidProof)
     char* inner_k_str_first_128_bits = "c8064b9113f19c058aa0295a88fd79c0";
     char* outer_k_str = "806e5c213a2f3d436273e924eb6311ac2db6c33624b28165b79c779e00fa2752";
     char* value_front_padded_zeroes = "0000000000000000000000000000000000000000000000002F0000000000000F";
-    char* cm_str = "823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331";
+    char* cm_str = "823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331"; 
+    // Note: We do not care about the commiment value, this is computed inside the circuit, this is not an input!!!
+    // TODO: Remove the commitment from the zeth note structure
 
     bits256 cm_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str));
 
@@ -178,6 +179,325 @@ TEST(MainTest, ValidProof)
 
     libff::leave_block("[END] Verify proof", true);
 
+    return res;
+}
+
+bool Test_InvalidJS1In1Out(
+    CircuitWrapper<1, 1> &prover,
+    libsnark::r1cs_ppzksnark_keypair<ppT> keypair
+) {
+    // --- General setup for the tests --- //
+    libff::print_header("Starting test (JS: 1 in, 1 out of DIFFERENT value");
+
+    libff::enter_block("[START] General setup for the tests", true);
+
+    // Create a merkle tree to run our tests
+    // Note: make_unique should be C++14 compliant, but here we use c++11, so we instantiate our unique_ptr manually
+    std::unique_ptr<merkle_tree<HashT>> test_merkle_tree = std::unique_ptr<libsnark::merkle_tree<HashT>>(
+        new libsnark::merkle_tree<HashT>(
+            ZETH_MERKLE_TREE_DEPTH,
+            HashT::get_digest_len()
+        )
+    );
+    std::ostream &stream = std::cout;
+    libff::leave_block("[END] General setup for the tests", true);
+
+    // --- Test 1: Generate a valid proof for commitment inserted at address 1 -- //
+    libff::enter_block("[BEGIN] Create JSInput", true);
+
+    char* trap_r_str = "0F000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF";
+    bits384 trap_r_bits384 = get_bits384_from_vector(hexadecimal_str_to_binary_vector(trap_r_str));
+
+    char* value_str = "2F0000000000000F";
+    bits64 value_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_str));
+
+    char* a_sk_str = "FF0000000000000000000000000000000000000000000000000000000000000F";
+    bits256 a_sk_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_sk_str));
+
+    char* rho_str = "FFFF000000000000000000000000000000000000000000000000000000009009";
+    bits256 rho_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(rho_str));
+
+    char* rho_254_padded_with_01 = "8FFFC00000000000000000000000000000000000000000000000000000002402";
+
+    char* a_pk_str = "6461f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b";
+    bits256 a_pk_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_pk_str));
+
+    char* nf_str = "69f12603c2cfb2acf6f80a8f72cbdeb4417a6b8c7290e793c4d22830c4b35c5f";
+    bits256 nf_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(nf_str));
+
+    char* inner_k_str = "c8064b9113f19c058aa0295a88fd79c096eb2b9553f95af0c9c7b322448e8446";
+    char* inner_k_str_first_128_bits = "c8064b9113f19c058aa0295a88fd79c0";
+    char* outer_k_str = "806e5c213a2f3d436273e924eb6311ac2db6c33624b28165b79c779e00fa2752";
+    char* value_front_padded_zeroes = "0000000000000000000000000000000000000000000000002F0000000000000F";
+    char* cm_str = "823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331";
+
+    bits256 cm_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str));
+
+    libff::bit_vector address_bits = {1, 0, 0, 0}; // 4 being the value of ZETH_MERKLE_TREE_DEPTH
+    const size_t address_commitment = 1;
+    test_merkle_tree->set_value(address_commitment, libff::bit_vector(get_vector_from_bits256(cm_bits256)));
+    // Get the root of the new/non-empty tree (after insertion)
+    libff::bit_vector updated_root_value = test_merkle_tree->get_root();
+
+    // Get the merkle path
+    std::vector<libsnark::merkle_authentication_node> path = test_merkle_tree->get_path(address_commitment);
+
+    ZethNote note_input(
+        a_pk_bits256,
+        value_bits64,
+        rho_bits256,
+        trap_r_bits384,
+        cm_bits256
+    );
+
+    bitsAddr binary_address = get_bitsAddr_from_vector(address_bits);
+    JSInput input(
+        path,
+        address_commitment,
+        binary_address,
+        note_input,
+        a_sk_bits256,
+        nf_bits256
+    );
+
+    std::array<JSInput, 1> inputs;
+    inputs.fill(input);
+
+    libff::leave_block("[END] Create JSInput", true);
+
+    libff::enter_block("[BEGIN] Create JSOutput/ZethNote", true);
+
+    char* value_str_out = "2F000000000000FF";
+    bits64 value_out_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_str_out));
+
+    char* a_pk_str_out = "7777f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b";
+    bits256 a_pk_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_pk_str_out));
+
+    char* rho_out_str = "1111000000000000000000000000000000000000000000000000000000009777";
+    bits256 rho_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(rho_out_str));
+
+    char* trap_r_out_str = "11000000000000990000000000000099000000000000007700000000000000FF00000000000000FF0000000000000777";
+    bits384 trap_r_out_bits384 = get_bits384_from_vector(hexadecimal_str_to_binary_vector(trap_r_out_str));
+
+    char* inner_k_str_out = "59f8b7f7bcbc492e4d9f96517e75ad1fc675dfb600bfae3ee53b79adb8c392fd";
+    char* inner_k_str_first_128_bits_out = "59f8b7f7bcbc492e4d9f96517e75ad1f";
+    char* outer_k_str_out = "13a067be67971e25bf9e46fb93bb98b91619aba6c383cfcc803d70527a6ba04d";
+    char* value_front_padded_zeroes_out = "0000000000000000000000000000000000000000000000002F0000000000000F";
+    char* cm_str_out = "7777777725a5844abdae214896d13c376a9aea4bfe4cafbb5572822feb39b8ea"; // wrong hash
+    bits256 cm_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str_out));
+
+    ZethNote note_output(
+        a_pk_out_bits256,
+        value_out_bits64,
+        rho_out_bits256,
+        trap_r_out_bits384,
+        cm_out_bits256
+    );
+
+    char* value_pub_str_out = "0000000000000000";
+    bits64 value_pub_out_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_pub_str_out));
+
+    std::array<ZethNote, 1> outputs;
+    outputs.fill(note_output);
+
+    libff::leave_block("[END] Create JSOutput/ZethNote", true);
+
+    libff::enter_block("[BEGIN] Generate proof", true);
+
+    extended_proof<ppT> ext_proof = prover.prove(
+        get_bits256_from_vector(updated_root_value),
+        inputs,
+        outputs,
+        value_pub_out_bits64,
+        keypair.pk
+    );
+
+    libff::leave_block("[END] Generate proof", true);
+
+    libff::enter_block("[BEGIN] Verify proof", true);
+
+    // Get the verification key
+    libsnark::r1cs_ppzksnark_verification_key<ppT> vk = keypair.vk;
+
+    libff::print_header("=== R1CS ppzkSNARK Verifier ===");
+    libsnark::r1cs_ppzksnark_primary_input<ppT> primary_inputs = ext_proof.get_primary_input();
+    bool res = libsnark::r1cs_ppzksnark_verifier_strong_IC<ppT>(vk, primary_inputs, ext_proof.get_proof());
+
+    display_primary_input<ppT>(primary_inputs);
+
+    libff::leave_block("[END] Verify proof", true);
+
+    return res;
+}
+
+bool Test_ValidJS1In1Out1PubVal(
+    CircuitWrapper<1, 1> &prover,
+    libsnark::r1cs_ppzksnark_keypair<ppT> keypair
+) {
+    // --- General setup for the tests --- //
+    libff::print_header("Starting test (JS: 1 in, 1 out and 1 out pub val)");
+
+    libff::enter_block("[START] General setup for the tests", true);
+
+    // Create a merkle tree to run our tests
+    // Note: make_unique should be C++14 compliant, but here we use c++11, so we instantiate our unique_ptr manually
+    std::unique_ptr<merkle_tree<HashT>> test_merkle_tree = std::unique_ptr<libsnark::merkle_tree<HashT>>(
+        new libsnark::merkle_tree<HashT>(
+            ZETH_MERKLE_TREE_DEPTH,
+            HashT::get_digest_len()
+        )
+    );
+    std::ostream &stream = std::cout;
+    libff::leave_block("[END] General setup for the tests", true);
+
+    // --- Test 1: Generate a valid proof for commitment inserted at address 1 -- //
+    libff::enter_block("[BEGIN] Create JSInput", true);
+
+    char* trap_r_str = "0F000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF00000000000000FF";
+    bits384 trap_r_bits384 = get_bits384_from_vector(hexadecimal_str_to_binary_vector(trap_r_str));
+
+    char* value_str = "2F0000000000000F";
+    bits64 value_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_str));
+
+    char* a_sk_str = "FF0000000000000000000000000000000000000000000000000000000000000F";
+    bits256 a_sk_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_sk_str));
+
+    char* rho_str = "FFFF000000000000000000000000000000000000000000000000000000009009";
+    bits256 rho_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(rho_str));
+
+    char* rho_254_padded_with_01 = "8FFFC00000000000000000000000000000000000000000000000000000002402";
+
+    char* a_pk_str = "6461f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b";
+    bits256 a_pk_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_pk_str));
+
+    char* nf_str = "69f12603c2cfb2acf6f80a8f72cbdeb4417a6b8c7290e793c4d22830c4b35c5f";
+    bits256 nf_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(nf_str));
+
+    char* inner_k_str = "c8064b9113f19c058aa0295a88fd79c096eb2b9553f95af0c9c7b322448e8446";
+    char* inner_k_str_first_128_bits = "c8064b9113f19c058aa0295a88fd79c0";
+    char* outer_k_str = "806e5c213a2f3d436273e924eb6311ac2db6c33624b28165b79c779e00fa2752";
+    char* value_front_padded_zeroes = "0000000000000000000000000000000000000000000000002F0000000000000F";
+    char* cm_str = "823d19485c94f74b4739ba7d17e4b434693086a996fa2e8d1438a91b1c220331";
+
+    bits256 cm_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str));
+
+    libff::bit_vector address_bits = {1, 0, 0, 0}; // 4 being the value of ZETH_MERKLE_TREE_DEPTH
+    const size_t address_commitment = 1;
+    test_merkle_tree->set_value(address_commitment, libff::bit_vector(get_vector_from_bits256(cm_bits256)));
+    // Get the root of the new/non-empty tree (after insertion)
+    libff::bit_vector updated_root_value = test_merkle_tree->get_root();
+
+    // Get the merkle path
+    std::vector<libsnark::merkle_authentication_node> path = test_merkle_tree->get_path(address_commitment);
+
+    ZethNote note_input(
+        a_pk_bits256,
+        value_bits64,
+        rho_bits256,
+        trap_r_bits384,
+        cm_bits256
+    );
+
+    bitsAddr binary_address = get_bitsAddr_from_vector(address_bits);
+    JSInput input(
+        path,
+        address_commitment,
+        binary_address,
+        note_input,
+        a_sk_bits256,
+        nf_bits256
+    );
+
+    std::array<JSInput, 1> inputs;
+    inputs.fill(input);
+
+    libff::leave_block("[END] Create JSInput", true);
+
+    libff::enter_block("[BEGIN] Create JSOutput/ZethNote", true);
+
+    char* value_str_out = "1800000000000008";
+    bits64 value_out_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_str_out));
+
+    char* a_pk_str_out = "7777f753bfe21ba2219ced74875b8dbd8c114c3c79d7e41306dd82118de1895b";
+    bits256 a_pk_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(a_pk_str_out));
+
+    char* rho_out_str = "1111000000000000000000000000000000000000000000000000000000009777";
+    bits256 rho_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(rho_out_str));
+
+    char* trap_r_out_str = "11000000000000990000000000000099000000000000007700000000000000FF00000000000000FF0000000000000777";
+    bits384 trap_r_out_bits384 = get_bits384_from_vector(hexadecimal_str_to_binary_vector(trap_r_out_str));
+
+    char* inner_k_str_out = "59f8b7f7bcbc492e4d9f96517e75ad1fc675dfb600bfae3ee53b79adb8c392fd";
+    char* inner_k_str_first_128_bits_out = "59f8b7f7bcbc492e4d9f96517e75ad1f";
+    char* outer_k_str_out = "13a067be67971e25bf9e46fb93bb98b91619aba6c383cfcc803d70527a6ba04d";
+    char* value_front_padded_zeroes_out = "0000000000000000000000000000000000000000000000002F0000000000000F";
+    char* cm_str_out = "7777777725a5844abdae214896d13c376a9aea4bfe4cafbb5572822feb39b8ea"; // wrong hash
+    bits256 cm_out_bits256 = get_bits256_from_vector(hexadecimal_digest_to_binary_vector(cm_str_out));
+
+    ZethNote note_output(
+        a_pk_out_bits256,
+        value_out_bits64,
+        rho_out_bits256,
+        trap_r_out_bits384,
+        cm_out_bits256
+    );
+
+    char* value_pub_str_out = "1700000000000007";
+    bits64 value_pub_out_bits64 = get_bits64_from_vector(hexadecimal_str_to_binary_vector(value_pub_str_out));
+
+    std::array<ZethNote, 1> outputs;
+    outputs.fill(note_output);
+
+    libff::leave_block("[END] Create JSOutput/ZethNote", true);
+
+    libff::enter_block("[BEGIN] Generate proof", true);
+
+    extended_proof<ppT> ext_proof = prover.prove(
+        get_bits256_from_vector(updated_root_value),
+        inputs,
+        outputs,
+        value_pub_out_bits64,
+        keypair.pk
+    );
+
+    libff::leave_block("[END] Generate proof", true);
+
+    libff::enter_block("[BEGIN] Verify proof", true);
+
+    // Get the verification key
+    libsnark::r1cs_ppzksnark_verification_key<ppT> vk = keypair.vk;
+
+    libff::print_header("=== R1CS ppzkSNARK Verifier ===");
+    libsnark::r1cs_ppzksnark_primary_input<ppT> primary_inputs = ext_proof.get_primary_input();
+    bool res = libsnark::r1cs_ppzksnark_verifier_strong_IC<ppT>(vk, primary_inputs, ext_proof.get_proof());
+
+    display_primary_input<ppT>(primary_inputs);
+
+    libff::leave_block("[END] Verify proof", true);
+
+    return res;
+}
+
+TEST(MainTest, ProofGenAndVerif) {
+    // Run the trusted setup once for all tests, and keep the keypair in memory for the duration of the tests
+    CircuitWrapper<1, 1> prover;
+    libsnark::r1cs_ppzksnark_keypair<ppT> keypair = prover.generate_trusted_setup();
+    bool res = false;
+
+    res = Test_ValidJS1In1Out(prover, keypair);
+    std::cout << "Test 1" << std::endl;
+    ASSERT_TRUE(res);
+
+    try {
+        res = Test_InvalidJS1In1Out(prover, keypair);
+        std::cout << "Test 2" << std::endl;
+        ASSERT_FALSE(res);
+    } catch (const std::invalid_argument& ia) {
+        std::cerr << "Expected invalid argument (Joinsplit balance constraint violated: Error thrown and proof generation aborted): " << ia.what() << '\n';
+    }
+
+    res = Test_ValidJS1In1Out1PubVal(prover, keypair);
+    std::cout << "Test 3" << std::endl;
     ASSERT_TRUE(res);
 }
 
