463cc3b960a5ecdf6088caabfccb47f64e62f6e5
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-02-15 15:06:11 +0000

    Removed useless files in the api and added script to fetch vk

--- setup_env.sh
+++ setup_env.sh
@@ -5,6 +5,7 @@ export ZETH_KEYSTORE=$ZETH/keystore
 export ZETH_COINSTORE=$ZETH/coinstore
 export ZETH_TRUSTED_SETUP_DIR=$ZETH/trusted_setup
 export ZETH_DEBUG_DIR=$ZETH/debug
+export ZETH_API_DIR=$ZETH/api
 
 # Add the zeth executable in the PATH
 export PATH=$ZETH/build/src:$PATH
