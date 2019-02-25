fd0edd3b2ecb86026845c20ecae3c1d5ee820c59
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-02-25 14:36:52 +0000

    Added encryption and decryption functions

--- pyClient/testStub.py
+++ pyClient/testStub.py
@@ -3,6 +3,10 @@ import logging
 import json
 import os
 import time
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import zlib
+import base64
 
 from web3 import Web3, HTTPProvider, IPCProvider, WebsocketProvider
 
@@ -18,6 +22,49 @@ import zethMock
 
 w3 = Web3(HTTPProvider("http://localhost:8545"));
 test_grpc_endpoint = 'localhost:50051'
+
+def encrypt(message, public_key):
+    rsa_key = RSA.importKey(public_key)
+    rsa_key = PKCS1_OAEP.new(rsa_key)
+
+    blob = zlib.compress(message.encode())
+
+    # Refer to: https://pycryptodome.readthedocs.io/en/latest/src/cipher/oaep.html#Crypto.Cipher.PKCS1_OAEP.PKCS1OAEP_Cipher.encrypt
+    # to define the chunk size
+    chunk_size = 62 # (128 - 2 - 2*32) since we use RSA modulus of 1024 bits (128 bytes) in the keystore
+    offset = 0
+    end_loop = False
+    encrypted =  "".encode()
+
+    while not end_loop:
+        chunk = blob[offset:offset + chunk_size]
+
+        # Padding
+        if len(chunk) % chunk_size != 0:
+            end_loop = True
+            chunk += " ".encode() * (chunk_size - len(chunk))
+
+        encrypted += rsa_key.encrypt(chunk)
+        offset += chunk_size
+
+    return base64.b64encode(encrypted)
+
+def decrypt(encrypted_blob, private_key):
+    rsakey = RSA.importKey(private_key)
+    rsakey = PKCS1_OAEP.new(rsakey)
+
+    encrypted_blob = base64.b64decode(encrypted_blob)
+
+    chunk_size = 128 # Size of the modulus we use here
+    offset = 0
+    decrypted = "".encode()
+
+    # Loop over our chunks
+    while offset < len(encrypted_blob):
+        chunk = encrypted_blob[offset: offset + chunk_size]
+        decrypted += rsakey.decrypt(chunk)
+        offset += chunk_size
+    return str(zlib.decompress(decrypted), 'utf-8')
 
 def get_proof_bob_deposit(keystore, mk_root):
     print("Bob deposits 4 ETH for himself and splits them into note1: 2ETH, note2: 2ETH")
@@ -78,10 +125,12 @@ def bob_deposit(mixer_instance, mk_root, bob_eth_address):
     (output_note1, output_note2, proof_json) = get_proof_bob_deposit(keystore, mk_root)
     output_note1_str = json.dumps(zethGRPC.parseZethNote(output_note1))
     output_note2_str = json.dumps(zethGRPC.parseZethNote(output_note2))
+    ciphertext1 = encrypt(output_note1_str, keystore["Bob"]["AddrPk"]["ek"])
+    ciphertext2 = encrypt(output_note2_str, keystore["Bob"]["AddrPk"]["ek"])
     return zethContracts.mix(
         mixer_instance,
-        output_note1_str,
-        output_note2_str,
+        ciphertext1,
+        ciphertext2,
         proof_json,
         bob_eth_address,
         w3.toWei(4, 'ether'),
@@ -177,3 +226,12 @@ if __name__ == '__main__':
     print(ciphertext1)
     print("ciphertext2: ")
     print(ciphertext2)
+    print("plaintext1: ")
+    print(decrypt(ciphertext1, keystore["Bob"]["AddrSk"]["dk"]))
+    print("plaintext2: ")
+    print(decrypt(ciphertext1, keystore["Bob"]["AddrSk"]["dk"]))
+    try:
+        alice_plaintext = decrypt(ciphertext1, keystore["Alice"]["AddrSk"]["dk"])
+        print("Alice plaintext: " +  alice_plaintext)
+    except:
+        print("Alice tried to decrypt a ciphertext of Bob but failed")
