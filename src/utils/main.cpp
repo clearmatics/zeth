b4f245b0f4a2acc680e14eb238b5ec577e0daf6c
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-02-28 18:53:06 +0000

    Removed old cli and utils folder

--- src/utils/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "randomness_generator.hpp"
-
-int main() {
-    std::string rnd_str = generate_random_number();
-    std::cout << "Random string: " << rnd_str << std::endl;
-
-    return 0;
-}
