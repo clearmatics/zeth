83a7ff43af128558e26d050b3a6411fb2d96339f
riemann89 <giu.giffone@tiscali.it>
2019-04-02 12:43:13 +0100

    add snark specific functions

--- src/snarks/pghr13/pghr13_computation.hpp
+++ src/snarks/pghr13/pghr13_computation.hpp
@@ -1,6 +1,7 @@
 #ifndef __ZETH_PGHR13_COMPUTATION_HPP__
 #define __ZETH_PGHR13_COMPUTATION_HPP__
 
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
 #include "libsnark_helpers/extended_proof.hpp"
 
 // We instantiate the ppT (public parameters Template with the public paramaters of the curve we use (alt_bn128))
@@ -15,6 +16,16 @@ extended_proof<ppT> gen_proof(libsnark::protoboard<libff::Fr<ppT> > pb, provingK
 template<typename ppT>
 keyPairT<ppT> gen_trusted_setup(libsnark::protoboard<libff::Fr<ppT> > pb);
 
+// other functions
+template<typename ppT> void exportVerificationKey(libsnark::r1cs_ppzksnark_keypair<ppT> keypair);
+
+template<typename ppT> void displayProof(libsnark::r1cs_ppzksnark_proof<ppT> proof);
+
+template<typename ppT> void verificationKeyToJson(libsnark::r1cs_ppzksnark_keypair<ppT> keypair, boost::filesystem::path path = "");
+
+template<typename ppT> void proofToJson(libsnark::r1cs_ppzksnark_proof<ppT> proof, boost::filesystem::path path);
+
+template<typename ppT> void proofAndInputToJson(libsnark::r1cs_ppzksnark_proof<ppT> proof, libsnark::r1cs_ppzksnark_primary_input<ppT> input, boost::filesystem::path path = "");
 
 } // libzeth
 #include "pghr13_computation.tcc"
