1603f837aeb28c774569133591b9240644273dfe
riemann89 <giu.giffone@tiscali.it>
2019-04-03 10:38:31 +0100

    changes for bctv test

--- pyClient/zethGRPC.py
+++ pyClient/zethGRPC.py
@@ -8,6 +8,12 @@ from eth_abi import encode_single, encode_abi
 
 # Access the gRPC service and the proto messages
 import grpc
+import util_pb2
+import util_pb2_grpc
+import bctv14_messages_pb2
+import bctv14_messages_pb2_grpc
+import groth16_messages_pb2
+import groth16_messages_pb2_grpc
 import prover_pb2
 import prover_pb2_grpc
 
@@ -45,7 +51,7 @@ def noteRandomness():
 
 # We follow the formatting of the proto file
 def createZethNote(randomness, recipientApk, value):
-    note = prover_pb2.ZethNote(
+    note = util_pb2.ZethNote(
         aPK=recipientApk,
         value=value,
         rho=randomness["rho"],
@@ -63,7 +69,7 @@ def parseZethNote(zethNoteGRPCObj):
     return noteJSON
 
 def zethNoteObjFromParsed(parsedZethNote):
-    note = prover_pb2.ZethNote(
+    note = util_pb2.ZethNote(
         aPK=parsedZethNote["aPK"],
         value=parsedZethNote["value"],
         rho=parsedZethNote["rho"],
@@ -133,7 +139,7 @@ def generateApkAskKeypair():
 
 
 def createJSInput(merklePath, address, note, ask, nullifier):
-    jsInput = prover_pb2.JSInput(
+    jsInput = util_pb2.JSInput(
         merkleNode=merklePath,
         address=address,
         note=note,
@@ -156,14 +162,14 @@ def make_empty_message():
 
 def parseVerificationKey(vkObj):
     vkJSON = {}
-    vkJSON["a"] = parseHexadecimalPointBaseGroup2Affine(vkObj.a)
-    vkJSON["b"] = parseHexadecimalPointBaseGroup1Affine(vkObj.b)
-    vkJSON["c"] = parseHexadecimalPointBaseGroup2Affine(vkObj.c)
-    vkJSON["g"] = parseHexadecimalPointBaseGroup2Affine(vkObj.g)
-    vkJSON["gb1"] = parseHexadecimalPointBaseGroup1Affine(vkObj.gb1)
-    vkJSON["gb2"] = parseHexadecimalPointBaseGroup2Affine(vkObj.gb2)
-    vkJSON["z"] = parseHexadecimalPointBaseGroup2Affine(vkObj.z)
-    vkJSON["IC"] = json.loads(vkObj.IC)
+    vkJSON["a"] = parseHexadecimalPointBaseGroup2Affine(vkObj.r1csPpzksnarkVerificationKey.a)
+    vkJSON["b"] = parseHexadecimalPointBaseGroup1Affine(vkObj.r1csPpzksnarkVerificationKey.b)
+    vkJSON["c"] = parseHexadecimalPointBaseGroup2Affine(vkObj.r1csPpzksnarkVerificationKey.c)
+    vkJSON["g"] = parseHexadecimalPointBaseGroup2Affine(vkObj.r1csPpzksnarkVerificationKey.g)
+    vkJSON["gb1"] = parseHexadecimalPointBaseGroup1Affine(vkObj.r1csPpzksnarkVerificationKey.gb1)
+    vkJSON["gb2"] = parseHexadecimalPointBaseGroup2Affine(vkObj.r1csPpzksnarkVerificationKey.gb2)
+    vkJSON["z"] = parseHexadecimalPointBaseGroup2Affine(vkObj.r1csPpzksnarkVerificationKey.z)
+    vkJSON["IC"] = json.loads(vkObj.r1csPpzksnarkVerificationKey.IC)
     return vkJSON
 
 # Writes the verification key (object) in a json file
@@ -185,15 +191,15 @@ def makeProofInputs(root, jsInputs, jsOutputs, inPubValue, outPubValue):
 
 def parseProof(proofObj):
     proofJSON = {}
-    proofJSON["a"] = parseHexadecimalPointBaseGroup1Affine(proofObj.a)
-    proofJSON["a_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.aP)
-    proofJSON["b"] = parseHexadecimalPointBaseGroup2Affine(proofObj.b)
-    proofJSON["b_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.bP)
-    proofJSON["c"] = parseHexadecimalPointBaseGroup1Affine(proofObj.c)
-    proofJSON["c_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.cP)
-    proofJSON["h"] = parseHexadecimalPointBaseGroup1Affine(proofObj.h)
-    proofJSON["k"] = parseHexadecimalPointBaseGroup1Affine(proofObj.k)
-    proofJSON["inputs"] = json.loads(proofObj.inputs)
+    proofJSON["a"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.a)
+    proofJSON["a_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.aP)
+    proofJSON["b"] = parseHexadecimalPointBaseGroup2Affine(proofObj.r1csPpzksnarkExtendedProof.b)
+    proofJSON["b_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.bP)
+    proofJSON["c"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.c)
+    proofJSON["c_p"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.cP)
+    proofJSON["h"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.h)
+    proofJSON["k"] = parseHexadecimalPointBaseGroup1Affine(proofObj.r1csPpzksnarkExtendedProof.k)
+    proofJSON["inputs"] = json.loads(proofObj.r1csPpzksnarkExtendedProof.inputs)
     return proofJSON
 
 def get_proof_joinsplit_2by2(
