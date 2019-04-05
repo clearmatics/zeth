cb40c596e5269263dd8172a971626f4e8250999a
riemann89 <giu.giffone@tiscali.it>
2019-04-05 13:01:44 +0100

    fix snarks function import

--- src/snarks.hpp
+++ src/snarks.hpp
@@ -4,7 +4,7 @@
 /************************ Pick a Snark ****************************/
 
 #ifdef SNARK_R1CS_PPZKSNARK
-#define LIBZETH_DEFAULT_SNARK_DEFINED
+#define LIBZETH_SNARK_DEFINED
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
 
 namespace libzeth {
@@ -22,7 +22,7 @@ using keyPairT = libsnark::r1cs_ppzksnark_keypair <ppT>;
 #endif
 
 #ifdef SNARK_R1CS_GG_PPZKSNARK
-#define LIBZETH_DEFAULT_SNARK_DEFINED
+#define LIBZETH_SNARK_DEFINED
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_gg_ppzksnark.hpp>
 
 namespace libzeth {
@@ -38,7 +38,7 @@ using keyPairT = libsnark::r1cs_gg_ppzksnark_keypair <ppT>;
 
 #endif
 
-#ifndef LIBZETH_DEFAULT_SNARK_DEFINED
+#ifndef LIBZETH_SNARK_DEFINED
 #error You must define one of the SNARK_* symbols indicated into the CMakelists.txt file.
 #endif
 
