e4baa1e5c3451269b981bc8ba306b25cd4996a6d
riemann89 <giu.giffone@tiscali.it>
2019-04-08 12:17:22 +0100

    split compute functions from helpers

--- src/snarks/pghr13/pghr13_computation.hpp
+++ src/snarks/pghr13/pghr13_computation.hpp
@@ -10,28 +10,12 @@ typedef libff::default_ec_pp ppT; // We use the public parameters of the alt_bn_
 
 namespace libzeth {
 
-    // circuit-wrapper functions //TODO: here I can specify the type
+    // circuit-wrapper functions
     template<typename ppT>
     libsnark::r1cs_ppzksnark_proof<ppT> gen_proof(libsnark::protoboard<libff::Fr<ppT> > pb, libsnark::r1cs_ppzksnark_proving_key<ppT> proving_key);
 
     template<typename ppT>
-    keyPairT<ppT> gen_trusted_setup(libsnark::protoboard<libff::Fr<ppT> > pb);
-
-    // other functions
-    template<typename ppT> 
-    void exportVerificationKey(libsnark::r1cs_ppzksnark_keypair<ppT> keypair);
-
-    template<typename ppT> 
-    void displayProof(libsnark::r1cs_ppzksnark_proof<ppT> proof);
-
-    template<typename ppT>
-    void verificationKeyToJson(libsnark::r1cs_ppzksnark_keypair<ppT> keypair, boost::filesystem::path path = "");
-
-    template<typename ppT> 
-    void proofAndInputToJson(libsnark::r1cs_ppzksnark_proof<ppT> proof, libsnark::r1cs_ppzksnark_primary_input<ppT> input, boost::filesystem::path path = "");
-
-    template<typename ppT> 
-    void proofToJson(libsnark::r1cs_ppzksnark_proof<ppT> proof, boost::filesystem::path path);
+    libsnark::r1cs_ppzksnark_keypair<ppT> gen_trusted_setup(libsnark::protoboard<libff::Fr<ppT> > pb);
 
 } // libzeth
 #include "pghr13_computation.tcc"
