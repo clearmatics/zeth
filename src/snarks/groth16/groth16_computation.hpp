95b630cecafb8e0ea9213fc698e46fdf0c002f96
riemann89 <giu.giffone@tiscali.it>
2019-04-08 14:36:36 +0100

    groth16 computation implemented

---
+++ src/snarks/groth16/groth16_computation.hpp
@@ -0,0 +1,23 @@
+#ifndef __ZETH_GROTH16_COMPUTATION_HPP__
+#define __ZETH_GROTH16_COMPUTATION_HPP__
+
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_gg_ppzksnark.hpp>
+#include <libsnark/gadgetlib1/gadget.hpp>
+#include "libsnark_helpers/extended_proof.hpp"
+#include "libsnark_helpers/debug_helpers.hpp"
+
+typedef libff::default_ec_pp ppT; // We use the public parameters of the alt_bn_128 curve to do our operations
+
+namespace libzeth {
+
+    // circuit-wrapper functions //TODO: here I can specify the type
+    template<typename ppT>
+    extended_proof<ppT> gen_proof(libsnark::protoboard<libff::Fr<ppT> > pb, r1cs_gg_ppzksnark_proving_key<ppT> proving_key);
+
+    template<typename ppT>
+    libsnark::r1cs_gg_ppzksnark_keypair<ppT> gen_trusted_setup (libsnark::protoboard<libff::Fr<ppT> > pb);
+
+} // libzeth
+#include "groth16_computation.tcc"
+
+#endif // __ZETH_GROTH16_COMPUTATION_HPP__
