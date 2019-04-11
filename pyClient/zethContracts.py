64d0c72720279793bfc2f46507244911407b4bab
riemann89 <giu.giffone@tiscali.it>
2019-04-12 00:43:36 +0100

    add deploy and mix for groth16

--- pyClient/zethContracts.py
+++ pyClient/zethContracts.py
@@ -76,6 +76,48 @@ def deploy_pghr13_contracts(mk_tree_depth, verifier_interface, mixer_interface, 
     initialRoot = w3.toHex(event_logs_logMerkleRoot[0].args.root)
     return(mixer, initialRoot[2:])
 
+def deploy_groth16():
+    setup_dir = os.environ['ZETH_TRUSTED_SETUP_DIR']
+    vk_json = os.path.join(setup_dir, "vk.json")
+    with open(vk_json) as json_data:
+        vk = json.load(json_data)
+
+    # Deploy the verifier contract with the good verification key
+    verifier = w3.eth.contract(abi=verifier_interface['abi'], bytecode=verifier_interface['bin'])
+    tx_hash = verifier.constructor(
+        Alpha=zethGRPC.hex2int(vk["alpha_g1"]),
+        Beta1=zethGRPC.hex2int(vk["beta_g2"][0]),
+        Beta2=zethGRPC.hex2int(vk["beta_g2"][1]),
+        Gamma1=zethGRPC.hex2int(vk["gamma_g2"][0]),
+        Gamma2=zethGRPC.hex2int(vk["gamma_g2"][1]),
+        Delta1=zethGRPC.hex2int(vk["delta_g2"][0]),
+        Delta2=zethGRPC.hex2int(vk["delta_g2"][1]),
+        gamma_ABC_elements=zethGRPC.hex2int(sum(vk["gamma_ABC"], []))
+    ).transact({'from': deployer_address, 'gas': deployment_gas})
+    # Get tx receipt to get Verifier contract address
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash, 10000)
+    verifier_address = tx_receipt['contractAddress']
+    # Deploy the Mixer contract once the Verifier is successfully deployed
+    mixer = w3.eth.contract(abi=mixer_interface['abi'], bytecode=mixer_interface['bin'])
+    tx_hash = mixer.constructor(
+        _zksnark_verify=verifier_address,
+        depth=mk_tree_depth,
+        token=token_address
+    ).transact({'from': deployer_address, 'gas': deployment_gas})
+    # Get tx receipt to get Mixer contract address
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash, 10000)
+    mixer_address = tx_receipt['contractAddress']
+    # Get the mixer contract instance
+    mixer = w3.eth.contract(
+        address=mixer_address,
+        abi=mixer_interface['abi']
+    )
+    # Get the initial merkle root to proceed to the first payments
+    ef_logMerkleRoot = mixer.eventFilter("LogMerkleRoot", {'fromBlock': 'latest'})
+    event_logs_logMerkleRoot = ef_logMerkleRoot.get_all_entries()
+    initialRoot = w3.toHex(event_logs_logMerkleRoot[0].args.root)
+    return(mixer, initialRoot[2:])
+
 # Call to the mixer's mix function to do zero knowledge payments
 def mix_pghr13(
         mixer_instance,
@@ -118,3 +160,44 @@ def mix_pghr13(
     ciphertext1 = event_logs_logSecretCiphers[0].args.ciphertext
     ciphertext2 = event_logs_logSecretCiphers[1].args.ciphertext
     return (commitment_address1, commitment_address2, new_mk_root, ciphertext1, ciphertext2)
+<<<<<<< HEAD
+=======
+
+def mix_groth16(
+    mixer_instance,
+    ciphertext1,
+    ciphertext2,
+    parsed_proof,
+    sender_address,
+    wei_pub_value,
+    call_gas
+    ):
+    tx_hash = mixer_instance.functions.mix(
+        ciphertext1,
+        ciphertext2,
+        zethGRPC.hex2int(parsed_proof["a"]),
+        [zethGRPC.hex2int(parsed_proof["b"][0]), zethGRPC.hex2int(parsed_proof["b"][1])],
+        zethGRPC.hex2int(parsed_proof["b_p"]),
+        zethGRPC.hex2int(parsed_proof["c"]),
+        zethGRPC.hex2int(parsed_proof["inputs"])
+    ).transact({'from': sender_address, 'value': wei_pub_value, 'gas': call_gas})
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash, 10000)
+    # Get the logs data associated with this mixing
+    #
+    # Gather the addresses of the appended commitments
+    event_filter_logAddress = mixer_instance.eventFilter("LogAddress", {'fromBlock': 'latest'})
+    event_logs_logAddress = event_filter_logAddress.get_all_entries()
+    # Get the new merkle root
+    event_filter_logMerkleRoot = mixer_instance.eventFilter("LogMerkleRoot", {'fromBlock': 'latest'})
+    event_logs_logMerkleRoot = event_filter_logMerkleRoot.get_all_entries()
+    # Get the ciphertexts
+    event_filter_logSecretCiphers = mixer_instance.eventFilter("LogSecretCiphers", {'fromBlock': 'latest'})
+    event_logs_logSecretCiphers = event_filter_logSecretCiphers.get_all_entries()
+
+    commitment_address1 = event_logs_logAddress[0].args.commAddr
+    commitment_address2 = event_logs_logAddress[1].args.commAddr
+    new_mk_root = w3.toHex(event_logs_logMerkleRoot[0].args.root)[2:] # [2:] to strip the '0x' prefix
+    ciphertext1 = event_logs_logSecretCiphers[0].args.ciphertext
+    ciphertext2 = event_logs_logSecretCiphers[1].args.ciphertext
+    return (commitment_address1, commitment_address2, new_mk_root, ciphertext1, ciphertext2)
+>>>>>>> add deploy and mix for groth16
