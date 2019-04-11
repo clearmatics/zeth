b39adfeb4f201099bda4627d4695842a52b75673
riemann89 <giu.giffone@tiscali.it>
2019-04-12 00:52:32 +0100

    vk struct fix and verify/verifyTx implemeted

--- zeth-contracts/contracts/Groth16Verifier.sol
+++ zeth-contracts/contracts/Groth16Verifier.sol
@@ -23,7 +23,8 @@ contract Verifier {
     using Pairing for *;
 
     struct VerifyingKey {
-        Pairing.GTPoint AB;
+        Pairing.G1Point Alpha;
+        Pairing.G2Point Beta;
         Pairing.G2Point Gamma;
         Pairing.G2Point Delta;
         Pairing.G1Point[] Gamma_ABC;
@@ -40,15 +41,17 @@ contract Verifier {
     event LogVerifier(string);
 
     constructor(
-        uint[3][2] memory AB1,
-        uint[3][2] memory AB2,
+        uint[2] memory Alpha,
+        uint[2] memory Beta1,
+        uint[2] memory Beta2,
         uint[2] memory Gamma1,
         uint[2] memory Gamma2,
         uint[2] memory Delta1,
         uint[2] memory Delta2,
         uint[] memory Gamma_ABC_elements
     ) public {
-        verifyKey.AB = Pairing.GTPoint(AB1, AB2);//TODO not sure if it works
+        verifyKey.Alpha = Pairing.G1Point(Alpha[0], Alpha[1]);
+        verifyKey.Beta = Pairing.G2Point(Beta1, Beta2);
         verifyKey.Gamma = Pairing.G2Point(Gamma1, Gamma2);
         verifyKey.Delta = Pairing.G2Point(Delta1, Delta2);
         
@@ -70,14 +73,18 @@ contract Verifier {
     function verify(uint[] memory input, Proof memory proof) internal returns (uint) {
         VerifyingKey memory vk = verifyKey;
 
-        y_side_1 = (Pairing.negate(g_alpha), g2_beta);//TODO fix this
-        y_side_2 = 2;//TODO;
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);//TODO: to check
+        for (uint i = 0; i < input.length; i++) {
+            vk_x = Pairing.add(vk_x, Pairing.mul(vk.Gamma_ABC[i + 1], input[i]));
+        }
+        vk_x = Pairing.add(vk_x, vk.Gamma_ABC[0]);
 
         bool res = !Pairing.pairingProd4(
-            proof.A,proof.B, 
-            y_side_1, 
-            y_side_2, 
-            Pairing.negate(proof.C), Pairing.G2(vk.Delta1, vk.Delta2));
+            proof.A,proof.B,
+            Pairing.negate(vk.Alpha), vk.Beta, 
+            Pairing.negate(vk_x), vk.Gamma, 
+            Pairing.negate(proof.C), vk.Delta);
+
         if(!res){
             return 1;
         }
@@ -88,13 +95,26 @@ contract Verifier {
     function verifyTx(
         uint[2] memory a,
         uint[2][2] memory b,
-        uint[2] memory b_p,
         uint[2] memory c,
-        uint[2] memory c_p,
-        uint[2] memory h,
-        uint[2] memory k,
         uint[] memory primaryInputs
     ) public returns (bool) {
-        //TODO
+        Proof memory proof;
+        proof.A = Pairing.G1Point(a[0], a[1]);
+        proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.C = Pairing.G1Point(c[0], c[1]);
+
+        uint[] memory inputValues = new uint[](primaryInputs.length);
+        for(uint i = 0; i < primaryInputs.length; i++){
+            inputValues[i] = primaryInputs[i];
+        }
+
+        uint verification_result = verify(inputValues, proof);
+        if (verification_result != 0) {
+            emit LogVerifier("Failed to verify the transaction");
+            return false;
+        }
+
+        emit LogVerifier("Proof verification successfull");
+        return true;
     }
 }
