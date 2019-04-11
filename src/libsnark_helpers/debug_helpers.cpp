1d5827199b194fa51455947ec6537b8fa9ec07f1
riemann89 <giu.giffone@tiscali.it>
2019-04-11 20:46:12 +0100

    removed gt structs and functions

--- src/libsnark_helpers/debug_helpers.cpp
+++ src/libsnark_helpers/debug_helpers.cpp
@@ -67,30 +67,6 @@ std::string outputPointG2AffineAsHex(libff::alt_bn128_G2 _p)
         HexStringFromLibsnarkBigint(aff.Y.c0.as_bigint()) + "\"]";
 }
 
-std::string outputPointGTAsHex(libff::alt_bn128_GT _p)//TODO: review aggregation
-{
-    return
-        "[\"0x" +
-        HexStringFromLibsnarkBigint(_p.c0.c0.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c0.c0.c1.as_bigint()) + "\"],\n [\"0x" +
-
-        HexStringFromLibsnarkBigint(_p.c0.c1.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c0.c1.c1.as_bigint()) + "\"],\n [\"0x" +
-
-        HexStringFromLibsnarkBigint(_p.c0.c2.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c0.c2.c1.as_bigint()) + "\"],\n [\"0x" +
-
-
-        HexStringFromLibsnarkBigint(_p.c1.c0.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c1.c0.c1.as_bigint()) + "\"],\n [\"0x" +
-
-        HexStringFromLibsnarkBigint(_p.c1.c1.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c1.c1.c1.as_bigint()) + "\"],\n [\"0x" +
-
-        HexStringFromLibsnarkBigint(_p.c1.c2.c0.as_bigint()) + "\", \"0x" +
-        HexStringFromLibsnarkBigint(_p.c1.c2.c1.as_bigint()) + "\"]";
-}
-
 boost::filesystem::path getPathToSetupDir()
 {
     char* pathToSetupFolder;
