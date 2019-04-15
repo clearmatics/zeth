52ed2ed20238da0e2f0ec89f14aae79e75f892c6
riemann89 <giu.giffone@tiscali.it>
2019-04-15 15:10:56 +0100

    Fix compilation error due to a refactoring

--- src/snarks_import.hpp
+++ src/snarks_import.hpp
@@ -2,9 +2,9 @@
 #define __ZETH_SNARKS_IMPORT_HPP__
 
 #ifdef SNARK_R1CS_PPZKSNARK
-#include "snarks/pghr13/pghr13_helpers.hpp"
-#include "snarks/pghr13/pghr13_computation.hpp"
-#include "snarks/pghr13/pghr13_response.hpp"
+#include "snarks/pghr13/helpers.hpp"
+#include "snarks/pghr13/computation.hpp"
+#include "snarks/pghr13/response.hpp"
 
 #else
 #error You must define one of the SNARK_* symbols indicated into the CMakelists.txt file.
