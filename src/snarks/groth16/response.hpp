046a8394b64f1a9a7ed1f3ec8fc01a1478ebae87
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-04-23 19:23:46 +0100

    Fixed bad formatting

--- src/snarks/groth16/response.hpp
+++ src/snarks/groth16/response.hpp
@@ -6,7 +6,6 @@
 #include "prover.grpc.pb.h"
 #include "util_api.hpp"
 
-
 using proverpkg::VerificationKey;
 using proverpkg::ExtendedProof;
 using proverpkg::R1csGgPpzksnarkExtendedProof;
@@ -14,15 +13,12 @@ using proverpkg::R1csGgPpzksnarkVerificationKey;
 using proverpkg::HexadecimalPointBaseGroup1Affine;
 using proverpkg::HexadecimalPointBaseGroup2Affine;
 
-namespace libzeth{
+namespace libzeth {
 
-    template<typename ppT>
-    void PrepareProofResponse(extended_proof<ppT>& ext_proof, ExtendedProof* message);
-    
-    template<typename ppT>
-    void PrepareVerifyingKeyResponse(libsnark::r1cs_gg_ppzksnark_verification_key<ppT>& vk, VerificationKey* message);
-    
-}
+template<typename ppT> void PrepareProofResponse(extended_proof<ppT>& ext_proof, ExtendedProof* message);
+template<typename ppT>void PrepareVerifyingKeyResponse(libsnark::r1cs_gg_ppzksnark_verification_key<ppT>& vk, VerificationKey* message);
 
+} // libzeth
 #include "snarks/groth16/response.tcc"
+
 #endif // __ZETH_RESPONSE_HPP__
