6b71b0b7295ff598314932fb907f0a5515110b92
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-04-24 19:49:26 +0100

    Fixed broken make check and split the imports for core and api functionalities

--- src/snarks_core_imports.hpp
+++ src/snarks_core_imports.hpp
@@ -1,16 +1,14 @@
-#ifndef __ZETH_SNARKS_IMPORT_HPP__
-#define __ZETH_SNARKS_IMPORT_HPP__
+#ifndef __ZETH_SNARKS_CORE_IMPORTS_HPP__
+#define __ZETH_SNARKS_CORE_IMPORTS_HPP__
 
 #ifdef ZKSNARK_PGHR13
 #include "snarks/pghr13/helpers.hpp"
 #include "snarks/pghr13/computation.hpp"
-#include "snarks/pghr13/response.hpp"
 #elif ZKSNARK_GROTH16
 #include "snarks/groth16/helpers.hpp"
 #include "snarks/groth16/computation.hpp"
-#include "snarks/groth16/response.hpp"
 #else
 #error You must define one of the SNARK_* symbols indicated into the CMakelists.txt file.
 #endif
 
-#endif // __ZETH_SNARKS_IMPORT_HPP__
+#endif // __ZETH_SNARKS_CORE_IMPORTS_HPP__
