c9b7ea21e1aa09deaecbf39d297607f18ab67a4e
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-04-30 10:33:04 +0100

    Refactored mixer contracts

--- zeth-contracts/contracts/Mixer.sol
+++ zeth-contracts/contracts/Mixer.sol
@@ -37,36 +37,6 @@ contract ERC223ReceivingContract {
     }
 }
 
-/*
- * Note1:
- * We might want to use the `Szabo` as a unit for the payments.
- * In fact, as we are using hex strings of length 64bits in the prover to handle the values,
- * the max value we can encode is: "0xFFFFFFFFFFFFFFFF" which corresponds to:
- * "18446744073709551615". If we use this number as WEI, this represents:
- * "18.446744073709551615" ETH. Here we clearly see that we do not as many digits after the
- * floating point, and moreover, we would like to be able to do payments of more than
- * 18ETH. If we use the number "18446744073709551615" (our max) as Szabo, we can make payments
- * up to "18446744073709.551615" ETH which presents a sufficiently high upper bound and which contains
- * 6 digits after the floating point.
- * Here we assume that this is sufficient for our use cases.
- *
- * It is possible to adjust by changing the way the max number "18446744073709551615" is interpreted
- * (changing the unit)
- * See: https://solidity.readthedocs.io/en/v0.4.21/units-and-global-variables.html#ether-units
-**/
-
-/*
- * Note2: Because we emit the ciphertexts and the addresses of insertion of the commitments in the tree in the same
- * tx, we have a great way for the recipient to accelerate the verification of a payment.
- * In fact:
- * If Alice receives a payment, then she has the decryption key to retrieve the plaintext of an encrypted note that is emitted in the
- * call of the `transfer`. However, in the same smart contract call are emitted the addresses of insertion of the commitments
- * in the merkle tree. This means that the recipient does not need to verify that the re-computed commitment is "somewhere" in the set of the merkle tree leaves.
- * Instead the recipient JUST has to verify that the recomputed commitment is one of the commitment in the address set that is emitted during the smart contract call.
- * This makes it easier to check the validity of a payment, and is much faster to verify as we directly tell the recipient "where to look for his commitment in the tree".
- * By leveraging this data, we avoid a lot of unecessary overhead just to confirm a payment here.
-**/
-
 contract Mixer is MerkleTreeSha256, ERC223ReceivingContract {
     using Bytes for *;
 
@@ -88,6 +58,10 @@ contract Mixer is MerkleTreeSha256, ERC223ReceivingContract {
     address public token;
 
     // Event to emit the address of a commitment in the merke tree
+    // Allows for faster execution of the "Receive" functions on the receiver side.
+    // The ciphertext of a note is emitted along the address of insertion in the tree
+    // Thus, instead of checking that the decrypted note is represented somewhere in the tree, the recipient just needs
+    // to check that the decrypted note opens the commitment at the emitted address
     event LogAddress(uint commAddr);
 
     // Event to emit the merkle root of a tree
@@ -103,12 +77,116 @@ contract Mixer is MerkleTreeSha256, ERC223ReceivingContract {
 
     // Constructor
     constructor(uint depth, address _token) MerkleTreeSha256(depth) public {
-        
+
         // We log the first root to get started
         bytes32 initialRoot = getRoot();
         roots[initialRoot] = true;
         emit LogMerkleRoot(initialRoot);
 
         token = _token;
+    }
+
+    // This function processes the primary inputs to append and check the root and nullifiers int he primary inputs (instance)
+    // and modifies the state of the mixer contract accordingly
+    // (ie: Appends the commitments to the tree, appends the nullifiers to the list and so on)
+    function assemble_root_and_nullifiers_and_append_to_state(uint[] memory primary_inputs) internal {
+        // Reminder: Remember that the primary inputs are ordered as follows:
+        // We make sure to have the primary inputs ordered as follow:
+        // [Root, NullifierS, CommitmentS, value_pub_in, value_pub_out]
+        // ie, below is the index mapping of the primary input elements on the protoboard:
+        // - Index of the "Root" field elements: {0}
+        // - Index of the "NullifierS" field elements: [1, NumInputs + 1[
+        // - Index of the "CommitmentS" field elements: [NumInputs + 1, NumOutputs + NumInputs + 1[
+        // - Index of the "v_pub_in" field element: {NumOutputs + NumInputs + 1}
+        // - Index of the "v_pub_out" field element: {NumOutputs + NumInputs + 1 + 1}
+        //
+        // 1. We re-assemble the full root digest from the 2 field elements it was packed into
+        uint256[] memory digest_inputs = new uint[](2);
+        digest_inputs[0] = primary_inputs[0];
+        digest_inputs[1] = primary_inputs[1];
+        require(
+            roots[Bytes.sha256_digest_from_field_elements(digest_inputs)],
+            "Invalid root: This root doesn't exist"
+        );
+
+        // 2. We re-assemble the nullifiers (JSInputs)
+        for(uint i = 2; i < 2 * (1 + jsIn); i += 2) {
+            digest_inputs[0] = primary_inputs[i];
+            digest_inputs[1] = primary_inputs[i+1];
+            bytes32 current_nullifier = Bytes.sha256_digest_from_field_elements(digest_inputs);
+            require(
+                !nullifiers[current_nullifier],
+                "Invalid nullifier: This nullifier has already been used"
+            );
+            nullifiers[current_nullifier] = true;
+        }
+    }
+
+    function assemble_commitments_and_append_to_state(uint[] memory primary_inputs) internal {
+        // Reminder: Remember that the primary inputs are ordered as follows:
+        // We make sure to have the primary inputs ordered as follow:
+        // [Root, NullifierS, CommitmentS, value_pub_in, value_pub_out]
+        // ie, below is the index mapping of the primary input elements on the protoboard:
+        // - Index of the "Root" field elements: {0}
+        // - Index of the "NullifierS" field elements: [1, NumInputs + 1[
+        // - Index of the "CommitmentS" field elements: [NumInputs + 1, NumOutputs + NumInputs + 1[
+        // - Index of the "v_pub_in" field element: {NumOutputs + NumInputs + 1}
+        // - Index of the "v_pub_out" field element: {NumOutputs + NumInputs + 1 + 1}
+        //
+        // 1. We re-assemble the commitments (JSOutputs)
+        uint256[] memory digest_inputs = new uint[](2);
+        for(uint i = 2 * (1 + (jsIn)); i < 2 * (1 + jsIn + jsOut); i += 2) {
+            digest_inputs[0] = primary_inputs[i]; // See the way the inputs are ordered in the extended proof
+            digest_inputs[1] = primary_inputs[i+1];
+            bytes32 current_commitment = Bytes.sha256_digest_from_field_elements(digest_inputs);
+            uint commitmentAddress = insert(current_commitment);
+            emit LogAddress(commitmentAddress);
+        }
+    }
+
+    function process_public_values(uint[] memory primary_inputs) internal {
+        // 1. We get the vpub_in in wei
+        uint64 vpub_in = Bytes.get_value_from_inputs(Bytes.int256ToBytes8(primary_inputs[2 * (1 + jsIn + jsOut)]));
+
+        // If the vpub_in is > 0, we need to make sure the right amount is paid
+        if (vpub_in > 0) {
+            if (token != address(0)) {
+                ERC20 erc20Token = ERC20(token);
+                erc20Token.transferFrom(msg.sender, address(this), uint256(vpub_in));
+            } else {
+                require(
+                    msg.value == vpub_in,
+                    "Wrong msg.value: Value paid is not correct"
+                );
+            }
+        } else {
+            // If vpub_in is = 0, since we have a payable function, we need to
+            // send the amount paid back to the caller
+            msg.sender.transfer(msg.value);
+        }
+
+        // 2. Get vpub_out in wei
+        uint64 vpub_out = Bytes.get_value_from_inputs(Bytes.int256ToBytes8(primary_inputs[2 * (1 + jsIn + jsOut) + 1]));
+
+        // If value_pub_out > 0 then we do a withdraw
+        // We retrieve the msg.sender and send him the appropriate value IF proof is valid
+        if (vpub_out > 0) {
+            if (token != address(0)) {
+                ERC20 erc20Token = ERC20(token);
+                erc20Token.transfer(msg.sender, uint256(vpub_out));
+            } else {
+                msg.sender.transfer(vpub_out);
+            }
+        }
+    }
+
+    function add_and_emit_merkle_root(bytes32 root) internal {
+        roots[root] = true;
+        emit LogMerkleRoot(root);
+    }
+
+    function emit_ciphertexts(string memory ciphertext1, string memory ciphertext2) internal {
+        emit LogSecretCiphers(ciphertext1);
+        emit LogSecretCiphers(ciphertext2);
     }
 }
