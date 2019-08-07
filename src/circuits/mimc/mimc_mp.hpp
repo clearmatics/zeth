e9df8ebfb80d2a59b99b3821c39c225b6eafcc0d
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-08-07 20:32:02 +0100

    Moved attributes to private

--- src/circuits/mimc/mimc_mp.hpp
+++ src/circuits/mimc/mimc_mp.hpp
@@ -9,6 +9,8 @@
 #include "mimc.hpp"
 
 /*
+ * This gadget implements the interface of the HashTreeT template
+ *
  * MiMC_mp_gadget enforces correct computation of MiMC compression function
  * based on a the Miyaguchi-Preneel compression construct and MiMC block cipher on Z_p with exponent 7 (and 91 rounds)
  * p is given by the size(FieldT)
@@ -18,12 +20,13 @@ namespace libzeth {
 
 template<typename FieldT>
 class MiMC_mp_gadget : public libsnark::gadget<FieldT> {
-public:
+private:
     libsnark::pb_variable<FieldT> x; // First input
     libsnark::pb_variable<FieldT> y; // Second input
     MiMCe7_permutation_gadget<FieldT> permutation_gadget; // Permutation gadget
     libsnark::pb_variable<FieldT> output; // Output variable
 
+public:
     MiMC_mp_gadget(
         libsnark::protoboard<FieldT> &pb,
         const libsnark::pb_variable<FieldT> x,
