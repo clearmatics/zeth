1bb9bf4562151efe07470366e508e93807065871
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-08-07 16:21:58 +0100

    Fixed erroneous name for the round_constant variable in gen code utility

--- pyClient/zethCodeConstantsGeneration.py
+++ pyClient/zethCodeConstantsGeneration.py
@@ -1,6 +1,6 @@
 from zethMimc import sha3_256
 
-#C++ code generation for constants of a given seed see src/circuits/mimc/round_constants.tcc
+# C++ code generation for constants of a given seed see src/circuits/mimc/round_constants.tcc
 
 try:
     # pysha3
@@ -10,10 +10,11 @@ except ImportError:
     from Crypto.Hash import keccak
     keccak_256 = lambda *args: keccak.new(digest_bits=256)
 
-print("mt_round_constants.push_back(FieldT(\"0\"));")
+print("round_constants.push_back(FieldT(\"0\"));")
 
 # First hash is skipped
 res = sha3_256(b"clearmatics_mt_seed")
+# We generate the round constants for the remaining 90 rounds (total number of rounds = 91)
 for i in range(90):
     res = sha3_256(res)
-    print("mt_round_constants.push_back(FieldT(\""+str(res)+"\"));")
+    print("round_constants.push_back(FieldT(\"" + str(res) + "\"));")
