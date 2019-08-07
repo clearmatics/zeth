a3d439f6b9412e1f168f60de47bfd648d2ac2215
Antoine Rondelet <rondelet.antoine@gmail.com>
2019-08-07 16:38:11 +0100

    Moved erroneously exposed attributes to private

--- src/circuits/mimc/mimc.hpp
+++ src/circuits/mimc/mimc.hpp
@@ -8,47 +8,47 @@
 #include "snarks_alias.hpp"
 #include "round.hpp"
 
+/*
+ * MiMCe7_permutation_gadget enforces correct computation of a MiMC permutation with exponent 7 and rounds 91. 
+ * It makes use of MiMCe7_round_gadget to enforce correct computation in each round.
+**/
+
 namespace libzeth  {
- /*
-  * MiMCe7_permutation_gadget enforces correct computation of a MiMC permutation with exponent 7 and rounds 91. It makes use of MiMCe7_round_gadget to enforce correct computation in each round.
-  */
+
 template<typename FieldT>
 class MiMCe7_permutation_gadget : public libsnark::gadget<FieldT> {
-public:
-    std::vector<MiMCe7_round_gadget<FieldT>> round_gadgets;     // Vector of MiMC round gadgets
-    std::vector<FieldT> round_constants;                        // Current Vector of round constants
-    static const int ROUNDS = 91;                               // Nb of rounds suggested by the MiMC paper
-    const libsnark::pb_variable<FieldT> k;                      // The permutation key
-
-    // utility functions
-    // MiMC round gadgets initialization
-    void setup_gadgets(
-        const libsnark::pb_variable<FieldT> x,
-        const libsnark::pb_variable<FieldT> k);
-
-    //Constants vector initialization
-    void setup_sha3_constants();
+private:
+    std::vector<MiMCe7_round_gadget<FieldT>> round_gadgets; // Vector of MiMC round_gadgets
+    std::vector<FieldT> round_constants; // Vector of round constants
+    const libsnark::pb_variable<FieldT> k; // Permutation key
 
 public:
+    static const int ROUNDS = 91; // Nb of rounds suggested by the MiMC paper
+
     MiMCe7_permutation_gadget(
         libsnark::protoboard<FieldT>& pb,
-        const libsnark::pb_variable<FieldT> x,                  // The message to encrypt
-        const libsnark::pb_variable<FieldT> k,                  // The encryption key (/permutation seed)
-        const std::string& annotation_prefix = "MiMCe7_permutation_gadget");
+        const libsnark::pb_variable<FieldT> x, // Message to encrypt
+        const libsnark::pb_variable<FieldT> k, // Encryption key (/permutation seed)
+        const std::string& annotation_prefix = "MiMCe7_permutation_gadget"
+    );
 
     void generate_r1cs_constraints();
-
     void generate_r1cs_witness() const;
 
     const libsnark::pb_variable<FieldT>& result() const;
 
+    // Utils functions
+    //
+    // MiMC round gadgets initialization
+    void setup_gadgets(const libsnark::pb_variable<FieldT> x, const libsnark::pb_variable<FieldT> k);
+    // Constants vector initialization
+    void setup_sha3_constants();
 };
 
 template<typename FieldT>
 using MiMC_gadget = MiMCe7_permutation_gadget<FieldT>;
 
 } // libzeth
-
 #include "mimc.tcc"
 
 #endif // __ZETH_MIMC_PERMUTATION_HPP__
--- src/circuits/mimc/round.hpp
+++ src/circuits/mimc/round.hpp
@@ -10,14 +10,17 @@
 #include <libsnark/gadgetlib1/gadget.hpp>
 #include "circuits/circuits-util.hpp"
 
-namespace libzeth {
+/*
+ * MiMCe7_round_gadget enforces correct computation of a MiMC permutation round with exponent 7.
+ * In MiMC permutation last round differs from the others since the key is added again.
+ * We use a boolean variable `add_k_to_result` to manage this case.
+**/
 
-// MiMCe7_round_gadget enforces correct computation of a MiMC permutation round with exponent 7.
-// In MiMC permutation last round differs from the others since the key is added again. We use a boolean variable `add_k_to_result` to manage this case.
+namespace libzeth {
 
 template<typename FieldT>
 class MiMCe7_round_gadget : public libsnark::gadget<FieldT> {
-public:
+private:
     const libsnark::pb_variable<FieldT> x;          // The message of the current round
     const libsnark::pb_variable<FieldT> k;          // The key of the current round
     const FieldT c;                                 // The round constant of the current round
@@ -41,16 +44,13 @@ public:
     );
 
     void generate_r1cs_constraints();
-
     void generate_r1cs_witness() const;
 
-    // Returns round result (x + k + c) **7  + add_k_to_result * k
+    // Returns round result (x + k + c) ** 7 + add_k_to_result * k
     const libsnark::pb_variable<FieldT>& result() const;
-
 };
 
-}   // libzeth
-
+} // libzeth
 #include "round.tcc"
 
-#endif // __ZETH_MIMC_ROUND_HPP
+#endif // __ZETH_MIMC_ROUND_HPP__
