06d84288771af42a3380b9cd7150c1fc3a73acbd
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-08-27 16:32:21 +0100

    powersoftau: support writing out compatible powersoftau data

--- src/snarks/groth16/powersoftau_utils.cpp
+++ src/snarks/groth16/powersoftau_utils.cpp
@@ -30,6 +30,24 @@ std::istream &read_powersoftau_fp(
 }
 
 template<mp_size_t n, const libff::bigint<n> &modulus>
+libff::Fp_model<n, modulus> from_montgomery_repr(libff::Fp_model<n, modulus> m)
+{
+    libff::Fp_model<n, modulus> tmp;
+    tmp.mont_repr.data[0] = 1;
+    tmp.mul_reduce(m.mont_repr);
+    return tmp;
+}
+
+template<mp_size_t n, const libff::bigint<n> &modulus>
+void write_powersoftau_fp(
+    std::ostream &out, const libff::Fp_model<n, modulus> &fp)
+{
+    libff::Fp_model<n, modulus> copy = from_montgomery_repr(fp);
+    std::reverse((char *)&copy, (char *)(&copy + 1));
+    out.write((const char *)&copy, sizeof(mp_limb_t) * n);
+}
+
+template<mp_size_t n, const libff::bigint<n> &modulus>
 std::istream &read_powersoftau_fp2(
     std::istream &in, libff::Fp2_model<n, modulus> &el)
 {
@@ -58,6 +76,35 @@ std::istream &read_powersoftau_fp2(
     el.c1.mul_reduce(libff::Fp_model<n, modulus>::Rsquared);
 
     return in;
+}
+
+template<mp_size_t n, const libff::bigint<n> &modulus>
+void write_powersoftau_fp2(
+    std::ostream &out, const libff::Fp2_model<n, modulus> &fp2)
+{
+    // Fq2 data is packed into a single 512 bit integer as:
+    //
+    //   c1 * modulus + c0
+    libff::Fp_model<n, modulus> c0 = from_montgomery_repr(fp2.c0);
+    libff::Fp_model<n, modulus> c1 = from_montgomery_repr(fp2.c1);
+
+    libff::bigint<2 * n> packed;
+    packed.clear();
+    for (size_t i = 0; i < n; ++i) {
+        packed.data[i] = c0.mont_repr.data[i];
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        mp_limb_t carryout = mpn_addmul_1(
+            &packed.data[i], modulus.data, n, c1.mont_repr.data[i]);
+        assert(packed.data[n + i] == 0);
+        carryout = mpn_add_1(
+            &packed.data[n + i], &packed.data[n + i], n - i, carryout);
+        assert(carryout == 0);
+    }
+
+    std::reverse((uint8_t *)&packed, (uint8_t *)((&packed) + 1));
+    out.write((const char *)&packed, sizeof(packed));
 }
 
 } // namespace
@@ -122,6 +169,50 @@ void read_powersoftau_g2(std::istream &in, libff::G2<ppT> &out)
         assert(false);
         break;
     }
+}
+
+void write_powersoftau_fr(std::ostream &out, const libff::Fr<ppT> &fr)
+{
+    write_powersoftau_fp(out, fr);
+}
+
+void write_powersoftau_fq2(std::ostream &out, const libff::alt_bn128_Fq2 &fq2)
+{
+    write_powersoftau_fp2(out, fq2);
+}
+
+void write_powersoftau_g1(std::ostream &out, const libff::G1<ppT> &g1)
+{
+    if (g1.is_zero()) {
+        const uint8_t zero = 0;
+        out.write((const char *)&zero, 1);
+        return;
+    }
+
+    libff::alt_bn128_G1 copy(g1);
+    copy.to_affine_coordinates();
+
+    const uint8_t marker = 0x04;
+    out.write((const char *)&marker, 1);
+    write_powersoftau_fp(out, copy.X);
+    write_powersoftau_fp(out, copy.Y);
+}
+
+void write_powersoftau_g2(std::ostream &out, const libff::G2<ppT> &g2)
+{
+    if (g2.is_zero()) {
+        const uint8_t zero = 0;
+        out.write((const char *)&zero, 1);
+        return;
+    }
+
+    libff::alt_bn128_G2 copy(g2);
+    copy.to_affine_coordinates();
+
+    const uint8_t marker = 0x04;
+    out.write((const char *)&marker, 1);
+    write_powersoftau_fp2(out, copy.X);
+    write_powersoftau_fp2(out, copy.Y);
 }
 
 srs_powersoftau<ppT> powersoftau_load(std::istream &in, size_t n)
@@ -190,6 +281,30 @@ srs_powersoftau<ppT> powersoftau_load(std::istream &in, size_t n)
         std::move(alpha_tau_powers_g1),
         std::move(beta_tau_powers_g1),
         beta_g2);
+}
+
+void powersoftau_write(std::ostream &out, const srs_powersoftau<ppT> &pot)
+{
+    // Fake the hash
+    uint8_t hash[64];
+    memset(hash, 0, sizeof(hash));
+    out.write((char *)(&hash[0]), sizeof(hash));
+
+    const size_t n = pot.tau_powers_g2.size();
+    const size_t num_powers_of_tau = 2 * n - 1;
+    for (size_t i = 0; i < num_powers_of_tau; ++i) {
+        write_powersoftau_g1(out, pot.tau_powers_g1[i]);
+    }
+    for (size_t i = 0; i < n; ++i) {
+        write_powersoftau_g2(out, pot.tau_powers_g2[i]);
+    }
+    for (size_t i = 0; i < n; ++i) {
+        write_powersoftau_g1(out, pot.alpha_tau_powers_g1[i]);
+    }
+    for (size_t i = 0; i < n; ++i) {
+        write_powersoftau_g1(out, pot.beta_tau_powers_g1[i]);
+    }
+    write_powersoftau_g2(out, pot.beta_g2);
 }
 
 } // namespace libzeth
