784503a20d76c375a135b23f68052a022f04125a
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-08-28 17:24:10 +0100

    avoid copying objects passed to computation functions

--- src/snarks/pghr13/computation.hpp
+++ src/snarks/pghr13/computation.hpp
@@ -6,24 +6,20 @@
 #include <libsnark/gadgetlib1/gadget.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp>
 
-// We instantiate the ppT (public parameters Template with the public paramaters
-// of the curve we use (alt_bn128))
-typedef libff::default_ec_pp ppT;
-
 namespace libzeth
 {
 
 template<typename ppT>
 libsnark::r1cs_ppzksnark_proof<ppT> gen_proof(
-    libsnark::protoboard<libff::Fr<ppT>> pb,
-    libsnark::r1cs_ppzksnark_proving_key<ppT> proving_key);
+    const libsnark::protoboard<libff::Fr<ppT>> &pb,
+    const libsnark::r1cs_ppzksnark_proving_key<ppT> &proving_key);
 template<typename ppT>
 libsnark::r1cs_ppzksnark_keypair<ppT> gen_trusted_setup(
-    libsnark::protoboard<libff::Fr<ppT>> pb);
+    const libsnark::protoboard<libff::Fr<ppT>> &pb);
 template<typename ppT>
 bool verify(
-    libzeth::extended_proof<ppT> ext_proof,
-    libsnark::r1cs_ppzksnark_verification_key<ppT> verification_key);
+    const libzeth::extended_proof<ppT> &ext_proof,
+    const libsnark::r1cs_ppzksnark_verification_key<ppT> &verification_key);
 
 } // namespace libzeth
 #include "snarks/pghr13/computation.tcc"
