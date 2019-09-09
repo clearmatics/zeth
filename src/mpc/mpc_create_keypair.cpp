ffdfc59b153309ef2a962dc4c8279f48f6590077
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-09-09 18:03:02 +0100

    mpc: refactor mpc subcommands to remove some repeated code

--- src/mpc/mpc_create_keypair.cpp
+++ src/mpc/mpc_create_keypair.cpp
@@ -1,8 +1,8 @@
 #include "circuit-wrapper.hpp"
+#include "mpc_common.hpp"
 #include "snarks/groth16/mpc_utils.hpp"
 #include "snarks/groth16/powersoftau_utils.hpp"
 
-#include <boost/program_options.hpp>
 #include <vector>
 
 using namespace libzeth;
@@ -25,199 +25,164 @@ using HashT = sha256_ethereum<FieldT>;
 //  -h,--help           This message
 //  --pot-degree        powersoftau degree (assumed to match linear comb)
 //  --out <file>        Write key-pair to <file> (mpc-keypair.bin)
-class cli_options
+class mpc_create_keypair : public subcommand
 {
-public:
-    po::options_description desc;
-    po::options_description all_desc;
-    po::positional_options_description pos;
-
-    std::string argv0;
-    bool help;
     std::string powersoftau_file;
     std::string lin_comb_file;
     std::string phase2_file;
     size_t powersoftau_degree;
-    std::string out;
+    std::string out_file;
 
-    cli_options();
-    void parse(const std::vector<std::string> &args);
-    void usage() const;
-};
-
-cli_options::cli_options()
-    : desc("Options")
-    , all_desc("")
-    , pos()
-    , argv0("mpc create-keypair")
-    , help(false)
-    , powersoftau_file()
-    , lin_comb_file()
-    , phase2_file()
-    , powersoftau_degree(0)
-    , out()
-{
-    desc.add_options()("help,h", "This help")(
-        "pot-degree",
-        po::value<size_t>(),
-        "powersoftau degree (assumed to match linear comb)")(
-        "out,o",
-        po::value<std::string>(),
-        "Write key-pair to file (mpc-keypair.bin)");
-    all_desc.add(desc).add_options()(
-        "powersoftau_file", po::value<std::string>(), "powersoftau file")(
-        "linear_combination_file",
-        po::value<std::string>(),
-        "linear combination file")(
-        "phase2_file", po::value<std::string>(), "phase2 final challenge file");
-    pos.add("powersoftau_file", 1)
-        .add("linear_combination_file", 1)
-        .add("phase2_file", 1);
-}
-
-void cli_options::usage() const
-{
-    std::cout << "Usage:\n"
-              << "  " << argv0 << " [<options>]  \\\n"
-              << "        <powersoftau_file> <linear_combination_file> "
-                 "<phase2_file>\n\n"
-              << desc << std::endl;
-}
-
-void cli_options::parse(const std::vector<std::string> &args)
-{
-    po::variables_map vm;
-    po::parsed_options parsed =
-        po::command_line_parser(
-            std::vector<std::string>(args.begin() + 1, args.end()))
-            .options(all_desc)
-            .positional(pos)
-            .run();
-    po::store(parsed, vm);
-
-    argv0 = args[0];
-
-    if (vm.count("help")) {
-        help = true;
-        return;
-    }
-
-    if (0 == vm.count("powersoftau_file")) {
-        throw po::error("powersoftau_file not specified");
-    }
-    if (0 == vm.count("linear_combination_file")) {
-        throw po::error("linear_combination_file not specified");
-    }
-    if (0 == vm.count("phase2_file")) {
-        throw po::error("phase2_file not specified");
-    }
-
-    powersoftau_file = vm["powersoftau_file"].as<std::string>();
-    lin_comb_file = vm["linear_combination_file"].as<std::string>();
-    phase2_file = vm["phase2_file"].as<std::string>();
-    powersoftau_degree =
-        vm.count("pot-degree") ? vm["pot-degree"].as<size_t>() : 0;
-    out = vm.count("out") ? vm["out"].as<std::string>() : "mpc-keypair.bin";
-}
-
-int zeth_mpc_create_keypair_main(const cli_options &options)
-{
-    // Load all data
-    // TODO: Load just degree from lin_comb data, then load everything
-    // in parallel.
-    libff::enter_block("Load linear combination data");
-    libff::print_indent();
-    std::cout << options.lin_comb_file << std::endl;
-    srs_mpc_layer_L1<ppT> lin_comb = [&options]() {
-        std::ifstream in(
-            options.lin_comb_file, std::ios_base::binary | std::ios_base::in);
-        return srs_mpc_layer_L1<ppT>::read(in);
-    }();
-    libff::leave_block("Load linear combination data");
-
-    libff::enter_block("Load powers of tau");
-    libff::print_indent();
-    std::cout << options.powersoftau_file << std::endl;
-    srs_powersoftau<ppT> pot = [&options, &lin_comb]() {
-        std::ifstream in(
-            options.powersoftau_file,
-            std::ios_base::binary | std::ios_base::in);
-        const size_t pot_degree = options.powersoftau_degree
-                                      ? options.powersoftau_degree
-                                      : lin_comb.degree();
-        return powersoftau_load(in, pot_degree);
-    }();
-    libff::leave_block("Load powers of tau");
-
-    libff::enter_block("Load phase2 data");
-    if (!libff::inhibit_profiling_info) {
-        libff::print_indent();
-        std::cout << options.phase2_file << "\n";
-    }
-    srs_mpc_layer_C2<ppT> phase2 = [&options]() {
-        std::ifstream in(
-            options.phase2_file, std::ios_base::binary | std::ios_base::in);
-        return srs_mpc_layer_C2<ppT>::read(in);
-    }();
-    libff::leave_block("Load phase2 data");
-
-    // Compute circuit
-    libff::enter_block("Generate QAP");
-    libsnark::protoboard<FieldT> pb;
-    joinsplit_gadget<
-        FieldT,
-        HashT,
-        HashTreeT,
-        ZETH_NUM_JS_INPUTS,
-        ZETH_NUM_JS_OUTPUTS>
-        js(pb);
-    js.generate_r1cs_constraints();
-    libsnark::r1cs_constraint_system<FieldT> cs = pb.get_constraint_system();
-    const libsnark::qap_instance<FieldT> qap =
-        libsnark::r1cs_to_qap_instance_map(cs, true);
-    libff::leave_block("Generate QAP");
-
-    libsnark::r1cs_gg_ppzksnark_keypair<ppT> keypair = mpc_create_key_pair(
-        std::move(pot),
-        std::move(lin_comb),
-        std::move(phase2),
-        std::move(cs),
-        qap);
-
-    // Write keypair to a file
-    libff::enter_block("Writing keypair file");
-    if (!libff::inhibit_profiling_info) {
-        libff::print_indent();
-        std::cout << options.out << std::endl;
-    }
+public:
+    mpc_create_keypair()
+        : subcommand("create-keypair")
+        , powersoftau_file()
+        , lin_comb_file()
+        , phase2_file()
+        , powersoftau_degree(0)
+        , out_file()
     {
-        std::ofstream out(
-            options.out, std::ios_base::binary | std::ios_base::out);
-        mpc_write_keypair(out, keypair);
     }
-    libff::leave_block("Writing keypair file");
 
-    return 0;
-}
+private:
+    void initialize_suboptions(
+        boost::program_options::options_description &options,
+        boost::program_options::options_description &all_options,
+        boost::program_options::positional_options_description &pos) override
+    {
+        options.add_options()(
+            "pot-degree",
+            po::value<size_t>(),
+            "powersoftau degree (assumed to match linear comb)")(
+            "out,o",
+            po::value<std::string>(),
+            "Write key-pair to file (mpc-keypair.bin)");
+        all_options.add(options).add_options()(
+            "powersoftau_file", po::value<std::string>(), "powersoftau file")(
+            "linear_combination_file",
+            po::value<std::string>(),
+            "linear combination file")(
+            "phase2_file",
+            po::value<std::string>(),
+            "phase2 final challenge file");
+        pos.add("powersoftau_file", 1)
+            .add("linear_combination_file", 1)
+            .add("phase2_file", 1);
+    }
+
+    void parse_suboptions(
+        const boost::program_options::variables_map &vm) override
+    {
+        if (0 == vm.count("powersoftau_file")) {
+            throw po::error("powersoftau_file not specified");
+        }
+        if (0 == vm.count("linear_combination_file")) {
+            throw po::error("linear_combination_file not specified");
+        }
+        if (0 == vm.count("phase2_file")) {
+            throw po::error("phase2_file not specified");
+        }
+
+        powersoftau_file = vm["powersoftau_file"].as<std::string>();
+        lin_comb_file = vm["linear_combination_file"].as<std::string>();
+        phase2_file = vm["phase2_file"].as<std::string>();
+        powersoftau_degree =
+            vm.count("pot-degree") ? vm["pot-degree"].as<size_t>() : 0;
+        out_file =
+            vm.count("out") ? vm["out"].as<std::string>() : "mpc-keypair.bin";
+    }
+
+    void subcommand_usage() override
+    {
+        std::cout << "Usage:\n"
+                  << "  " << subcommand_name << " [<options>]  \\\n"
+                  << "        <powersoftau_file> <linear_combination_file> "
+                     "<phase2_file>\n\n";
+    }
+
+    int execute_subcommand() override
+    {
+        if (verbose) {
+            std::cout << "powersoftau_file: " << powersoftau_file << "\n"
+                      << "lin_comb_file: " << lin_comb_file << "\n"
+                      << "phase2_file: " << phase2_file << "\n"
+                      << "powersoftau_degree: " << powersoftau_degree << "\n"
+                      << "out_file: " << out_file << std::endl;
+        }
+
+        // Load all data
+        // TODO: Load just degree from lin_comb data, then load everything
+        // in parallel.
+        libff::enter_block("Load linear combination data");
+        libff::print_indent();
+        std::cout << lin_comb_file << std::endl;
+        srs_mpc_layer_L1<ppT> lin_comb =
+            read_from_file<srs_mpc_layer_L1<ppT>>(lin_comb_file);
+        libff::leave_block("Load linear combination data");
+
+        libff::enter_block("Load powers of tau");
+        libff::print_indent();
+        std::cout << powersoftau_file << std::endl;
+        srs_powersoftau<ppT> pot = [this, &lin_comb]() {
+            std::ifstream in(
+                powersoftau_file, std::ios_base::binary | std::ios_base::in);
+            const size_t pot_degree =
+                powersoftau_degree ? powersoftau_degree : lin_comb.degree();
+            return powersoftau_load(in, pot_degree);
+        }();
+        libff::leave_block("Load powers of tau");
+
+        libff::enter_block("Load phase2 data");
+        if (!libff::inhibit_profiling_info) {
+            libff::print_indent();
+            std::cout << phase2_file << "\n";
+        }
+        srs_mpc_layer_C2<ppT> phase2 =
+            read_from_file<srs_mpc_layer_C2<ppT>>(phase2_file);
+        libff::leave_block("Load phase2 data");
+
+        // Compute circuit
+        libff::enter_block("Generate QAP");
+        libsnark::protoboard<FieldT> pb;
+        joinsplit_gadget<
+            FieldT,
+            HashT,
+            HashTreeT,
+            ZETH_NUM_JS_INPUTS,
+            ZETH_NUM_JS_OUTPUTS>
+            js(pb);
+        js.generate_r1cs_constraints();
+        libsnark::r1cs_constraint_system<FieldT> cs =
+            pb.get_constraint_system();
+        const libsnark::qap_instance<FieldT> qap =
+            libsnark::r1cs_to_qap_instance_map(cs, true);
+        libff::leave_block("Generate QAP");
+
+        libsnark::r1cs_gg_ppzksnark_keypair<ppT> keypair = mpc_create_key_pair(
+            std::move(pot),
+            std::move(lin_comb),
+            std::move(phase2),
+            std::move(cs),
+            qap);
+
+        // Write keypair to a file
+        libff::enter_block("Writing keypair file");
+        if (!libff::inhibit_profiling_info) {
+            libff::print_indent();
+            std::cout << out_file << std::endl;
+        }
+        {
+            std::ofstream out(
+                out_file, std::ios_base::binary | std::ios_base::out);
+            mpc_write_keypair(out, keypair);
+        }
+        libff::leave_block("Writing keypair file");
+
+        return 0;
+    }
+};
 
 } // namespace
 
-int zeth_mpc_create_keypair(const std::vector<std::string> &args)
-{
-    cli_options options;
-    try {
-        options.parse(args);
-    } catch (po::error &error) {
-        std::cerr << " ERROR: " << error.what() << std::endl;
-        options.usage();
-        return 1;
-    }
-
-    // Execute and handle errors
-    try {
-        return zeth_mpc_create_keypair_main(options);
-    } catch (std::invalid_argument &error) {
-        std::cerr << " ERROR: " << error.what() << std::endl;
-        return 1;
-    }
-}
+// Subcommand instance
+subcommand *mpc_create_keypair_cmd = new mpc_create_keypair();
--- src/mpc/mpc_dummy_phase2.cpp
+++ src/mpc/mpc_dummy_phase2.cpp
@@ -1,9 +1,7 @@
 #include "circuit-wrapper.hpp"
+#include "mpc_common.hpp"
 #include "snarks/groth16/mpc_utils.hpp"
 #include "zeth.h"
-
-#include <boost/program_options.hpp>
-#include <fstream>
 
 using namespace libzeth;
 namespace po = boost::program_options;
@@ -21,150 +19,100 @@ using HashT = sha256_ethereum<FieldT>;
 // Options:
 //     -h,--help           This message
 //     --out <file>        Write phase2 to <file> (mpc-phase2.bin)
-class cli_options
+class mpc_dummy_phase2 : public subcommand
 {
-public:
-    po::options_description desc;
-    po::options_description all_desc;
-    po::positional_options_description pos;
-
-    std::string argv0;
-    bool help;
     std::string linear_combination_file;
-    std::string out;
+    std::string out_file;
 
-    cli_options();
-    void parse(const std::vector<std::string> &args);
-    void usage() const;
-};
-
-cli_options::cli_options()
-    : desc("Options")
-    , all_desc("")
-    , pos()
-    , argv0("mpc dummy_phase2")
-    , help(false)
-    , linear_combination_file()
-    , out()
-{
-    desc.add_options()("help,h", "This help")(
-        "out,o",
-        po::value<std::string>(),
-        "phase2 output file (mpc-phase2.bin)");
-    all_desc.add(desc).add_options()(
-        "linear_combination_file",
-        po::value<std::string>(),
-        "Linear combination file");
-    pos.add("linear_combination_file", 1);
-}
-
-void cli_options::usage() const
-{
-    std::cout << "Usage:" << std::endl
-              << "  " << argv0 << " [<options>] <linear_combination_file>"
-              << std::endl
-              << std::endl
-              << desc << std::endl;
-}
-
-void cli_options::parse(const std::vector<std::string> &args)
-{
-    po::variables_map vm;
-    po::parsed_options parsed =
-        po::command_line_parser(
-            std::vector<std::string>(args.begin() + 1, args.end()))
-            .options(all_desc)
-            .positional(pos)
-            .run();
-    po::store(parsed, vm);
-
-    argv0 = args[0];
-
-    if (vm.count("help")) {
-        help = true;
-        return;
-    }
-
-    if (0 == vm.count("linear_combination_file")) {
-        throw po::error("linear_combination file not specified");
-    }
-    linear_combination_file = vm["linear_combination_file"].as<std::string>();
-
-    out = vm.count("out") ? vm["out"].as<std::string>() : "mpc-phase2.bin";
-}
-
-int zeth_mpc_dummy_phase2_main(const cli_options &options)
-{
-#if 1
-    std::cout << "argv0: " << options.argv0 << std::endl;
-    std::cout << "help: " << std::to_string(options.help) << std::endl;
-    std::cout << "linear_combination_file: " << options.linear_combination_file
-              << std::endl;
-    std::cout << "out: " << options.out << std::endl;
-#endif
-
-    // Load the linear_combination output
-    libff::enter_block("reading linear combination data");
-    srs_mpc_layer_L1<ppT> lin_comb = [&options]() {
-        std::ifstream in(options.linear_combination_file);
-        in.exceptions(
-            std::ios_base::eofbit | std::ios_base::badbit |
-            std::ios_base::failbit);
-        return srs_mpc_layer_L1<ppT>::read(in);
-    }();
-    libff::leave_block("reading linear combination data");
-
-    // Generate the zeth circuit (to determine the number of inputs)
-    const size_t num_inputs = []() {
-        libsnark::protoboard<FieldT> pb;
-        joinsplit_gadget<
-            FieldT,
-            HashT,
-            HashTreeT,
-            ZETH_NUM_JS_INPUTS,
-            ZETH_NUM_JS_OUTPUTS>
-            js(pb);
-        const libsnark::r1cs_constraint_system<FieldT> cs =
-            pb.get_constraint_system();
-        return cs.num_inputs();
-    }();
-
-    // Generate the artifical delta
-    const FieldT delta = FieldT::random_element();
-
-    // Generate and save the dummy phase2 data
-    const srs_mpc_layer_C2<ppT> phase2 =
-        mpc_dummy_layer_C2<ppT>(lin_comb, delta, num_inputs);
-    libff::enter_block("writing phase2 data");
+public:
+    mpc_dummy_phase2()
+        : subcommand("dummy-phase2"), linear_combination_file(), out_file()
     {
-        std::ofstream out(options.out);
-        phase2.write(out);
     }
-    libff::leave_block("writing phase2 data");
 
-    return 0;
-}
+private:
+    void initialize_suboptions(
+        boost::program_options::options_description &options,
+        boost::program_options::options_description &all_options,
+        boost::program_options::positional_options_description &pos) override
+    {
+        options.add_options()(
+            "out,o",
+            po::value<std::string>(),
+            "phase2 output file (mpc-phase2.bin)");
+        all_options.add(options).add_options()(
+            "linear_combination_file",
+            po::value<std::string>(),
+            "Linear combination file");
+        pos.add("linear_combination_file", 1);
+    }
+
+    void parse_suboptions(
+        const boost::program_options::variables_map &vm) override
+    {
+        if (0 == vm.count("linear_combination_file")) {
+            throw po::error("linear_combination file not specified");
+        }
+        linear_combination_file =
+            vm["linear_combination_file"].as<std::string>();
+
+        out_file =
+            vm.count("out") ? vm["out"].as<std::string>() : "mpc-phase2.bin";
+    }
+
+    void subcommand_usage() override
+    {
+        std::cout << "Usage:" << std::endl
+                  << "  " << subcommand_name
+                  << " [<options>] <linear_combination_file>\n";
+    }
+
+    int execute_subcommand() override
+    {
+        if (verbose) {
+            std::cout << "linear_combination_file: " << linear_combination_file
+                      << "\n"
+                      << "out_file: " << out_file << std::endl;
+        }
+
+        // Load the linear_combination output
+        libff::enter_block("reading linear combination data");
+        srs_mpc_layer_L1<ppT> lin_comb =
+            read_from_file<srs_mpc_layer_L1<ppT>>(linear_combination_file);
+        libff::leave_block("reading linear combination data");
+
+        // Generate the zeth circuit (to determine the number of inputs)
+        const size_t num_inputs = []() {
+            libsnark::protoboard<FieldT> pb;
+            joinsplit_gadget<
+                FieldT,
+                HashT,
+                HashTreeT,
+                ZETH_NUM_JS_INPUTS,
+                ZETH_NUM_JS_OUTPUTS>
+                js(pb);
+            const libsnark::r1cs_constraint_system<FieldT> cs =
+                pb.get_constraint_system();
+            return cs.num_inputs();
+        }();
+
+        // Generate the artifical delta
+        const FieldT delta = FieldT::random_element();
+
+        // Generate and save the dummy phase2 data
+        const srs_mpc_layer_C2<ppT> phase2 =
+            mpc_dummy_layer_C2<ppT>(lin_comb, delta, num_inputs);
+        libff::enter_block("writing phase2 data");
+        {
+            std::ofstream out(out_file);
+            phase2.write(out);
+        }
+        libff::leave_block("writing phase2 data");
+
+        return 0;
+    }
+};
 
 } // namespace
 
-int zeth_mpc_dummy_phase2(const std::vector<std::string> &args)
-{
-    cli_options options;
-    try {
-        options.parse(args);
-    } catch (po::error &error) {
-        std::cerr << " ERROR: " << error.what() << std::endl;
-        std::cout << std::endl;
-        options.usage();
-        return 1;
-    }
-
-    // Execute and handle errors
-    try {
-        return zeth_mpc_dummy_phase2_main(options);
-    } catch (std::invalid_argument &error) {
-        std::cerr << " ERROR: " << error.what() << std::endl;
-        std::cout << std::endl;
-        return 1;
-    }
-}
+subcommand *mpc_dummy_phase2_cmd = new mpc_dummy_phase2();
