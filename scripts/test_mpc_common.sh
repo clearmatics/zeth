ee7326424dbca034f7218778fdf10e706144a74c
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-09-26 15:04:35 +0100

    WIP: notify handler at end of MPC.  tests.

---
+++ scripts/test_mpc_common.sh
@@ -0,0 +1,10 @@
+
+SERVER_DIR=test_server
+PRV_KEY_1=testdata/mpc_key1.bin
+PUB_KEY_1=testdata/mpc_key1.pub
+PRV_KEY_2=testdata/mpc_key2.bin
+PUB_KEY_2=testdata/mpc_key2.pub
+PRV_KEY_3=testdata/mpc_key3.bin
+PUB_KEY_3=testdata/mpc_key3.pub
+PRV_KEY_4=testdata/mpc_key4.bin
+PUB_KEY_4=testdata/mpc_key4.pub
