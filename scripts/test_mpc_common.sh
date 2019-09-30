67b5366727870139f023ca780b23588e7e70817e
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-09-30 10:26:34 +0100

    mpc: WIP phase2_server with tests passing

--- scripts/test_mpc_common.sh
+++ scripts/test_mpc_common.sh
@@ -1,16 +1,37 @@
+# Values common to mpc server testing scripts
 
-SERVER_DIR=_test_server_data
+# Directories
+TEST_DATA_DIR=`pwd`/testdata
+BIN_DIR=`pwd`/build/src
+SERVER_DIR=`pwd`/_test_server_data
 
-SERVER_CONFIG_TEMPLATE=testdata/mpc_server_config.json
+# Server files
+SERVER_CONFIG_TEMPLATE=${TEST_DATA_DIR}/mpc_server_config.json
+CHALLENGE_0_FILE=${SERVER_DIR}/challenge_0.bin
+TRANSCRIPT_FILE=${SERVER_DIR}/transcript.bin
+FINAL_OUTPUT_FILE=${SERVER_DIR}/final_output.bin
+FINAL_TRANSCRIPT_FILE=${SERVER_DIR}/final_transcript.bin
 
+# Commands
+POT="${BIN_DIR}/pot-process"
+MPC="${BIN_DIR}/mpc/mpc-test"
+QAP_DEGREE=8
+
+# Server address (consistent with server config template)
 HOST=localhost
 PORT=8001
 
-PRV_KEY_1=testdata/mpc_key1.bin
-PUB_KEY_1=testdata/mpc_key1.pub
-PRV_KEY_2=testdata/mpc_key2.bin
-PUB_KEY_2=testdata/mpc_key2.pub
-PRV_KEY_3=testdata/mpc_key3.bin
-PUB_KEY_3=testdata/mpc_key3.pub
-PRV_KEY_4=testdata/mpc_key4.bin
-PUB_KEY_4=testdata/mpc_key4.pub
+# Client keys (consistent with server config template)
+PRV_KEY_1=${TEST_DATA_DIR}/mpc_key1.bin
+PUB_KEY_1=${TEST_DATA_DIR}/mpc_key1.pub
+PRV_KEY_2=${TEST_DATA_DIR}/mpc_key2.bin
+PUB_KEY_2=${TEST_DATA_DIR}/mpc_key2.pub
+PRV_KEY_3=${TEST_DATA_DIR}/mpc_key3.bin
+PUB_KEY_3=${TEST_DATA_DIR}/mpc_key3.pub
+PRV_KEY_4=${TEST_DATA_DIR}/mpc_key4.bin
+PUB_KEY_4=${TEST_DATA_DIR}/mpc_key4.pub
+
+# Get server state
+function get_state() {
+    curl --fail http://${HOST}:${PORT}/state
+}
---
+++ src/mpc/coordinator/phase2_contribution_handler.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+"""
+Implementation of Phase2ContributionHandler
+"""
+
+from coordinator.icontributionhandler import IContributionHandler
+from coordinator.mpc_command import MPCCommand
+from os.path import exists
+from os import rename
+
+
+CHALLENGE_0_FILE = "challenge_0.bin"
+NEXT_CHALLENGE_FILE = "next_challenge.bin"
+NEW_CHALLENGE_FILE = "new_challenge.bin"
+TRANSCRIPT_FILE = "transcript.bin"
+FINAL_OUTPUT = "final_output.bin"
+FINAL_TRANSCRIPT = "final_transcript.bin"
+
+
+class Phase2ContributionHandler(IContributionHandler):
+    """
+    Handler processing phase2 challenges and contributions.
+    """
+
+    def __init__(self) -> None:
+        # Sanity check
+        if not exists(CHALLENGE_0_FILE):
+            raise Exception(f"no {CHALLENGE_0_FILE} found in server dir")
+
+        # If there is no NEXT_CHALLENGE, there should also be no TRANSCRIPT
+        if not exists(NEXT_CHALLENGE_FILE):
+            if exists(TRANSCRIPT_FILE):
+                raise Exception(f"unexpected {TRANSCRIPT_FILE} in server dir")
+
+        self.mpc = MPCCommand()
+
+    def get_current_challenge_file(self, contributor_idx: int) -> str:
+        # If there is no NEXT_CHALLENGE_FILE, use CHALLENGE_0_FILE.  (Note,
+        # contributor_idx may be > 0, even if there is no NEXT_CHALLENGE_FILE.
+        # The only condition related ot contributor_idx is that, if
+        # contributor_idx is 0, we MUST ONLY have the initial challenge.)
+        have_next_challenge = exists(NEXT_CHALLENGE_FILE)
+        if have_next_challenge:
+            if 0 == contributor_idx:
+                raise Exception(
+                    f"unexpected {NEXT_CHALLENGE_FILE} for 0-th contributor")
+            return NEXT_CHALLENGE_FILE
+        return CHALLENGE_0_FILE
+
+    def process_contribution(
+            self, contribution_idx: int, file_name: str) -> bool:
+        orig_challenge = self.get_current_challenge_file(contribution_idx)
+        contribution_valid = self.mpc.phase2_verify_contribution(
+            orig_challenge=orig_challenge,
+            response=file_name,
+            out_new_challenge=NEW_CHALLENGE_FILE,
+            transcript=TRANSCRIPT_FILE)
+
+        if contribution_valid:
+            if not exists(NEW_CHALLENGE_FILE):
+                raise Exception("unknown error creating new challenge")
+
+            # Contribution has been recorded in TRANSCRIPT_FILE.  Replace
+            # NEXT_CHALLENGE_FILE with NEW_CHALLENGE_FILE.
+            rename(NEW_CHALLENGE_FILE, NEXT_CHALLENGE_FILE)
+            return True
+
+        return False
+
+    def on_completed(self) -> None:
+        # Confirm that there has been at least one contribution, otherwise the
+        # MPC is invalid.
+        if not exists(NEXT_CHALLENGE_FILE):
+            raise Exception("no contributions made")
+
+        # Perform a validation of the full transcript
+        mpc_valid = self.mpc.phase2_verify_transcript(
+            CHALLENGE_0_FILE,
+            NEXT_CHALLENGE_FILE,
+            TRANSCRIPT_FILE)
+        if not mpc_valid:
+            raise Exception("error in MPC transcript")
+
+        # If all is well, move the final challenge file
+        rename(NEXT_CHALLENGE_FILE, FINAL_OUTPUT)
+        rename(TRANSCRIPT_FILE, FINAL_TRANSCRIPT)
