37f60cdbd059c1aea44513e2882bb51026f7e18c
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-10-08 13:58:31 +0100

    mpc: refactor tests for working phase1 and phase2 server tests

--- scripts/test_mpc_common.sh
+++ scripts/test_mpc_common.sh
@@ -14,26 +14,29 @@ set -x
 # Directories
 TEST_DATA_DIR=`pwd`/testdata
 BIN_DIR=`pwd`/build/src
-SERVER_DIR=`pwd`/_test_server_data
+PHASE1_SERVER_DIR=`pwd`/_test_server_phase1
+PHASE2_SERVER_DIR=`pwd`/_test_server_phase2
 
 # Server files
 SERVER_CONFIG_TEMPLATE=${TEST_DATA_DIR}/mpc_server_config.json
-CHALLENGE_0_FILE=${SERVER_DIR}/challenge_0.bin
-TRANSCRIPT_FILE=${SERVER_DIR}/transcript.bin
-FINAL_OUTPUT_FILE=${SERVER_DIR}/final_output.bin
-FINAL_TRANSCRIPT_FILE=${SERVER_DIR}/final_transcript.bin
+CHALLENGE_0_FILE=challenge_0.bin
+TRANSCRIPT_FILE=transcript
+FINAL_OUTPUT_FILE=final_output.bin
+FINAL_TRANSCRIPT_FILE=final_transcript.bin
 
-SERVER_KEY=${SERVER_DIR}/key.pem
-SERVER_CERT=${SERVER_DIR}/cert.pem
+SERVER_KEY=key.pem
+SERVER_CERT=cert.pem
 
 # Commands
-POT="${BIN_DIR}/pot-process"
+POT_PROCESS="${BIN_DIR}/pot-process"
 MPC="${BIN_DIR}/mpc/mpc-test"
+POT_DIR=`pwd`"/../powersoftau"
 QAP_DEGREE=8
 
 # Server address (consistent with server config template)
 HOST=localhost
-PORT=8001
+PHASE1_PORT=8001
+PHASE2_PORT=8002
 
 # Client keys (consistent with server config template)
 PRV_KEY_1=${TEST_DATA_DIR}/mpc_key1.bin
@@ -45,23 +48,22 @@ PUB_KEY_3=${TEST_DATA_DIR}/mpc_key3.pub
 PRV_KEY_4=${TEST_DATA_DIR}/mpc_key4.bin
 PUB_KEY_4=${TEST_DATA_DIR}/mpc_key4.pub
 
-# Get server state
-function get_state() {
-    curl --cacert ${SERVER_CERT} --fail https://${HOST}:${PORT}/state
-}
+# 1 - server_dir
+# 2 - port
+function prepare_server_common() {
 
-function prepare_server() {
-    rm -rf ${SERVER_DIR}/server_state.json
-    rm -rf ${SERVER_DIR}/server_config.json
+    mkdir -p $1
+    pushd $1
+
+    rm -rf server_state.json
+    rm -rf server_config.json
     rm -rf ${TRANSCRIPT_FILE} ${FINAL_OUTPUT_FILE} ${FINAL_TRANSCRIPT_FILE} \
-       ${SERVER_DIR}/next_challenge.bin
-    mkdir -p ${SERVER_DIR}
-    pushd ${SERVER_DIR}
+       next_challenge.bin
 
     # Config
     now=`python -c 'import time; print(time.strftime("%Y-%m-%d %H:%M:%S"))'`
-    sed -e "s/TIME/${now}/g" ${SERVER_CONFIG_TEMPLATE} \
-        > ${SERVER_DIR}/server_config.json
+    sed -e "s/TIME/${now}/g" -e "s/PORT/$2/g" ${SERVER_CONFIG_TEMPLATE} \
+        > server_config.json
 
     # TLS server certs
     if ! [ -e ${SERVER_KEY} ] || ! [ -e ${SERVER_CERT} ] ; then
@@ -83,42 +85,31 @@ function prepare_server() {
                 -days 365
     fi
 
-    if ! [ -e ${CHALLENGE_0_FILE} ] ; then
-        echo Initial challenge ...
-        # Set up phase2
-        pot_file=${SERVER_DIR}/_test_pot-${QAP_DEGREE}.bin
-        lagrange_file=${SERVER_DIR}/_test_lagrange-${QAP_DEGREE}.bin
-        linear_combination_file=${SERVER_DIR}/_test_linear_combination-${QAP_DEGREE}.bin
-        # keypair_file=${SERVER_DIR}/_test_keypair-${QAP_DEGREE}.bin
-
-        ${POT} --dummy ${pot_file} ${QAP_DEGREE}
-        ${POT} --out ${lagrange_file} ${pot_file} ${QAP_DEGREE}
-        ${MPC} linear-combination --out ${linear_combination_file} \
-               ${pot_file} ${lagrange_file}
-        ${MPC} phase2-begin \
-               --out ${CHALLENGE_0_FILE} ${linear_combination_file}
-    fi
-
     popd
 }
 
-function start_server() {
-    pushd ${SERVER_DIR}
+# 1 - server dir
+# 2 - command
+# 3 - get_state function
+function start_server_common() {
+    pushd $1
 
-    phase2_server > server.log &
+    $2 > server.log &
     echo $! > server.pid
 
-    while ! get_state ; do
+    while ! $3 ; do
         echo "TEST: waiting for server to start ..."
         sleep 1
     done
     echo "TEST: server up (pid: "`cat server.pid`")"
     popd
+
 }
 
-function stop_server() {
-    if [ -d ${SERVER_DIR} ] ; then
-        pushd ${SERVER_DIR}
+# 1 - server dir
+function stop_server_common() {
+    if [ -d $1 ] ; then
+        pushd $1
         if [ -e server.pid ] ; then
             pid=`cat server.pid`
             echo "TEST: Stopping server (pid: "${pid}")"
@@ -130,6 +121,69 @@ function stop_server() {
         fi
         popd
     fi
+}
+
+function get_state_phase1() {
+    curl \
+        --cacert ${PHASE1_SERVER_DIR}/${SERVER_CERT} \
+        --fail https://${HOST}:${PHASE1_PORT}/state
+}
+
+function get_state_phase2() {
+    curl \
+        --cacert ${PHASE2_SERVER_DIR}/${SERVER_CERT} \
+        --fail https://${HOST}:${PHASE2_PORT}/state
+}
+
+function prepare_server_phase1() {
+    prepare_server_common ${PHASE1_SERVER_DIR} ${PHASE1_PORT}
+}
+
+function prepare_server_phase2() {
+    prepare_server_common ${PHASE2_SERVER_DIR} ${PHASE2_PORT}
+
+    # Perform the phase2-specific setup based on POT data
+    pushd ${PHASE2_SERVER_DIR}
+    if ! [ -e ${CHALLENGE_0_FILE} ] ; then
+
+        if [ -e ${PHASE1_SERVER_DIR}/${FINAL_OUTPUT_FILE} ] ; then
+            echo "Creating initial challenge (from POT MPC) ..."
+           pot_file=${PHASE1_SERVER_DIR}/${FINAL_OUTPUT_FILE}
+        else
+            echo "Creating initial challenge (from dummy POT) ..."
+           pot_file=_test_pot-${QAP_DEGREE}.bin
+           ${POT_PROCESS} --dummy ${pot_file} ${QAP_DEGREE}
+        fi
+
+        lagrange_file=_test_lagrange-${QAP_DEGREE}.bin
+        linear_combination_file=_test_linear_combination-${QAP_DEGREE}.bin
+
+        ${POT_PROCESS} --out ${lagrange_file} ${pot_file} ${QAP_DEGREE}
+        ${MPC} linear-combination --out ${linear_combination_file} \
+               ${pot_file} ${lagrange_file}
+        ${MPC} phase2-begin \
+               --out ${CHALLENGE_0_FILE} ${linear_combination_file}
+    fi
+    popd
+}
+
+function start_server_phase1() {
+    start_server_common \
+        ${PHASE1_SERVER_DIR} \
+        "phase1_server -n ${QAP_DEGREE}" \
+        get_state_phase1
+}
+
+function start_server_phase2() {
+    start_server_common ${PHASE2_SERVER_DIR} phase2_server get_state_phase2
+}
+
+function stop_server_phase1() {
+    stop_server_common ${PHASE1_SERVER_DIR}
+}
+
+function stop_server_phase2() {
+    stop_server_common ${PHASE2_SERVER_DIR}
 }
 
 set +x
