f6a21c9aa35dbaf4b65b2c270ac5365b2a7a219b
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-10-08 21:09:45 +0100

    mpc: refactor server_state to hold configuration

--- mpc/coordinator/server_state.py
+++ mpc/coordinator/server_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from __future__ import annotations
+from .server_configuration import JsonDict, Configuration
 from typing import cast
 import json
-from .server_configuration import JsonDict, Configuration
+from logging import error
 
 
 class ServerState(object):
@@ -12,10 +13,12 @@ class ServerState(object):
     """
     def __init__(
             self,
+            configuration: Configuration,
             next_contributor_index: int,
             num_contributors: int,
             next_contributor_deadline: float):
         assert num_contributors != 0
+        self.configuration = configuration
         self.next_contributor_index: int = next_contributor_index
         self.num_contributors: int = num_contributors
         self.next_contributor_deadline: float = next_contributor_deadline
@@ -24,17 +27,20 @@ class ServerState(object):
     def new(configuration: Configuration) -> ServerState:
         assert configuration.start_time != 0.0
         assert configuration.contribution_interval != 0.0
-        return ServerState(
+        state = ServerState(
+            configuration,
             0,
             len(configuration.contributors),
             configuration.start_time + configuration.contribution_interval)
+        state._notify_next_contributor()
+        return state
 
     def to_json(self) -> str:
         return json.dumps(self._to_json_dict())
 
     @staticmethod
-    def from_json(state_json: str) -> ServerState:
-        return ServerState._from_json_dict(json.loads(state_json))
+    def from_json(configuration: Configuration, state_json: str) -> ServerState:
+        return ServerState._from_json_dict(configuration, json.loads(state_json))
 
     def have_all_contributions(self) -> bool:
         """
@@ -42,14 +48,14 @@ class ServerState(object):
         """
         return self.num_contributors <= self.next_contributor_index
 
-    def received_contribution(self, config: Configuration, now: float) -> None:
+    def received_contribution(self, now: float) -> None:
         """
         Update the state after new contribution has been successfully received.
         """
         assert not self.have_all_contributions()
-        self._next_contributor(config, now)
+        self._next_contributor(now)
 
-    def update(self, config: Configuration, now: float) -> bool:
+    def update(self, now: float) -> bool:
         """
         Check whether a contributor has missed his chance.  If the next deadline
         has not passed, do nothing and return False.  If the deadline has
@@ -59,27 +65,35 @@ class ServerState(object):
         if now < self.next_contributor_deadline:
             return False
 
-        self._next_contributor(config, now)
+        self._next_contributor(now)
         return True
 
-    def _next_contributor(self, config: Configuration, now: float) -> None:
-        from .crypto import export_verification_key
+    def _next_contributor(self, now: float) -> None:
 
         self.next_contributor_index = self.next_contributor_index + 1
-        self._update_deadline(config, now)
-        if self.have_all_contributions() or not config.email_server:
+        self._update_deadline(now)
+        self._notify_next_contributor()
+
+    def _notify_next_contributor(self) -> None:
+        from .crypto import export_verification_key
+
+        if self.have_all_contributions() or not self.configuration.email_server:
             return
 
-        contr = config.contributors[self.next_contributor_index]
-
-        _send_mail(
-            email_server=config.email_server,
-            email_address=cast(str, config.email_address),
-            email_password=cast(str, config.email_password),
-            to=contr.email,
-            subject="[MPC] Your contribution window has begun.",
-            body="Please contribute to the MPC using your contribution key: " +
-            export_verification_key(contr.verification_key))
+        contr = self.configuration.contributors[self.next_contributor_index]
+        contr_idx = self.next_contributor_index + 1
+        total = self.num_contributors
+        try:
+            _send_mail(
+                email_server=self.configuration.email_server,
+                email_address=cast(str, self.configuration.email_address),
+                email_password=cast(str, self.configuration.email_password),
+                to=contr.email,
+                subject=f"[MPC] Your timeslot has begun ({contr_idx}/{total})",
+                body="Please contribute to the MPC using your key: " +
+                export_verification_key(contr.verification_key))
+        except Exception as ex:
+            error(f"Failed to notify: {contr.email}: {ex}")
 
     def _to_json_dict(self) -> JsonDict:
         return {
@@ -89,17 +103,20 @@ class ServerState(object):
         }
 
     @staticmethod
-    def _from_json_dict(json_dict: JsonDict) -> ServerState:
+    def _from_json_dict(
+            configuration: Configuration, json_dict: JsonDict) -> ServerState:
         return ServerState(
+            configuration,
             cast(int, json_dict["next_contributor_index"]),
             cast(int, json_dict["num_contributors"]),
             float(cast(str, json_dict["next_contributor_deadline"])))
 
-    def _update_deadline(self, config: Configuration, now: float) -> None:
+    def _update_deadline(self, now: float) -> None:
         if self.have_all_contributions():
             self.next_contributor_deadline = 0.0
         else:
-            self.next_contributor_deadline = now + config.contribution_interval
+            self.next_contributor_deadline = \
+                now + self.configuration.contribution_interval
 
 
 def _send_mail(
