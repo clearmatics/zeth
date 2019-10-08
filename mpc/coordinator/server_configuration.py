0f0099d03e8d9a68b47248cf4e4c649c05d4074a
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-10-08 17:59:03 +0100

    mpc: support for notifying participants by email

--- mpc/coordinator/server_configuration.py
+++ mpc/coordinator/server_configuration.py
@@ -5,7 +5,7 @@ from .crypto import \
     VerificationKey, import_verification_key, export_verification_key
 import json
 import time
-from typing import List, Dict, cast
+from typing import List, Dict, cast, Optional
 
 JsonDict = Dict[str, object]
 
@@ -44,11 +44,24 @@ class Configuration(object):
             contribution_interval: float,
             tls_key: str,
             tls_certificate: str,
-            port: int = 5000):
-        assert 0 != start_time
+            port: int = 5000,
+            email_server: Optional[str] = None,
+            email_address: Optional[str] = None,
+            email_password: Optional[str] = None):
+        if 0 == start_time:
+            raise Exception("invalid start time")
+        if (email_server or email_address or email_password) and \
+           (not (email_server and email_address and email_password)):
+            raise Exception(
+                "must all or none of email server, address and password " +
+                "in config")
+
         self.contributors: List[Contributor] = contributors
         self.start_time: float = float(start_time)
         self.contribution_interval: float = float(contribution_interval)
+        self.email_server: Optional[str] = email_server
+        self.email_address: Optional[str] = email_address
+        self.email_password: Optional[str] = email_password
         self.tls_key: str = tls_key
         self.tls_certificate: str = tls_certificate
         self.port = port
@@ -66,6 +79,9 @@ class Configuration(object):
             "contributors": [c._to_json_dict() for c in self.contributors],
             "start_time": time.strftime(TIME_FORMAT, start_local),
             "contribution_interval": str(self.contribution_interval),
+            "email_server": self.email_server,
+            "email_address": self.email_address,
+            "email_password": self.email_password,
             "tls_key": self.tls_key,
             "tls_certificate": self.tls_certificate,
             "port": self.port,
@@ -81,6 +97,9 @@ class Configuration(object):
             [Contributor._from_json_dict(c) for c in contributors_json_list],
             time.mktime(start_local),
             float(cast(str, json_dict["contribution_interval"])),
+            email_server=cast(str, json_dict.get("email_server", None)),
+            email_address=cast(str, json_dict.get("email_address", None)),
+            email_password=cast(str, json_dict.get("email_password", None)),
             tls_key=cast(str, json_dict["tls_key"]),
             tls_certificate=cast(str, json_dict["tls_certificate"]),
             port=int(cast(int, json_dict["port"])))
