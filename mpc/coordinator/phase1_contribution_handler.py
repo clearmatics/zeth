8e2e51334e66fe2465233fcc37aba9b5dff881b3
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-10-14 14:46:22 +0100

    mpc: powersoftau invocation based on number of contributions

--- mpc/coordinator/phase1_contribution_handler.py
+++ mpc/coordinator/phase1_contribution_handler.py
@@ -4,21 +4,61 @@
 Implementation of Phase1ContributionHandler
 """
 
+from __future__ import annotations
+from .server_configuration import JsonDict
 from .icontributionhandler import IContributionHandler
 from .powersoftau_command import \
     PowersOfTauCommand, CHALLENGE_FILE, NEW_CHALLENGE_FILE, RESPONSE_FILE
 from os.path import exists
 from os import rename
-from typing import Optional
+from typing import Optional, cast
+import json
+
+PHASE1_STATE_FILE = "phase1_state.json"
 
 TRANSCRIPT_FILE = "transcript"
 FINAL_OUTPUT = "final_output.bin"
 FINAL_TRANSCRIPT = "final_transcript.bin"
 
 
+class _Phase1State(object):
+    """
+    Internal persisted state model for this handler.
+    """
+
+    def __init__(self, num_valid_contributions: int):
+        self.num_valid_contributions = num_valid_contributions
+
+    @staticmethod
+    def new() -> _Phase1State:
+        return _Phase1State(0)
+
+    def on_valid_contribution(self) -> None:
+        self.num_valid_contributions = self.num_valid_contributions + 1
+
+    def to_json(self) -> str:
+        return json.dumps(self._to_json_dict())
+
+    @staticmethod
+    def from_json(state_json: str) -> _Phase1State:
+        return _Phase1State._from_json_dict(json.loads(state_json))
+
+    def _to_json_dict(self) -> JsonDict:
+        return {
+            "num_valid_contributions": self.num_valid_contributions,
+        }
+
+    @staticmethod
+    def _from_json_dict(json_dict: JsonDict) -> _Phase1State:
+        return _Phase1State(
+            cast(int, json_dict["num_valid_contributions"]))
+
+
 class Phase1ContributionHandler(IContributionHandler):
     """
-    Handler processing phase1 (powersoftau) challenges and contributions.
+    Handler processing phase1 (powersoftau) challenges and contributions.  Some
+    complexity is involved, because we need to track the number of valid
+    contributions that have been made.
     """
 
     def __init__(
@@ -27,6 +67,12 @@ class Phase1ContributionHandler(IContributionHandler):
             num_powers: Optional[int] = None) -> None:
 
         self.powersoftau = PowersOfTauCommand(powersoftau_path, num_powers)
+
+        if exists(PHASE1_STATE_FILE):
+            with open(PHASE1_STATE_FILE, "r") as state_f:
+                self.state = _Phase1State.from_json(state_f.read())
+        else:
+            self.state = _Phase1State.new()
 
         # Create challenge file if it does not exist.
         if not exists(CHALLENGE_FILE):
@@ -55,6 +101,10 @@ class Phase1ContributionHandler(IContributionHandler):
             self.powersoftau.append_response_to_transcript(
                 RESPONSE_FILE, TRANSCRIPT_FILE)
 
+            # update internal state
+            self.state.on_valid_contribution()
+            self._save_state()
+
             # move new_challenge ot be the next challenge
             rename(NEW_CHALLENGE_FILE, CHALLENGE_FILE)
             return True
@@ -68,10 +118,15 @@ class Phase1ContributionHandler(IContributionHandler):
             raise Exception("no contributions made")
 
         # Perform a validation of the full transcript
-        mpc_valid = self.powersoftau.verify_transcript()
+        mpc_valid = self.powersoftau.verify_transcript(
+            self.state.num_valid_contributions)
         if not mpc_valid:
             raise Exception("error in MPC transcript")
 
         # If all is well, move the final challenge file
         rename(CHALLENGE_FILE, FINAL_OUTPUT)
         rename(TRANSCRIPT_FILE, FINAL_TRANSCRIPT)
+
+    def _save_state(self) -> None:
+        with open(PHASE1_STATE_FILE, "w") as state_f:
+            state_f.write(self.state.to_json())
