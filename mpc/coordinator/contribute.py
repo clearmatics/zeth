fd23300540be3e89063592ef86089a25fd0711b3
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-10-21 11:38:33 +0100

    mpc: factor out contribution upload.  conistent challenge filenames.

--- mpc/coordinator/contribute.py
+++ mpc/coordinator/contribute.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
+"""
+Functions performing the common steps of contribution: download challenge,
+sign and upload response.
+"""
+
 from coordinator.client import Client
 from coordinator.crypto import \
-    compute_file_digest, import_signing_key, get_verification_key, sign
+    compute_file_digest, import_signing_key, get_verification_key, sign, \
+    SigningKey
 from typing import Callable, Optional
+
+
+def _upload_response(client: Client, response_file: str, sk: SigningKey) -> None:
+    # Compute digest and sign
+    digest = compute_file_digest(response_file)
+    signature = sign(sk, digest)
+    vk = get_verification_key(sk)
+
+    # Upload
+    client.push_contribution(response_file, digest, vk, signature)
+
+
+def upload_response(client: Client, response_file: str, key_file: str) -> None:
+    """
+    Given some response file and a key, sign the response and upload the
+    coordinator connected to by client.
+    """
+    with open(key_file, "rb") as key_f:
+        sk = import_signing_key(key_f.read())
+    _upload_response(client, response_file, sk)
 
 
 def contribute(
@@ -22,18 +48,14 @@ def contribute(
         sk = import_signing_key(key_f.read())
     print("got key")
 
-    # Get challenge
     client = Client(base_url, server_certificate, insecure)
+
+    # Get challenge
     client.get_challenge(challenge_file)
     print("got challenge")
 
     # Perform the contribution
     response_file = contribute()
 
-    # Compute digest and sign
-    digest = compute_file_digest(response_file)
-    signature = sign(sk, digest)
-    vk = get_verification_key(sk)
-
-    # Upload
-    client.push_contribution(response_file, digest, vk, signature)
+    # Sign and upload
+    _upload_response(client, response_file, sk)
