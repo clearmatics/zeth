fb567a5e64837e6812632078487806ab67c45c18
Duncan Tebbs <duncan.tebbs@gmail.com>
2019-12-10 22:07:06 +0000

    contracts: some indentation consistency

--- zeth-contracts/contracts/MerkleTreeMiMC7.sol
+++ zeth-contracts/contracts/MerkleTreeMiMC7.sol
@@ -9,56 +9,55 @@ import "./MiMC7.sol";
 
 contract MerkleTreeMiMC7 is BaseMerkleTree {
 
-  constructor(uint treeDepth)
-      BaseMerkleTree(treeDepth) public {
-  }
-
-  // Returns the current merkle tree
-  function getTree() public view returns (bytes32[] memory) {
-    uint nbNodes = 2**(depth + 1) - 1;
-    bytes32[] memory tmpTree = new bytes32[](nbNodes);
-    bytes32 left;
-    bytes32 right;
-    // Dump the leaves in the right indexes in the tree
-    for (uint i; i < nbLeaves; i++) {
-      tmpTree[(nbLeaves - 1) + i] = leaves[i];
+    constructor(uint treeDepth) BaseMerkleTree(treeDepth) public {
     }
 
-    // Compute the internal nodes of the merkle tree
-    for (uint i = nbLeaves - 2; i > 0; i--) {
-      left = tmpTree[2*i+1];
-      right = tmpTree[2*(i+1)];
+    // Returns the current merkle tree
+    function getTree() public view returns (bytes32[] memory) {
+        uint nbNodes = 2**(depth + 1) - 1;
+        bytes32[] memory tmpTree = new bytes32[](nbNodes);
+        bytes32 left;
+        bytes32 right;
+        // Dump the leaves in the right indexes in the tree
+        for (uint i = 0; i < nbLeaves; i++) {
+            tmpTree[(nbLeaves - 1) + i] = leaves[i];
+        }
 
-      // Seed is hardcoded and given by "clearmatics_mt_seed"
-      tmpTree[i] = MiMC7.hash(left, right);
+        // Compute the internal nodes of the merkle tree
+        for (uint i = nbLeaves - 2; i > 0; i--) {
+            left = tmpTree[2*i+1];
+            right = tmpTree[2*(i+1)];
+
+            // Seed is hardcoded and given by "clearmatics_mt_seed"
+            tmpTree[i] = MiMC7.hash(left, right);
+        }
+
+        // Compute the merkle root
+        left = tmpTree[1];
+        right = tmpTree[2];
+        tmpTree[0] = MiMC7.hash(left, right);
+
+        return tmpTree;
     }
 
-    // Compute the merkle root
-    left = tmpTree[1];
-    right = tmpTree[2];
-    tmpTree[0] = MiMC7.hash(left, right);
+    // Returns the root of the merkle tree
+    function getRoot() public view returns(bytes32) {
+        uint layerSize = nbLeaves / 2;
+        bytes32[nbLeaves/2] memory pad;
 
-    return tmpTree;
-  }
+        // Compute first layer from storage
+        for (uint i = 0 ; i < layerSize ; ++i) {
+            pad[i] = MiMC7.hash(leaves[2*i], leaves[2*i + 1]);
+        }
+        layerSize = layerSize / 2;
 
-  // Returns the root of the merkle tree
-  function getRoot() public view returns(bytes32) {
-      uint layerSize = nbLeaves / 2;
-      bytes32[] memory pad = new bytes32[](nbLeaves/2);
+        // Compute successive layers from their parents, in-place.
+        for ( ; layerSize > 0 ; layerSize = layerSize / 2) {
+            for (uint i = 0 ; i < layerSize ; ++i) {
+                pad[i] = MiMC7.hash(pad[2*i], pad[2*i + 1]);
+            }
+        }
 
-      // Compute first layer from storage
-      for (uint i = 0 ; i < layerSize ; ++i) {
-          pad[i] = MiMC7.hash(leaves[2*i], leaves[2*i + 1]);
-      }
-      layerSize = layerSize / 2;
-
-      // Compute successive layers from their parents, in-place.
-      for ( ; layerSize > 0 ; layerSize = layerSize / 2) {
-          for (uint i = 0 ; i < layerSize ; ++i) {
-              pad[i] = MiMC7.hash(pad[2*i], pad[2*i + 1]);
-          }
-      }
-
-      return pad[0];
-  }
+        return pad[0];
+    }
 }
