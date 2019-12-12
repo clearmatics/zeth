84b8164f0e859a4008aa609f9c08aff88cf4bd0b
rrtoledo <raphael.r.toledo@gmail.com>
2019-12-12 17:25:30 +0000

    adding unit test

---
+++ pyClient/test/test_signing.py
@@ -0,0 +1,33 @@
+from zeth import signing
+from hashlib import sha256
+from unittest import TestCase
+from os import urandom
+
+
+class TestSigning(TestCase):
+
+    def test_sign_verify(self) -> None:
+        """
+        Test the correct signing-verification flow:
+        verify(vk, sign(sk,m), m) = 1
+        """
+        m = sha256("clearmatics".encode()).digest()
+        keypair = signing.key_gen()
+        sigma = signing.sign(keypair.sk, m)
+        self.assertTrue(signing.verify(keypair.vk, m, sigma))
+
+        keypair2 = signing.key_gen()
+        self.assertFalse(signing.verify(keypair2.vk, m, sigma))
+
+    def test_sign_verify_random(self) -> None:
+        """
+        Test the correct signing-verification flow with random message:
+        verify(vk, sign(sk,m), m) = 1
+        """
+        m = urandom(32)
+        keypair = signing.key_gen()
+        sigma = signing.sign(keypair.sk, m)
+        self.assertTrue(signing.verify(keypair.vk, m, sigma))
+
+        keypair2 = signing.key_gen()
+        self.assertFalse(signing.verify(keypair2.vk, m, sigma))
