1068f1b8999467198f880149a115e6be896764c8
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-02-26 15:13:53 +0000

    Updated headers to 2020

--- pyClient/api/__init__.py
+++ pyClient/api/__init__.py
@@ -1,3 +1,3 @@
-# Copyright (c) 2015-2019 Clearmatics Technologies Ltd
+# Copyright (c) 2015-2020 Clearmatics Technologies Ltd
 #
 # SPDX-License-Identifier: LGPL-3.0+
--- pyClient/commands/constants.py
+++ pyClient/commands/constants.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2019 Clearmatics Technologies Ltd
+# Copyright (c) 2015-2020 Clearmatics Technologies Ltd
 #
 # SPDX-License-Identifier: LGPL-3.0+
 
