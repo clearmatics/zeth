f2184f6aea3fd3fa53a4f60b45f2e14d1256e015
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-04-23 13:02:27 +0100

    pyclient: use size constants in zeth note serialization

--- pyClient/zeth/mixer_client.py
+++ pyClient/zeth/mixer_client.py
@@ -34,6 +34,17 @@ from typing import Tuple, Dict, List, Callable, Optional, Any
 
 ZERO_UNITS_HEX = "0000000000000000"
 
+# ZethNote binary serialization format:
+#   [apk   : APK_LENGTH_BYTES]
+#   [value : PUBLIC_VALUE_LENGTH_BYTES]
+#   [rho   : RHO_LENGTH_BYTES]
+#   [trapr : TRAPR_LENGTH_BYTES]
+_APK_OFFSET_BYTES = 0
+_VALUE_OFFSET_BYTES = _APK_OFFSET_BYTES + constants.APK_LENGTH_BYTES
+_RHO_OFFSET_BYTES = _VALUE_OFFSET_BYTES + constants.PUBLIC_VALUE_LENGTH_BYTES
+_TRAPR_OFFSET_BYTES = _RHO_OFFSET_BYTES + constants.RHO_LENGTH_BYTES
+assert _TRAPR_OFFSET_BYTES + constants.TRAPR_LENGTH_BYTES \
+    == constants.NOTE_LENGTH_BYTES
 
 # JoinSplit Signature Keys definitions
 JoinsplitSigVerificationKey = signing.SigningVerificationKey
@@ -96,16 +107,6 @@ def zeth_note_to_json_dict(zeth_note_grpc_obj: ZethNote) -> Dict[str, str]:
     }
 
 
-def zeth_note_to_bytes(zeth_note_grpc_obj: ZethNote) -> bytes:
-    apk_bytes = bytes.fromhex(zeth_note_grpc_obj.apk)
-    value_bytes = bytes.fromhex(zeth_note_grpc_obj.value)
-    rho_bytes = bytes.fromhex(zeth_note_grpc_obj.rho)
-    trap_r_bytes = bytes.fromhex(zeth_note_grpc_obj.trap_r)
-    note_bytes = apk_bytes+value_bytes+rho_bytes+trap_r_bytes
-    assert len(note_bytes) == (constants.NOTE_LENGTH_BYTES)
-    return note_bytes
-
-
 def zeth_note_from_json_dict(parsed_zeth_note: Dict[str, str]) -> ZethNote:
     note = ZethNote(
         apk=parsed_zeth_note["a_pk"],
@@ -116,18 +117,31 @@ def zeth_note_from_json_dict(parsed_zeth_note: Dict[str, str]) -> ZethNote:
     return note
 
 
-def zeth_note_from_bytes(zeth_note_bytes: bytes) -> ZethNote:
-    if len(zeth_note_bytes) != (constants.NOTE_LENGTH_BYTES):
+def zeth_note_to_bytes(zeth_note_grpc_obj: ZethNote) -> bytes:
+    apk_bytes = bytes.fromhex(zeth_note_grpc_obj.apk)
+    value_bytes = bytes.fromhex(zeth_note_grpc_obj.value)
+    rho_bytes = bytes.fromhex(zeth_note_grpc_obj.rho)
+    trap_r_bytes = bytes.fromhex(zeth_note_grpc_obj.trap_r)
+    note_bytes = apk_bytes + value_bytes + rho_bytes + trap_r_bytes
+    assert len(note_bytes) == (constants.NOTE_LENGTH_BYTES)
+    return note_bytes
+
+
+def zeth_note_from_bytes(note_bytes: bytes) -> ZethNote:
+    if len(note_bytes) != (constants.NOTE_LENGTH_BYTES):
         raise ValueError(
-            f"note_bytes len {len(zeth_note_bytes)}, "
+            f"note_bytes len {len(note_bytes)}, "
             f"(expected {constants.NOTE_LENGTH_BYTES})")
-    note = ZethNote(
-        apk=zeth_note_bytes[:32].hex(),
-        value=zeth_note_bytes[32:40].hex(),
-        rho=zeth_note_bytes[40:72].hex(),
-        trap_r=zeth_note_bytes[72:].hex()
-    )
-    return note
+    apk = note_bytes[
+        _APK_OFFSET_BYTES:_APK_OFFSET_BYTES + constants.APK_LENGTH_BYTES]
+    value = note_bytes[
+        _VALUE_OFFSET_BYTES:
+        _VALUE_OFFSET_BYTES + constants.PUBLIC_VALUE_LENGTH_BYTES]
+    rho = note_bytes[
+        _RHO_OFFSET_BYTES:_RHO_OFFSET_BYTES + constants.RHO_LENGTH_BYTES]
+    trap_r = note_bytes[_TRAPR_OFFSET_BYTES:]
+    return ZethNote(
+        apk=apk.hex(), value=value.hex(), rho=rho.hex(), trap_r=trap_r.hex())
 
 
 def compute_commitment(zeth_note: ZethNote) -> bytes:
