31cdc632d5468431c8f59b71b142285794ab846a
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-05-04 13:45:24 +0100

    Added documentation cmake configuration

---
+++ cmake/documentation.cmake
@@ -0,0 +1,38 @@
+# Generate code documentation for the project.
+# We use Doxygen.
+# TODO: Consider using: https://codedocs.xyz/
+#
+# The following is adapted from:
+# https://vicrucann.github.io/tutorials/quick-cmake-doxygen/
+
+
+# Build the documentation by default
+option(
+  BUILD_DOC
+  "Build documentation"
+  ON
+)
+
+# Check if Doxygen is installed
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+  # Set input and output files
+  set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in)
+  set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+
+  # Request to configure the file
+  configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+  message("Doxygen build started")
+
+  # The option ALL allows to build the docs together with the application
+  add_custom_target(
+    doc_doxygen
+    ALL
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM
+  )
+else(DOXYGEN_FOUND)
+    message("You need to install Doxygen to generate the documentation")
+endif(DOXYGEN_FOUND)
