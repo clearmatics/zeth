ac350b8257ad632649b742f29d4199cdcf12c2ff
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-05-05 22:47:20 +0100

    Used cmake_cxx_clang_tidy instead of custom target

--- cmake/clang_dev_tools.cmake
+++ cmake/clang_dev_tools.cmake
@@ -7,11 +7,12 @@
 # - include-what-you-use
 
 # Get c++ files across all targets
-file(GLOB_RECURSE
-  ALL_CXX_SOURCE_FILES
-  libzeth/*.?pp
-  prover_server/*.?pp
-  mpc_tools/*.?pp
+file(
+  GLOB_RECURSE
+  ALL_SOURCE_FILES
+  libzeth/**.?pp libzeth/**.tcc
+  prover_server/**.?pp prover_server/**.tcc
+  mpc_tools/**.?pp mpc_tools/**.tcc
 )
 
 # Adding clang-format target if executable is found
@@ -20,35 +21,33 @@ if(CLANG_FORMAT)
   add_custom_target(
     clang-format
     COMMAND clang-format
-    -i
     -style=file
-    ${ALL_CXX_SOURCE_FILES}
+    -i
+    -verbose
+    ${ALL_SOURCE_FILES}
   )
 else()
   message(FATAL_ERROR "clang-format not found. Aborting...")
 endif()
 
 # Adding clang-tidy target if executable is found
-find_program(CLANG_TIDY "clang-tidy")
+# The configuration below requires cmake 3.6.3+
+# (https://cmake.org/cmake/help/v3.6/variable/CMAKE_LANG_CLANG_TIDY.html)
+find_program(CLANG_TIDY clang-tidy DOC "Path to the clang-tidy tool")
 if(CLANG_TIDY)
-  add_custom_target(
-    clang-tidy
-    COMMAND clang-tidy
-    ${ALL_CXX_SOURCE_FILES}
-    -p=${PROJECT_BINARY_DIR}
-    -config=''
-    --
-    -std=c++11
-  )
+  message("Using clang-tidy")
+  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY};")
 else()
-  message(FATAL_ERROR "clang-tidy not found. Aborting...")
+  message("clang-tidy not found. Proceeding without it...")
 endif()
 
 # Adding "include-what-you-use" target if executable is found
+# The configuration below requires cmake 3.3.2+
+# (https://cmake.org/cmake/help/v3.3/variable/CMAKE_LANG_INCLUDE_WHAT_YOU_USE.html)
 find_program(IWYU include-what-you-use DOC "Path to the include-what-you-use tool")
 if(IWYU)
   message("Using include-what-you-use")
   set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE "${IWYU};-Xiwyu;")
 else()
-    message("include-what-you-use not found. Proceeding without it...")
+  message("include-what-you-use not found. Proceeding without it...")
 endif()
