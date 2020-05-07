16cec6bde328f5877ebcc912fd7c08d75a609224
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-05-07 12:00:29 +0100

    Added options for all tools to better control which one is activated

--- cmake/clang_dev_tools.cmake
+++ cmake/clang_dev_tools.cmake
@@ -6,6 +6,22 @@
 # - clang-tidy
 # - include-what-you-use
 
+option(
+  USE_CLANG_FORMAT
+  "Use clang-format if the program is found."
+  OFF
+)
+option(
+  USE_CLANG_TIDY
+  "Use clang-tidy if the program is found."
+  OFF
+)
+option(
+  USE_IWYU
+  "Use IWYU if the program is found."
+  OFF
+)
+
 # Get c++ files across all targets
 file(
   GLOB_RECURSE
@@ -18,16 +34,20 @@ file(
 # Adding clang-format target if executable is found
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
-  add_custom_target(
-    clang-format
-    COMMAND clang-format
-    -style=file
-    -i
-    -verbose
-    ${ALL_SOURCE_FILES}
-  )
+  message("clang-format found!")
+    if(USE_CLANG_FORMAT)
+      message("clang-format found. Creating target...")
+      add_custom_target(
+        clang-format
+        COMMAND clang-format
+        -style=file
+        -i
+        -verbose
+        ${ALL_SOURCE_FILES}
+      )
+    endif()
 else()
-  message(FATAL_ERROR "clang-format not found. Aborting...")
+  message("clang-format not found. Proceeding without it...")
 endif()
 
 # Adding clang-tidy target if executable is found
@@ -35,8 +55,20 @@ endif()
 # (https://cmake.org/cmake/help/v3.6/variable/CMAKE_LANG_CLANG_TIDY.html)
 find_program(CLANG_TIDY clang-tidy DOC "Path to the clang-tidy tool")
 if(CLANG_TIDY)
-  message("Using clang-tidy")
-  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY};")
+  message("clang-tidy found!")
+  if(USE_CLANG_TIDY)
+    message("Using clang-tidy")
+    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY};")
+
+    # Hack to avoid running clang-tidy on generated files (gRPC etc).
+    # To do so, we generate a dummy .clang-tidy config file as done in:
+    # https://gitlab.kitware.com/cmake/cmake/commit/b13bc8659f87567b1b091806d42f5023b2a6b48b
+    file(WRITE "${PROJECT_BINARY_DIR}/.clang-tidy" "
+    ---
+    Checks: '-*,llvm-twine-local'
+    ...
+    ")
+  endif()
 else()
   message("clang-tidy not found. Proceeding without it...")
 endif()
@@ -46,8 +78,11 @@ endif()
 # (https://cmake.org/cmake/help/v3.3/variable/CMAKE_LANG_INCLUDE_WHAT_YOU_USE.html)
 find_program(IWYU include-what-you-use DOC "Path to the include-what-you-use tool")
 if(IWYU)
-  message("Using include-what-you-use")
-  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE "${IWYU};-Xiwyu;")
+  message("IWYU found!")
+  if(USE_IWYU)
+    message("Using include-what-you-use")
+    set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE "${IWYU};-Xiwyu;")
+  endif()
 else()
   message("include-what-you-use not found. Proceeding without it...")
 endif()
--- cmake/gcc_dev_tools.cmake
+++ cmake/gcc_dev_tools.cmake
@@ -4,18 +4,27 @@
 # Tools ran:
 # - cppcheck
 
+option(
+  USE_CPP_CHECK
+  "Use cppcheck if the program is found."
+  OFF
+)
+
 # Adding clang-format target if executable is found
 find_program(CPP_CHECK "cppcheck")
 if(CPP_CHECK)
-  message("cppcheck found. Creating target...")
-  add_custom_target(
-    cppcheck
-    COMMAND cppcheck
-    --project=compile_commands.json
-    --enable=all
-    --quiet
-    -i${DEPENDS_DIR}
-  )
+  message("cppcheck found!")
+  if(USE_CPP_CHECK)
+    message("Using cppcheck. Creating target...")
+    add_custom_target(
+      cppcheck
+      COMMAND cppcheck
+      --project=compile_commands.json
+      --enable=all
+      --quiet
+      -i${DEPENDS_DIR}
+    )
+  endif()
 else()
-  message(FATAL_ERROR "cppcheck not found. Aborting...")
+  message("cppcheck not found. Proceeding without it...")
 endif()
