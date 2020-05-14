d697323e4c273a0cea59e71878be7761101affe8
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-05-14 10:24:47 +0100

    Wrote minimal tests for encoding/decoding of verification keys

---
+++ libzeth/tests/snarks/groth16/groth16_api_handler_test.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2015-2020 Clearmatics Technologies Ltd
+//
+// SPDX-License-Identifier: LGPL-3.0+
+
+#include "libzeth/circuits/circuit_types.hpp"
+#include "libzeth/serialization/proto_utils.hpp"
+#include "libzeth/snarks/groth16/groth16_api_handler.hpp"
+
+#include <gtest/gtest.h>
+
+using ppT = libzeth::ppT;
+using Fr = libff::Fr<libzeth::ppT>;
+using G1 = libff::G1<libzeth::ppT>;
+using G2 = libff::G2<libzeth::ppT>;
+
+namespace
+{
+
+TEST(Groth16ApiHandlerTest, VerificationKeyEncodeDecode)
+{
+    libsnark::r1cs_gg_ppzksnark_verification_key<ppT> initial_vk = 
+        libsnark::r1cs_gg_ppzksnark_verification_key<ppT>::dummy_verification_key(42);
+    
+    zeth_proto::VerificationKey *proto_vk = new zeth_proto::VerificationKey();
+    libzeth::groth16_api_handler<ppT>::verification_key_to_proto(initial_vk, proto_vk);
+
+    libsnark::r1cs_gg_ppzksnark_verification_key<ppT> recovered_vk =
+        libzeth::groth16_api_handler<ppT>::verification_key_from_proto(*proto_vk);
+    
+    ASSERT_EQ(initial_vk, recovered_vk);
+
+    // The destructor of `zeth_proto::VerificationKey` should be
+    // invoked which whould free the memory allocated for the fields of this
+    // message
+    delete proto_vk;
+}
+
+} // namespace
+
+int main(int argc, char **argv)
+{
+    ppT::init_public_params();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
