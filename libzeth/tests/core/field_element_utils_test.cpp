91c4db4cf4fa401c8af276e01c20c7d84d8294ae
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-07-17 15:01:28 +0100

    Fixed tests for encode/decode of exttension field elements

--- libzeth/tests/core/field_element_utils_test.cpp
+++ libzeth/tests/core/field_element_utils_test.cpp
@@ -10,7 +10,6 @@
 using ppT = libzeth::ppT;
 using Fr = libzeth::FieldT;
 using Fqe = libff::Fqe<ppT>;
-using Fqk = libff::Fqk<ppT>;
 
 using bigint_t = libff::bigint<libzeth::FieldT::num_limbs>;
 
@@ -19,50 +18,52 @@ namespace
 
 bigint_t dummy_bigint()
 {
-    Field v = Field::random_element();
+    Fr v = Fr::random_element();
     return v.as_bigint();
 }
 
-Field dummy_field_element() { return Field::random_element(); }
+Fr dummy_field_element() { return Fr::random_element(); }
 
 TEST(FieldElementUtilsTest, BigIntEncodeDecode)
 {
     bigint_t bi = dummy_bigint();
-    std::string bi_hex = libzeth::bigint_to_hex<Field>(bi);
-    bigint_t bi_decoded = libzeth::bigint_from_hex<Field>(bi_hex);
+    std::string bi_hex = libzeth::bigint_to_hex<Fr>(bi);
+    bigint_t bi_decoded = libzeth::bigint_from_hex<Fr>(bi_hex);
     std::cout << "bi_hex: " << bi_hex << std::endl;
-    std::cout << "bi_decoded_hex: " << libzeth::bigint_to_hex<Field>(bi_decoded)
+    std::cout << "bi_decoded_hex: " << libzeth::bigint_to_hex<Fr>(bi_decoded)
               << std::endl;
 
     ASSERT_EQ(2 * sizeof(bi.data), bi_hex.size());
     ASSERT_EQ(bi, bi_decoded);
 }
 
-TEST(FieldElementUtilsTest, FieldElementEncodeDecode)
+TEST(FieldElementUtilsTest, BaseFieldElementEncodeDecode)
 {
     Fr fe = dummy_field_element();
     std::string fe_hex = libzeth::base_field_element_to_hex<Fr>(fe);
     std::cout << "fe_hex: " << fe_hex << std::endl;
-    Field fe_decoded = libzeth::field_element_from_hex<Field>(fe_hex);
+    Fr fe_decoded = libzeth::base_field_element_from_hex<Fr>(fe_hex);
     ASSERT_EQ(fe, fe_decoded);
 }
 
-TEST(FieldElementUtilsTest, FieldElementEncode)
+TEST(FieldElementUtilsTest, ExtFieldElementEncodeDecode)
 {
-    Fr fe = dummy_field_element();
-    std::string fe_hex = libzeth::base_field_element_to_hex<Fr>(fe);
-    std::cout << "fe_hex: " << fe_hex << std::endl;
+    Fqe fe = Fqe::random_element();
+    std::vector<std::string> fe_hex = libzeth::ext_field_element_to_hex<Fqe>(fe);
 
-    Fqe el = Fqe::random_element();
-    std::string el_hex = libzeth::ext_field_element_to_hex<Fqe>(el);
-    std::cout << "el_hex: " << el_hex << std::endl;
+    std::string str;
+    for (const auto &entry : fe_hex) str += ("," + entry);
+    std::cout << "fe_hex: " << str << std::endl;
+
+    Fqe fe_decoded = libzeth::ext_field_element_from_hex<Fqe>(fe_hex);
+    ASSERT_EQ(fe, fe_decoded);
 }
 
 TEST(FieldElementUtilsTest, FieldElementDecodeBadString)
 {
     std::string invalid_hex = "xxx";
     ASSERT_THROW(
-        libzeth::field_element_from_hex<Field>(invalid_hex), std::exception);
+        libzeth::base_field_element_from_hex<Fr>(invalid_hex), std::exception);
 };
 
 } // namespace
