ee1e94ddc19ba94e4afb0100f36a0d8a94ce074b
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-08-27 14:28:57 +0100

    client: fix token_approve command (after move to zeth_helper)

--- client/zeth/helper/token_approve.py
+++ client/zeth/helper/token_approve.py
@@ -2,8 +2,9 @@
 #
 # SPDX-License-Identifier: LGPL-3.0+
 
+from zeth.cli.constants import INSTANCE_FILE_DEFAULT
 from zeth.cli.utils import load_eth_address, load_eth_private_key, \
-    open_web3_from_ctx, load_mixer_description_from_ctx, EtherValue
+    get_eth_network, open_web3_from_network, load_mixer_description, EtherValue
 from zeth.core.contracts import send_contract_call
 from click import command, argument, option, pass_context, ClickException, Context
 
@@ -13,22 +14,26 @@ from click import command, argument, option, pass_context, ClickException, Conte
 @option("--eth-addr", help="Sender eth address or address filename")
 @option("--eth-private-key", help="Sender eth private key")
 @option("--wait", is_flag=True, help="Wait for transaction to complete")
+@option(
+    "--instance-file",
+    default=INSTANCE_FILE_DEFAULT,
+    help=f"Instance file (default={INSTANCE_FILE_DEFAULT})")
 @pass_context
 def token_approve(
         ctx: Context,
         value: str,
         eth_addr: str,
         eth_private_key: str,
-        wait: bool) -> None:
+        wait: bool,
+        instance_file: str) -> None:
     """
     Approve the mixer to spend some amount of ERC20/223 tokens
     """
     approve_value = EtherValue(value)
     eth_addr = load_eth_address(eth_addr)
     eth_private_key_data = load_eth_private_key(eth_private_key)
-    client_ctx = ctx.obj
-    web3 = open_web3_from_ctx(client_ctx)
-    mixer_desc = load_mixer_description_from_ctx(client_ctx)
+    web3 = open_web3_from_network(get_eth_network(ctx.obj["eth_network"]))
+    mixer_desc = load_mixer_description(instance_file)
     if not mixer_desc.token:
         raise ClickException("no token for mixer {mixer_desc.mixer.address}")
 
