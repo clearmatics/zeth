6c2923f86fdaa9bf6ea3f98ce2f3fcb1571e810b
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-09-01 12:56:19 +0100

    Used ZETH_SETUP_DIR env var in mkdir command

--- setup_env.sh
+++ setup_env.sh
@@ -4,8 +4,8 @@ export ZETH=`pwd`
 export ZETH_CONTRACTS_DIR=$ZETH/zeth_contracts/contracts
 export ZETH_DEBUG_DIR=$ZETH/debug
 
-mkdir -p $ZETH/zeth_setup
 export ZETH_SETUP_DIR=$ZETH/zeth_setup
+mkdir -p $ZETH_SETUP_DIR
 
 # Add the zeth executables in the PATH
 export PATH=$ZETH/build/prover_server:$ZETH/build/mpc_tools:$ZETH/build/mpc_tools/mpc_phase2:$PATH
