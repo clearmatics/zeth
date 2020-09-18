5c98f7b4b8dee9a047358da1b303695fae1d3e52
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-09-18 12:54:22 +0100

    client: helper support for importing eth private keys

--- client/zeth/helper/eth_gen_address.py
+++ client/zeth/helper/eth_gen_address.py
@@ -7,7 +7,7 @@
 from zeth.cli.constants import ETH_ADDRESS_DEFAULT, ETH_PRIVATE_KEY_FILE_DEFAULT
 from zeth.cli.utils import write_eth_private_key, write_eth_address
 from zeth.core.utils import eth_address_from_private_key
-from click import command, option
+from click import command, option, ClickException
 from web3 import Web3  # type: ignore
 import os
 import sys
@@ -17,9 +17,11 @@ from typing import Optional
 @command()
 @option("--eth-addr-file", help="Address output filename")
 @option("--eth-private-key-file", help="Private key output filename")
+@option("--use-private-key", help="Use existing private key")
 def eth_gen_address(
         eth_addr_file: Optional[str],
-        eth_private_key_file: Optional[str]) -> None:
+        eth_private_key_file: Optional[str],
+        use_private_key: Optional[str]) -> None:
     """
     Locally generate a new Ethereum private key and address file, and write
     them to the current directory.
@@ -30,7 +32,14 @@ def eth_gen_address(
     eth_addr_file = eth_addr_file or ETH_ADDRESS_DEFAULT
     eth_private_key_file = eth_private_key_file or ETH_PRIVATE_KEY_FILE_DEFAULT
 
-    eth_private_key = gen_eth_private_key()
+    if use_private_key:
+        eth_private_key = bytes.fromhex(use_private_key)
+    else:
+        eth_private_key = gen_eth_private_key()
+
+    if len(eth_private_key) != 32:
+        raise ClickException("invalid private key length")
+
     write_eth_private_key(eth_private_key, eth_private_key_file)
     eth_address = eth_address_from_private_key(eth_private_key)
     write_eth_address(eth_address, eth_addr_file)
