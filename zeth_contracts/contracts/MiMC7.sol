fcb1eb7d76a5e09cc451a4b477d047626f1de191
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-10-14 18:02:14 +0100

    contracts: simpler mimc7 code

--- zeth_contracts/contracts/MiMC7.sol
+++ zeth_contracts/contracts/MiMC7.sol
@@ -4,77 +4,65 @@
 
 pragma solidity ^0.5.0;
 
+// Reference papers:
+//
+// \[AGRRT16]:
+// "MiMC: Efficient Encryption and Cryptographic Hashing with Minimal
+// Multiplicative Complexity", Martin Albrecht, Lorenzo Grassi, Christian
+// Rechberger, Arnab Roy, and Tyge Tiessen, ASIACRYPT 2016,
+// <https://eprint.iacr.org/2016/492.pdf>
+//
+// "One-way compression function"
+// Section: "Miyaguchi–Preneel"
+// <https://en.wikipedia.org/wiki/One-way_compression_function#Miyaguchi%E2%80%93Preneel>
+
 library MiMC7
 {
-    /*
-     * Reference papers:
-     *
-     * \[AGRRT16]:
-     * "MiMC: Efficient Encryption and Cryptographic Hashing with Minimal
-     * Multiplicative Complexity", Martin Albrecht, Lorenzo Grassi, Christian
-     * Rechberger, Arnab Roy, and Tyge Tiessen, ASIACRYPT 2016,
-     * <https://eprint.iacr.org/2016/492.pdf>
-     *
-     * "One-way compression function"
-     * Section: "Miyaguchi–Preneel"
-     * <https://en.wikipedia.org/wiki/One-way_compression_function#Miyaguchi%E2%80%93Preneel>
-     **/
-    function hash(bytes32 x, bytes32 y) internal pure returns (bytes32 out) {
-        assembly {
-            // Use scratch space (0x00) for roundConstant. Must use memory since
-            // keccak256 is iteratively applied. Start with seed =
-            // keccak256("clearmatics_mt_seed")
-            mstore(0x0, 0xdec937b7fa8db3de380427a8cc947bfab68514522c3439cfa2e9965509836814)
+    function hash(bytes32 x, bytes32 y) internal pure returns (bytes32 out)
+    {
+        // See [AGRRT16]:
+        //   The round function is:
+        //     F_i(a, key, rc_i) -> a^7 + key + rc
+        //
+        //   where:
+        //     rc_0 = 0
+        //     rc_1 = keccak(seed)
+        //     rc_i = keccak(rc_{i-1}), for i = 2, ...
+        //
+        // a is initialized as x, and key is set y
 
-            // See:
-            // https://github.com/ethereum/go-ethereum/blob/master/crypto/bn256/cloudflare/constants.go#L23
+        assembly {
+
             let r := 21888242871839275222246405745257275088548364400416034343698204186575808495617
 
-            // y will be use used as round key of the block cipher as defined by
-            // Miyaguchi-Prenel construction
-            let key := y
-
-            // Round function f(message) = (message + key + roundConstant)^d
-            // d (= exponent) = 7; #rounds = 91
-            //
-            // Note on the exponent: gcd(7, r - 1) = 1 which confirms that the
-            // monomial x^7 is a permutation in Fr. See: Proposition 1, Section
-            // 4 and section 5; https://eprint.iacr.org/2016/492.pdf
-            //
-            // In the first round the constant is not used
-            let outPermutation := x
-
-            // a = outPermutation + roundConstant + key mod r
-            let a := addmod(outPermutation, key, r)
-            // a2 = a^2 mod r
+            // Perform round 0 with x + y (rc = 0 in first round)
+            let a := addmod(x, y, r)
             let a2 := mulmod(a, a, r)
-            // outPermutation = a^7 mod r
-            //   (x^7 is the permutation polynomial used)
-            outPermutation := mulmod(mulmod(a2, a2, r), mulmod(a2, a, r), r)
+            a := mulmod(mulmod(mulmod(a2, a2, r), a2, r), a, r)
+
+            // Write round constant seed to pad at 0x00, where keccak256 will
+            // be applied iteratively
+            mstore(0x0, 0xdec937b7fa8db3de380427a8cc947bfab68514522c3439cfa2e9965509836814)
 
             for {let j := 0} slt(j, 90) {j := add(j,1)} {
+
                 // roundConstant = H(roundConstant);
                 // we derive the (round) constants by iterative hash on the seed
                 let roundConstant := keccak256(0x0, 32)
                 mstore(0x0, roundConstant)
-                // a = outPermutation + roundConstant + key mod r
-                a := addmod(addmod(outPermutation, roundConstant, r), key, r)
-                // a2 = a^2 mod r
+
+                // a = (outPermutation + roundConstant + key) ^ 7 mod r
+                a := addmod(addmod(a, roundConstant, r), y, r)
                 a2 := mulmod(a, a, r)
-                // outPermutation = a^7 mod r
-                //   (x^7 is the permutation polynomial used)
-                outPermutation :=  mulmod(mulmod(mulmod(a2, a2, r), a2, r), a, r)
+                a :=  mulmod(mulmod(mulmod(a2, a2, r), a2, r), a, r)
             }
 
-            // Compute H_i from H_{i-1} to generate the round key for the next
-            // entry in the input slice x.  In MiMC the output of the last round
-            // is mixed with the round key: This corresponds to the
-            // `outMiMCCipher = addmod(outPermutation, key, r)`.  And, the
-            // Myjaguchi-Prenell OWCF is ran: `addmod(addmod(outMiMCCipher,
-            // message, r), key, r)`.  Note that we have merged the key addition
-            // ( +key ) of the last round of MiMC with the Myjaguchi-Prenell
-            // step.
-            out := addmod(addmod(addmod(outPermutation, key, r), x, r), key, r)
+            // In MiMC, the final round output is summed with the round key
+            a := addmod(a, y, r)
+
+            // Myjaguchi-Preneel OWCF is then applied, which adds the key and
+            // message to the output of MiMC.
+            out := addmod(addmod(a, x, r), y, r)
         }
     }
 }
