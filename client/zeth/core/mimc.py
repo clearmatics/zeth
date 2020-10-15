66bc88bd4e322a63767c537e19370c5452cd63e3
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-10-15 10:55:42 +0100

    client: refactor mimc code to support other configurations

--- client/zeth/core/mimc.py
+++ client/zeth/core/mimc.py
@@ -2,53 +2,84 @@
 #
 # SPDX-License-Identifier: LGPL-3.0+
 
-from zeth.core.constants import ZETH_PRIME, MIMC_MT_SEED
+from zeth.core.constants import MIMC_MT_SEED
 from Crypto.Hash import keccak \
     # pylint: disable=import-error,no-name-in-module,line-too-long  #type: ignore
-from typing import Optional
+from abc import (ABC, abstractmethod)
+
+# Reference papers:
+#
+# \[AGRRT16]:
+# "MiMC: Efficient Encryption and Cryptographic Hashing with Minimal
+# Multiplicative Complexity", Martin Albrecht, Lorenzo Grassi, Christian
+# Rechberger, Arnab Roy, and Tyge Tiessen, ASIACRYPT 2016,
+# <https://eprint.iacr.org/2016/492.pdf>
+#
+# "One-way compression function"
+# Section: "Miyaguchi–Preneel"
+# <https://en.wikipedia.org/wiki/One-way_compression_function#Miyaguchi%E2%80%93Preneel>
 
 
-class MiMC7:
+class MiMCBase(ABC):
     """
-    Python implementation of MiMC7 algorithm used in the mixer contract
+    Base class of MiMC implmentations.
     """
-
     def __init__(
             self,
-            seed: str = MIMC_MT_SEED,
-            prime: int = ZETH_PRIME):
+            seed_str: str,
+            prime: int,
+            num_rounds: int):
+        self.seed = _keccak_256(_str_to_bytes(seed_str))
         self.prime = prime
-        self.seed = seed
+        self.num_rounds = num_rounds
+
+    def encrypt(
+            self,
+            message: int,
+            ek: int) -> int:
+        result = message % self.prime
+        key = ek % self.prime
+        round_constant: int = self.seed
+
+        # The round constant in round 0 is 0 (see [AGRRT16])
+        result = self.mimc_round(result, key, 0)
+
+        for _ in range(self.num_rounds - 1):
+            round_constant = _update_round_constant(round_constant)
+            result = self.mimc_round(result, key, round_constant)
+
+        # Add key to the final result (see [AGRRT16])
+        return (result + key) % self.prime
+
+    def hash(self, x: int, y: int) -> int:
+        """
+        Apply Miyaguchi-Preneel to the output of the encrypt function.
+        """
+        x = x % self.prime
+        y = y % self.prime
+        return (self.encrypt(x, y) + x + y) % self.prime
+
+    @abstractmethod
+    def mimc_round(self, message: int, key: int, rc: int) -> int:
+        pass
+
+
+class MiMC7(MiMCBase):
+    """
+    MiMC specialized for Fr in ALT-BN128, in which the exponent is 7 and 91
+    rounds are used.
+    """
+    def __init__(self, seed_str: str = MIMC_MT_SEED):
+        MiMCBase.__init__(
+            self,
+            seed_str,
+            21888242871839275222246405745257275088548364400416034343698204186575808495617,  # noqa
+            # pylint: disable=line-too-long
+            91)
 
     def mimc_round(self, message: int, key: int, rc: int) -> int:
         xored = (message + key + rc) % self.prime
         return xored ** 7 % self.prime
-
-    def mimc_encrypt(
-            self,
-            message: int,
-            ek: int,
-            seed: Optional[str] = None,
-            rounds: int = 91) -> int:
-        seed = seed or self.seed
-        res = message % self.prime
-        key = ek % self.prime
-
-        # In the paper the first round constant is set to 0
-        res = self.mimc_round(res, key, 0)
-
-        round_constant: int = _keccak_256(_str_to_bytes(seed))
-
-        for _ in range(rounds - 1):
-            round_constant = _keccak_256(_int_to_bytes32(round_constant))
-            res = self.mimc_round(res, key, round_constant)
-
-        return (res + key) % self.prime
-
-    def mimc_mp(self, x: int, y: int) -> int:
-        x = x % self.prime
-        y = y % self.prime
-        return (self.mimc_encrypt(x, y, self.seed) + x + y) % self.prime
 
 
 def _str_to_bytes(value: str) -> bytes:
@@ -64,3 +95,7 @@ def _keccak_256(data_bytes: bytes) -> int:
     h.update(data_bytes)
     hashed = h.digest()
     return int.from_bytes(hashed, 'big')
+
+
+def _update_round_constant(rc: int) -> int:
+    return _keccak_256(_int_to_bytes32(rc))
