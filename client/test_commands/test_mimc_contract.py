b6e70fd4a20d47f149d09e0ad1ee0523f6cd6823
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-11-06 13:42:21 +0000

    contracts: support for MiMC31

--- client/test_commands/test_mimc_contract.py
+++ client/test_commands/test_mimc_contract.py
@@ -4,7 +4,7 @@
 
 from zeth.core.utils import get_contracts_dir
 from zeth.core.contracts import InstanceDescription
-from zeth.core.mimc import MiMC7
+from zeth.core.mimc import MiMC7, MiMC31
 from zeth.cli.utils import get_eth_network, open_web3_from_network
 from os.path import join
 import sys
@@ -18,6 +18,19 @@ def test_mimc7(instance: Any) -> None:
 
     result = instance.functions.test_mimc7(
         x.to_bytes(32, 'big'), y.to_bytes(32, 'big')).call()
+    assert result == h
+
+
+def test_mimc31(instance: Any) -> None:
+    x = int(28948022309329048855892746252171976963317496166410141009864396001978282409983)  # noqa
+    y = int(14220067918847996031108144435763672811050758065945364308986253046354060608451)  # noqa
+    h = MiMC31().hash(x, y).to_bytes(32, 'big')
+
+    result = instance.functions.test_mimc31(
+        x.to_bytes(32, 'big'), y.to_bytes(32, 'big')).call()
+
+    print(f"h={int.from_bytes(h, byteorder='big')}")
+    print(f"result={int.from_bytes(result, byteorder='big')}")
     assert result == h
 
 
@@ -35,6 +48,7 @@ def main() -> int:
     contract_instance = contract_instance_desc.instantiate(web3)
 
     test_mimc7(contract_instance)
+    test_mimc31(contract_instance)
 
     print("========================================")
     print("==              PASSED                ==")
--- zeth_contracts/contracts/MiMC_test.sol
+++ zeth_contracts/contracts/MiMC_test.sol
@@ -5,6 +5,7 @@
 pragma solidity ^0.5.0;
 
 import "./MiMC7.sol";
+import "./MiMC31.sol";
 
 // Contract to test the MiMC libraries
 contract MiMC_test
@@ -12,5 +13,10 @@ contract MiMC_test
     function test_mimc7(bytes32 x, bytes32 y) public returns (bytes32)
     {
         return MiMC7.hash(x, y);
+    }
+
+    function test_mimc31(bytes32 x, bytes32 y) public returns (bytes32)
+    {
+        return MiMC31.hash(x, y);
     }
 }
