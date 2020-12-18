afbbfe6fa2dbb9132c0599ea009bcde721db0a8c
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-12-18 18:15:05 +0000

    client: fix and clean up test_contract_mixer_base

--- client/test_commands/test_contract_base_mixer.py
+++ client/test_commands/test_contract_base_mixer.py
@@ -11,66 +11,44 @@ from zeth.core.mixer_client import MixerClient
 from typing import Any
 import test_commands.mock as mock
 
-
 # The UNPACKED_PRIMARY_INPUTS variable represents a dummy primary input,
 # it is structured as follows,
-UNPACKED_PRIMARY_INPUTS = [
-    0,  # rt
+
+ROOT = 0
+
+NULLIFIERS = [
     24,  # nf_0 = "0...01 1000"
     33,  # nf_1 = "0...010 0001"
+]
+
+COMMITMENTS = [
     1,  # cm_0 = "0...01"
     2,  # cm_1 = "0...010"
-    2**PUBLIC_VALUE_LENGTH - 1,  # v_in = "1...1"
-    0,  # v_out = "0...0"
-    47,  # h_sig = "0...010 1111"
-    50,  # htag_0 = "0...011 0010"
-    59  # htag_1 = "0...011 1011"
 ]
-# The values were set so that the RESIDUAL_BITS are easily distinguishable.
 
-# PACKED_PRIMARY_INPUTS =
-#   rt || {nf}_1,2 || {cm}_1,2 || h_sig || {h}_1,2 || RESIDUAL_BITS
-PACKED_PRIMARY_INPUTS = [
-    0,  # root
-    1,  # cm_0
-    2,  # cm_1
-    3,  # nf_0
-    4,  # nf_1
-    5,  # h_sig
-    6,  # h_0
-    7,  # h_1
-    11150372599265311570163396226516866165665875] \
-        # pylint: disable=no-member,invalid-name
+HSIG = 47  # h_sig = "0...010 1111"
 
-# RESIDUAL_BITS =
-#   v_in || v_out || h_sig || {nf}_1,2 || {h}_1,2
-# We set dummy values for all variables. The residual_bits are as follows:
-# RESIDUAL_BITS = 713623846352979940490457358497079434602616037, or in bits
-# 1-4:   00000000 00000000 00000000 00000000
-# 5-8:   00000000 00000000 00000000 00000000
-# 9-12:  00000000 00000000 00000000 00000000
-# 13-16: 00000000 00000000 01111111 11111111
-# 17-20: 11111111 11111111 11111111 11111111
-# 21-24: 11111111 11111111 10000000 00000000
-# 25-28: 00000000 00000000 00000000 00000000
-# 29-32: 00000000 00000000 01110000 01010011
-# This corresponds to
-# v_in  = "0xFFFFFFFFFFFFFFFF" = 2**PUBLIC_VALUE_LENGTH - 1
-# v_out = "0x0000000000000000" = 0
-# h_sig = "111" = 7
-# nf_0  = "000" = 0
-# nf_1  = "001" = 1
-# h_0   = "010" = 2
-# h_1   = "011" = 3
-RESIDUAL_BITS = [
-    2**PUBLIC_VALUE_LENGTH - 1,  # v_in
-    0,  # v_out
-    7,  # h_sig
-    0,  # nf_0
-    1,  # nf_1
-    2,  # h_0
-    3  # h_1
-    ]  # pylint: disable=no-member,invalid-name
+HTAGS = [
+    50,  # htag_0 = "0...011 0010"
+    59,  # htag_1 = "0...011 1011"
+]
+
+VPUB = (0xffffffffffffffff, 0)
+
+# 255                                         128         64           0
+# |<empty>|<h_sig>|<nullifiers>|<msg_auth_tags>|<v_pub_in>)|<v_pub_out>|
+RESIDUAL_BITS = int(
+    "000" # h_sig
+    "000" # nf_2
+    "000" # nf_1
+    "000" # htag_0
+    "000" # htag_1
+    "1111111111111111111111111111111111111111111111111111111111111111" # vin
+    "0000000000000000000000000000000000000000000000000000000000000000", # vout
+    2)
+
+PACKED_PRIMARY_INPUTS = \
+    [ROOT] + COMMITMENTS + NULLIFIERS + [HSIG] + HTAGS + [RESIDUAL_BITS]
 
 
 def test_assemble_nullifiers(mixer_instance: Any) -> int:
@@ -80,24 +58,24 @@ def test_assemble_nullifiers(mixer_instance: Any) -> int:
         res = mixer_instance.functions.\
             assemble_nullifier(i, PACKED_PRIMARY_INPUTS).call()
         val = int.from_bytes(res, byteorder="big")
-        if val != UNPACKED_PRIMARY_INPUTS[1+i]:
-            print("ERROR: extracted wrong nullifier")
-            print("expected:", UNPACKED_PRIMARY_INPUTS[1+i], i)
-            print("got:", val, i)
+        if val != NULLIFIERS[i]:
+            print(f"ERROR: extracted wrong nullifier[{i}]")
+            print(f"expected: {NULLIFIERS[i]}")
+            print(f"got: {val}")
             return 1
     return 0
 
 
-def test_assemble_hsig(mixer_instance: Any) -> Any:
+def test_assemble_hsig(mixer_instance: Any) -> int:
     # Test retrieving commitments
     print("--- testing ", "test_assemble_hsig")
     res = mixer_instance.functions.\
         assemble_hsig(PACKED_PRIMARY_INPUTS).call()
     hsig = int.from_bytes(res, byteorder="big")
-    if hsig != UNPACKED_PRIMARY_INPUTS[JS_INPUTS + JS_OUTPUTS + 3]:
-        print("ERROR: extracted wrong public values")
-        print("expected:", UNPACKED_PRIMARY_INPUTS[JS_INPUTS + JS_OUTPUTS + 3])
-        print("got:", hsig)
+    if hsig != HSIG:
+        print("ERROR: extracted wrong h_sig")
+        print(f"expected: {hsig}")
+        print(f"got: {hsig}")
         return 1
     return 0
 
@@ -106,12 +84,9 @@ def test_assemble_vpub(mixer_instance: Any) -> int:
     # Test retrieving commitments
     print("--- testing ", "test_assemble_vpub")
     v_in, v_out = mixer_instance.functions.assemble_public_values(
-        PACKED_PRIMARY_INPUTS).call()
-    v_in_expect = UNPACKED_PRIMARY_INPUTS[JS_INPUTS + JS_OUTPUTS + 1] \
-        * ZETH_PUBLIC_UNIT_VALUE
-    v_out_expect = UNPACKED_PRIMARY_INPUTS[JS_INPUTS + JS_OUTPUTS + 2] \
-        * ZETH_PUBLIC_UNIT_VALUE
-
+        PACKED_PRIMARY_INPUTS[-1]).call()
+    v_in_expect = VPUB[0] * ZETH_PUBLIC_UNIT_VALUE
+    v_out_expect = VPUB[1] * ZETH_PUBLIC_UNIT_VALUE
     if v_in != v_in_expect or v_out != v_out_expect:
         print("ERROR: extracted wrong public values")
         print(f"expected: {(v_in_expect, v_out_expect)}")
