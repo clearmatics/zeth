7d20964ddb988144bf7ca9283eccb1f12caa7766
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-01-07 21:58:56 +0000

    client: improve support for permitted_dispatcher and vk_hash in deploy command

--- client/zeth/cli/zeth_deploy.py
+++ client/zeth/cli/zeth_deploy.py
@@ -6,9 +6,9 @@ from zeth.cli.constants import INSTANCE_FILE_DEFAULT
 from zeth.cli.utils import \
     open_web3_from_ctx, get_erc20_instance_description, load_eth_address, \
     load_eth_private_key, write_mixer_description, MixerDescription, \
-    create_prover_client
+    create_prover_client, load_contract_address
 from zeth.core.mixer_client import MixerClient
-from click import Context, command, option, pass_context
+from click import Context, command, option, pass_context, ClickException
 from typing import Optional
 
 
@@ -22,7 +22,7 @@ from typing import Optional
 @option("--token-address", help="Address of token contract (if used)")
 @option(
     "--permitted-dispatcher",
-    help="Address of contract permitted to call dispatch method")
+    help="Instance file or address of contract permitted to call dispatch method")
 @option("--vk-hash", type=str, help="verification key hash for dispatch calls")
 @option("--deploy-gas", type=int, help="Maximum gas, in Wei")
 @pass_context
@@ -43,9 +43,18 @@ def deploy(
     client_ctx = ctx.obj
     web3 = open_web3_from_ctx(client_ctx)
 
+    if bool(permitted_dispatcher) != bool(vk_hash):
+        raise ClickException(
+            "Must supply BOTH --permitted-dispatch AND --vk-hash, or NEITHER")
+
     print(f"deploy: eth_address={eth_address}")
     print(f"deploy: instance_out={instance_out}")
     print(f"deploy: token_address={token_address}")
+
+    if permitted_dispatcher:
+        permitted_dispatcher = load_contract_address(permitted_dispatcher)
+        print(f"deploy: permitted_dispatcher={permitted_dispatcher}")
+        print(f"deploy: vk_hash={vk_hash}")
 
     token_instance_desc = get_erc20_instance_description(token_address) \
         if token_address else None
