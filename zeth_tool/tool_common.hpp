4396223febe36b97fc255ccaedebf2d55ccc62cf
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-05-12 17:46:39 +0100

    zeth_tool: fully generic resolution of curve and snark types from string names

--- zeth_tool/tool_common.hpp
+++ zeth_tool/tool_common.hpp
@@ -6,12 +6,65 @@
 #define __ZETH_TOOL_TOOL_COMMON_HPP__
 
 #include "libtool/subcommand.hpp"
+#include "libzeth/snarks/groth16/groth16_snark.hpp"
+#include "libzeth/snarks/pghr13/pghr13_snark.hpp"
+
+#include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
+#include <libff/algebra/curves/bls12_377/bls12_377_pp.hpp>
+#include <libff/algebra/curves/bw6_761/bw6_761_pp.hpp>
 
 namespace zethtool
 {
 
 using global_options = bool;
 using zeth_subcommand = libtool::subcommand<global_options>;
+
+/// Given a class of the form:
+///
+///   template<typename ppT, typename snarkT> class Callee
+///   {
+///   public:
+///       static int execute(std::string &a, ...);
+///   };
+///
+/// this class can be used to invoke the correct instantiation of Callee based
+/// on curve_name and snark_name strings. E.g.
+///
+///   int r = curve_and_snark_resolver<Callee>("alt-bn128", "groth16", a, ....);
+template<template<typename, typename> class Callee>
+class curve_and_snark_resolver
+{
+public:
+    template<typename... Args>
+    static int resolve(
+        const std::string &curve_name,
+        const std::string &snark_name,
+        Args... args)
+    {
+        if (curve_name == "alt-bn128") {
+            return resolve_snark<libff::alt_bn128_pp>(snark_name, args...);
+        } else if (curve_name == "bls12-377") {
+            return resolve_snark<libff::bls12_377_pp>(snark_name, args...);
+        } else if (curve_name == "bw6-761") {
+            return resolve_snark<libff::bw6_761_pp>(snark_name, args...);
+        }
+
+        throw po::error("unrecognized curve");
+    }
+
+protected:
+    template<typename ppT, typename... Args>
+    static int resolve_snark(const std::string &snark_name, Args... args)
+    {
+        if (snark_name == "groth16") {
+            return Callee<ppT, libzeth::groth16_snark<ppT>>::execute(args...);
+        } else if (snark_name == "pghr13") {
+            return Callee<ppT, libzeth::pghr13_snark<ppT>>::execute(args...);
+        }
+
+        throw po::error("unrecognized snark");
+    }
+};
 
 } // namespace zethtool
 
--- zeth_tool/verify_cmd.cpp
+++ zeth_tool/verify_cmd.cpp
@@ -6,74 +6,12 @@
 
 #include "libtool/tool_util.hpp"
 #include "libzeth/serialization/r1cs_variable_assignment_serialization.hpp"
-#include "libzeth/snarks/groth16/groth16_snark.hpp"
-#include "libzeth/snarks/pghr13/pghr13_snark.hpp"
-
-#include <fstream>
-#include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
-#include <libff/algebra/curves/bls12_377/bls12_377_pp.hpp>
-#include <libff/algebra/curves/bw6_761/bw6_761_pp.hpp>
 
 namespace zethtool
 {
 
 namespace commands
 {
-
-template<typename ppT, typename snarkT>
-int verifier_main(
-    const std::string &vk_file,
-    const std::string &primary_input_file,
-    const std::string &proof_file)
-{
-    ppT::init_public_params();
-    libff::inhibit_profiling_info = true;
-    libff::inhibit_profiling_counters = true;
-
-    typename snarkT::verification_key verification_key;
-    {
-        std::ifstream in_s = libtool::open_input_binary_file(vk_file);
-        snarkT::verification_key_read_bytes(verification_key, in_s);
-    }
-
-    libsnark::r1cs_primary_input<libff::Fr<ppT>> primary_input;
-    {
-        std::ifstream in_s =
-            libtool::open_input_binary_file(primary_input_file);
-        libzeth::r1cs_variable_assignment_read_bytes(primary_input, in_s);
-    }
-
-    typename snarkT::proof proof;
-    {
-        std::ifstream in_s = libtool::open_input_binary_file(proof_file);
-        snarkT::proof_read_bytes(proof, in_s);
-    }
-
-    if (!snarkT::verify(primary_input, proof, verification_key)) {
-        std::cout << "verification failed.\n";
-        return 1;
-    }
-
-    return 0;
-}
-
-template<typename ppT>
-int verifier_resolve_snark(
-    const std::string &vk_file,
-    const std::string &primary_input_file,
-    const std::string &proof_file,
-    const std::string &snark)
-{
-    if (snark == "groth16") {
-        return verifier_main<ppT, libzeth::groth16_snark<ppT>>(
-            vk_file, primary_input_file, proof_file);
-    } else if (snark == "pghr13") {
-        return verifier_main<ppT, libzeth::pghr13_snark<ppT>>(
-            vk_file, primary_input_file, proof_file);
-    }
-
-    throw po::error("unrecognized snark");
-}
 
 class verify_cmd : public zeth_subcommand
 {
@@ -85,6 +23,50 @@ public:
     }
 
 protected:
+    /// Given in the form of a class, in order to be used as a parameter to
+    /// curve_and_snark_resolver.
+    template<typename ppT, typename snarkT> class verify_runner
+    {
+    public:
+        static int execute(
+            const std::string &vk_file,
+            const std::string &primary_input_file,
+            const std::string &proof_file)
+        {
+            ppT::init_public_params();
+            libff::inhibit_profiling_info = true;
+            libff::inhibit_profiling_counters = true;
+
+            typename snarkT::verification_key verification_key;
+            {
+                std::ifstream in_s = libtool::open_input_binary_file(vk_file);
+                snarkT::verification_key_read_bytes(verification_key, in_s);
+            }
+
+            libsnark::r1cs_primary_input<libff::Fr<ppT>> primary_input;
+            {
+                std::ifstream in_s =
+                    libtool::open_input_binary_file(primary_input_file);
+                libzeth::r1cs_variable_assignment_read_bytes(
+                    primary_input, in_s);
+            }
+
+            typename snarkT::proof proof;
+            {
+                std::ifstream in_s =
+                    libtool::open_input_binary_file(proof_file);
+                snarkT::proof_read_bytes(proof, in_s);
+            }
+
+            if (!snarkT::verify(primary_input, proof, verification_key)) {
+                std::cout << "verification failed.\n";
+                return 1;
+            }
+
+            return 0;
+        }
+    };
+
     void initialize_suboptions(
         boost::program_options::options_description &options,
         boost::program_options::options_description &all_options,
@@ -142,18 +124,8 @@ protected:
 
     int execute_subcommand(const global_options &) override
     {
-        if (curve == "alt-bn128") {
-            return verifier_resolve_snark<libff::alt_bn128_pp>(
-                vk_file, primary_input_file, proof_file, snark);
-        } else if (curve == "bls12-377") {
-            return verifier_resolve_snark<libff::bls12_377_pp>(
-                vk_file, primary_input_file, proof_file, snark);
-        } else if (curve == "bw6-761") {
-            return verifier_resolve_snark<libff::bw6_761_pp>(
-                vk_file, primary_input_file, proof_file, snark);
-        }
-
-        throw po::error("unrecognized curve");
+        return curve_and_snark_resolver<verify_runner>::resolve(
+            curve, snark, vk_file, primary_input_file, proof_file);
     }
 
     std::string vk_file;
